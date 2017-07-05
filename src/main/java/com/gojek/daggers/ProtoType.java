f5aba59939786eab544ee6fb4880156276bf9110
Rajaram <rajaram.mallya@go-jek.com>
2017-07-05 06:04:09 +0530

    proto type gives out type information

--- src/main/java/com/gojek/daggers/ProtoType.java
+++ src/main/java/com/gojek/daggers/ProtoType.java
@@ -1,9 +1,27 @@
 package com.gojek.daggers;
 
 import com.google.protobuf.Descriptors;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.table.api.Types;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.google.protobuf.Descriptors.FieldDescriptor.JavaType;
 
 public class ProtoType {
     public static final String PROTO_CLASS_MISCONFIGURED_ERROR = "Proto class is misconfigured";
+    private static final Map<JavaType, TypeInformation> TYPE_MAP = new HashMap<JavaType, TypeInformation>(){{
+        put(JavaType.STRING, Types.STRING());
+        put(JavaType.BOOLEAN, Types.BOOLEAN());
+        put(JavaType.DOUBLE, Types.DOUBLE());
+        put(JavaType.LONG, Types.LONG());
+        put(JavaType.MESSAGE, Types.ROW());
+        put(JavaType.ENUM, Types.STRING());
+        put(JavaType.INT, Types.INT());
+        put(JavaType.FLOAT, Types.FLOAT());
+        put(JavaType.BYTE_STRING, Types.STRING());
+    }};
 
     private Descriptors.Descriptor protoFieldDescriptor;
 
@@ -18,5 +36,10 @@ public class ProtoType {
 
     public String[] getFieldNames() {
         return protoFieldDescriptor.getFields().stream().map(fieldDescriptor -> fieldDescriptor.getName()).toArray(String[]::new);
+    }
+
+    public TypeInformation[] getFieldTypes() {
+        return protoFieldDescriptor.getFields().stream()
+                .map(fieldDescriptor -> TYPE_MAP.get(fieldDescriptor.getJavaType())).toArray(TypeInformation[]::new);
     }
 }
