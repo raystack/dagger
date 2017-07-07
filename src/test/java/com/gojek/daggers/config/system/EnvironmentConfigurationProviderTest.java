3e04c99da1d2b9f57393c079192cb29e396ebee8
Deepak Marathe <dpkmarathe@gmail.com>
2017-07-07 15:49:50 +0530

    injecting environment into EnvironmentConfigurationProvider

--- src/test/java/com/gojek/daggers/config/system/EnvironmentConfigurationProviderTest.java
+++ src/test/java/com/gojek/daggers/config/system/EnvironmentConfigurationProviderTest.java
@@ -1,31 +1,26 @@
 package com.gojek.daggers.config.system;
 
+import com.gojek.daggers.config.EnvironmentConfigurationProvider;
 import org.apache.flink.configuration.Configuration;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import java.util.Map;
+import java.util.HashMap;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class EnvironmentConfigurationProviderTest {
 
-    private EnvironmentConfigurationProvider environmentConfigurationProvider;
-
-    @Before
-    public void setup(){
-        environmentConfigurationProvider = new EnvironmentConfigurationProvider();
-    }
-
     @Test
     public void shouldProvideSystemConfiguration() {
-        System.setProperty("key", "value");
-        System.setProperty("key2", "value2");
+        HashMap<String, String> environmentParameters = new HashMap<String, String>() {{
+            put("key", "value");
+            put("key2", "value2");
+        }};
 
-        Configuration stringStringMap = environmentConfigurationProvider.get();
+        Configuration stringStringMap = new EnvironmentConfigurationProvider(environmentParameters).get();
 
         assertEquals(stringStringMap.getString("key",""), "value");
         assertEquals(stringStringMap.getString("key2",""), "value2");
