2bdb97fa1676e57d45a42f75bfff0eb20dca398e
Deepak Marathe <deepak.marathe@go-jek.com>
2017-07-07 11:26:46 +0530

    adding KafkaEnvironmentVariables to dynamically parse configurations related to kafka.

---
+++ src/main/java/com/gojek/daggers/parser/KafkaEnvironmentVariables.java
@@ -0,0 +1,31 @@
+package com.gojek.daggers.parser;
+
+import org.apache.flink.configuration.Configuration;
+
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+public class KafkaEnvironmentVariables {
+
+    private static final String KAFKA_PREFIX = "kafka_consumer_config_";
+
+    public static Properties parse(Configuration configuration) {
+        Properties props = new Properties();
+
+        if (configuration == null || configuration.keySet().size() == 0) {
+            return props;
+        }
+
+        configuration.toMap().entrySet()
+                .stream()
+                .filter(e -> e.getKey().toLowerCase().startsWith(KAFKA_PREFIX))
+//                .collect(Collectors.toMap(e -> parseVarName(e.getKey()), e -> e.getValue()))
+                .forEach( e -> props.setProperty(parseVarName(e.getKey()), e.getValue()));
+        return props;
+    }
+
+    private static String parseVarName(String varName) {
+        String[] names = varName.toLowerCase().replaceAll(KAFKA_PREFIX, "").split("_");
+        return String.join(".", names);
+    }
+}
---
+++ src/test/java/com/gojek/daggers/parser/KafkaEnvironmentVariablesTest.java
@@ -0,0 +1,75 @@
+package com.gojek.daggers.parser;
+
+import org.apache.flink.configuration.Configuration;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import static org.junit.Assert.*;
+
+public class KafkaEnvironmentVariablesTest {
+
+    @Test
+    public void shouldReturnKafkaEnvVarsPositive() {
+        Configuration configuration = new Configuration();
+        new HashMap<String, String>() {{
+            put("PATH", "/usr/local/bin");
+            put("SHELL", "/usr/local/bin/zsh");
+            put("kafka_consumer_config_fetch_min_bytes", "1");
+            put("kafka_consumer_config_ssl_keystore_location", "/home/user/.ssh/keystore");
+            put("kafka_consumer_config_enable_auto_commit", "false");
+        }}.entrySet()
+                .stream()
+                .forEach(e -> configuration.setString(e.getKey(), e.getValue()));
+
+        Properties expectedEnvVars = new Properties() {{
+            put("fetch.min.bytes", "1");
+            put("ssl.keystore.location", "/home/user/.ssh/keystore");
+            put("enable.auto.commit", "false");
+        }};
+
+        Properties actualEnvVars = KafkaEnvironmentVariables.parse(configuration);
+
+        assertEquals(expectedEnvVars, actualEnvVars);
+    }
+
+    @Test
+    public void shouldReturnKafkaEnvVarsNegative() {
+        Configuration configuration = new Configuration();
+        new HashMap<String, String>() {{
+            put("PATH", "/usr/local/bin");
+            put("SHELL", "/usr/local/bin/zsh");
+        }}.entrySet()
+                .stream()
+                .forEach(e -> configuration.setString(e.getKey(), e.getValue()));
+
+        Map<String, String> expectedEnvVars = new HashMap<>();
+
+        Properties actualEnvVars = KafkaEnvironmentVariables.parse(configuration);
+
+        assertEquals(expectedEnvVars, actualEnvVars);
+    }
+
+    @Test
+    public void shouldReturnEmptyCollectionOnNullConfiguration() {
+        Configuration configuration = null;
+        Map<String, String> expectedEnvVars = new HashMap<>();
+
+        Properties actualEnvVars = KafkaEnvironmentVariables.parse(configuration);
+
+        assertEquals(expectedEnvVars, actualEnvVars);
+    }
+
+    @Test
+    public void shouldReturnEmptyCollectionOnEmptyConfiguration() {
+        Configuration configuration = new Configuration();
+
+        Map<String, String> expectedEnvVars = new HashMap<>();
+
+        Properties actualEnvVars = KafkaEnvironmentVariables.parse(configuration);
+
+        assertEquals(expectedEnvVars, actualEnvVars);
+    }
+}
