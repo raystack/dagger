57d4cf6d46f5732c435979b11e727dbee4bed7e2
Deepak Marathe <dpkmarathe@gmail.com>
2017-07-14 10:44:30 +0530

    changing configurationprovider to return commandline provider as fallback option.

--- src/main/java/com/gojek/daggers/config/ConfigurationProviderFactory.java
+++ src/main/java/com/gojek/daggers/config/ConfigurationProviderFactory.java
@@ -1,25 +1,36 @@
 package com.gojek.daggers.config;
 
 import com.gojek.daggers.DaggerConfigurationException;
+import org.slf4j.Logger;
 
-public class ConfigurationProviderFactory{
+import java.util.Arrays;
+
+public class ConfigurationProviderFactory {
 
     private String[] args;
 
     public ConfigurationProviderFactory(String[] args) {
 
         this.args = args;
+        System.out.println("Arguments are : ");
+        Arrays.asList(args).stream().forEach(s -> {
+            System.out.println(s);
+        });
     }
 
     public ConfigurationProvider Provider() {
-        switch (System.getProperty("ConfigSource")){
-            case "ENVIRONMENT":
-                return new EnvironmentConfigurationProvider(System.getenv());
-            case "ARGS":
-                return new CommandlineConfigurationProvider(args);
-            default:
-                throw new DaggerConfigurationException("Config source not provided");
+        if (System.getProperties().containsKey("ConfigSource")) {
+            String configSource = System.getProperty("ConfigSource");
+            switch (configSource) {
+                case "ENVIRONMENT":
+                    return new EnvironmentConfigurationProvider(System.getenv());
+                case "ARGS":
+                    return new CommandlineConfigurationProvider(args);
+                default:
+                    throw new DaggerConfigurationException("Config source not provided");
 
+            }
         }
+        return new CommandlineConfigurationProvider(args);
     }
 }
