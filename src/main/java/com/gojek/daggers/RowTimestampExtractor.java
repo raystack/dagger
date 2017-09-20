5dd9cd9bf97c334bbbf0189e366cdbf87bbd968d
Rajaram <rajarammallya@gmail.com>
2017-09-20 09:53:07 +0530

    [Rajaram and Prakhar]  fixed rowtime extractor

--- src/main/java/com/gojek/daggers/RowTimestampExtractor.java
+++ src/main/java/com/gojek/daggers/RowTimestampExtractor.java
@@ -1,34 +1,37 @@
 package com.gojek.daggers;
 
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
 import org.apache.flink.streaming.api.watermark.Watermark;
 import org.apache.flink.types.Row;
 
 import javax.annotation.Nullable;
+import java.time.Instant;
 
 public class RowTimestampExtractor implements AssignerWithPeriodicWatermarks<Row> {
 
     private int timestampRowIndex;
+    private int watermarkDelay;
     private long currentTimestamp;
-    private static final int MILLISECOND_FACTOR = 1000;
 
-    public RowTimestampExtractor(int timestampRowIndex) {
+    public RowTimestampExtractor(int timestampRowIndex, Configuration config) {
         this.timestampRowIndex = timestampRowIndex;
+        this.watermarkDelay = config.getInteger("WATERMARK_DELAY_MS", 0);
     }
 
     @Nullable
     @Override
     public Watermark getCurrentWatermark() {
-        return new Watermark(currentTimestamp);
+        return new Watermark(currentTimestamp - this.watermarkDelay);
     }
 
     @Override
     public long extractTimestamp(Row element, long previousElementTimestamp) {
         Row timestampRow = (Row) element.getField(timestampRowIndex);
         long timestampSeconds = (long) timestampRow.getField(0);
-        int timestampNanos = (int) timestampRow.getField(1);
+        long timestampNanos = (int) timestampRow.getField(1);
 
-        currentTimestamp = timestampSeconds * MILLISECOND_FACTOR + timestampNanos / MILLISECOND_FACTOR;
+        currentTimestamp = Instant.ofEpochSecond(timestampSeconds, timestampNanos).toEpochMilli();
         return currentTimestamp;
     }
 }
