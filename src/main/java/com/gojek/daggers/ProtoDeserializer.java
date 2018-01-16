9903fe601623f8ba74a054cf4e8c90893eadb622
rohilsurana <rohilsurana96@gmail.com>
2018-01-17 01:27:38 +0530

    [Rajaram | Rohil]  Move to FLink 1.4 and flud dev version

--- src/main/java/com/gojek/daggers/ProtoDeserializer.java
+++ src/main/java/com/gojek/daggers/ProtoDeserializer.java
@@ -9,115 +9,133 @@ import org.apache.flink.types.Row;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class ProtoDeserializer implements KeyedDeserializationSchema<Row> {
 
-  private static final String PROTO_CLASS_MISCONFIGURED_ERROR = "proto class is misconfigured";
-  private String protoClassName;
-  private ProtoType protoType;
-  private transient Method protoParser;
+    private static final String PROTO_CLASS_MISCONFIGURED_ERROR = "proto class is misconfigured";
+    private String protoClassName;
+    private ProtoType protoType;
+    private int timestampFieldIndex;
+    private transient Method protoParser;
 
-  public ProtoDeserializer(String protoClassName) {
-    this.protoClassName = protoClassName;
-    this.protoType = new ProtoType(protoClassName);
-  }
-
-  private Method createProtoParser() {
-    try {
-      Class<?> protoClass = Class.forName(protoClassName);
-      return protoClass.getMethod("parseFrom", byte[].class);
-    } catch (ReflectiveOperationException exception) {
-      throw new DaggerConfigurationException(PROTO_CLASS_MISCONFIGURED_ERROR, exception);
+    public ProtoDeserializer(String protoClassName, int timestampFieldIndex, String rowtimeAttributeName) {
+        this.protoClassName = protoClassName;
+        this.protoType = new ProtoType(protoClassName, rowtimeAttributeName);
+        this.timestampFieldIndex = timestampFieldIndex;
     }
-  }
 
-  private Method getProtoParser() {
-    if (protoParser == null) {
-      protoParser = createProtoParser();
-    }
-    return protoParser;
-  }
-
-  private Row getRow(GeneratedMessageV3 proto) {
-    List<Descriptors.FieldDescriptor> fields = proto.getDescriptorForType().getFields();
-    Row row = new Row(fields.size());
-    for (Descriptors.FieldDescriptor field : fields) {
-      if (field.isMapField()) {
-        List<MapEntry<Object, Object>> mapEntries = (List<MapEntry<Object, Object>>) proto.getField(field);
-        row.setField(field.getIndex(), getMapRow(mapEntries));
-        continue;
-      }
-
-      if (field.getType() == Descriptors.FieldDescriptor.Type.ENUM) {
-        row.setField(field.getIndex(), proto.getField(field).toString());
-      } else if (field.getType() == Descriptors.FieldDescriptor.Type.MESSAGE) {
-        if (field.isRepeated()) {
-          row.setField(field.getIndex(), getRow((List<GeneratedMessageV3>) proto.getField(field)));
-        } else {
-          row.setField(field.getIndex(), getRow((GeneratedMessageV3) proto.getField(field)));
+    private Method createProtoParser() {
+        try {
+            Class<?> protoClass = Class.forName(protoClassName);
+            return protoClass.getMethod("parseFrom", byte[].class);
+        } catch (ReflectiveOperationException exception) {
+            throw new DaggerConfigurationException(PROTO_CLASS_MISCONFIGURED_ERROR, exception);
         }
-      } else {
-        if (field.isRepeated()) {
-          if (field.getJavaType() == Descriptors.FieldDescriptor.JavaType.STRING) {
-            row.setField(field.getIndex(), getRowForStringList((List<String>) proto.getField(field)));
-          }
-        } else {
-          row.setField(field.getIndex(), proto.getField(field));
+    }
+
+    private Method getProtoParser() {
+        if (protoParser == null) {
+            protoParser = createProtoParser();
         }
-      }
+        return protoParser;
     }
-    return row;
-  }
 
-  private Object[] getRow(List<GeneratedMessageV3> protos) {
-    ArrayList<Row> rows = new ArrayList<>();
-    protos.forEach(generatedMessageV3 -> rows.add(getRow(generatedMessageV3)));
-    return rows.toArray();
-  }
+    private Row getRow(GeneratedMessageV3 proto) {
+        List<Descriptors.FieldDescriptor> fields = proto.getDescriptorForType().getFields();
+        Row row = new Row(fields.size());
+        for (Descriptors.FieldDescriptor field : fields) {
+            if (field.isMapField()) {
+                List<MapEntry<Object, Object>> mapEntries = (List<MapEntry<Object, Object>>) proto.getField(field);
+                row.setField(field.getIndex(), getMapRow(mapEntries));
+                continue;
+            }
 
-  private Object[] getMapRow(List<MapEntry<Object, Object>> protos) {
-    ArrayList<Row> rows = new ArrayList<>();
-    protos.forEach(entry -> rows.add(getRow(entry)));
-    return rows.toArray();
-  }
-
-  private Row getRow(MapEntry<Object, Object> protos) {
-    Row row = new Row(2);
-    row.setField(0, protos.getKey());
-    row.setField(1, protos.getValue());
-    return row;
-  }
-
-  private String[] getRowForStringList(List<String> listField) {
-    String[] list = new String[listField.size()];
-    for (int listIndex = 0; listIndex < listField.size(); listIndex++) {
-      list[listIndex] = listField.get(listIndex);
+            if (field.getType() == Descriptors.FieldDescriptor.Type.ENUM) {
+                row.setField(field.getIndex(), proto.getField(field).toString());
+            } else if (field.getType() == Descriptors.FieldDescriptor.Type.MESSAGE) {
+                if (field.isRepeated()) {
+                    row.setField(field.getIndex(), getRow((List<GeneratedMessageV3>) proto.getField(field)));
+                } else {
+                    row.setField(field.getIndex(), getRow((GeneratedMessageV3) proto.getField(field)));
+                }
+            } else {
+                if (field.isRepeated()) {
+                    if (field.getJavaType() == Descriptors.FieldDescriptor.JavaType.STRING) {
+                        row.setField(field.getIndex(), getRowForStringList((List<String>) proto.getField(field)));
+                    }
+                } else {
+                    row.setField(field.getIndex(), proto.getField(field));
+                }
+            }
+        }
+        return row;
     }
-    return list;
-  }
 
-
-  @Override
-  public Row deserialize(byte[] messageKey, byte[] message, String topic, int partition, long offset) throws IOException {
-    try {
-      GeneratedMessageV3 proto = (GeneratedMessageV3) getProtoParser().invoke(null, message);
-      Row row = getRow(proto);
-      return row;
-    } catch (ReflectiveOperationException e) {
-      throw new ProtoDeserializationExcpetion(e);
+    private Object[] getRow(List<GeneratedMessageV3> protos) {
+        ArrayList<Row> rows = new ArrayList<>();
+        protos.forEach(generatedMessageV3 -> rows.add(getRow(generatedMessageV3)));
+        return rows.toArray();
     }
-  }
 
-  @Override
-  public boolean isEndOfStream(Row nextElement) {
-    return false;
-  }
+    private Object[] getMapRow(List<MapEntry<Object, Object>> protos) {
+        ArrayList<Row> rows = new ArrayList<>();
+        protos.forEach(entry -> rows.add(getRow(entry)));
+        return rows.toArray();
+    }
 
-  @Override
-  public TypeInformation<Row> getProducedType() {
-    return protoType.getRowType();
-  }
+    private Row getRow(MapEntry<Object, Object> protos) {
+        Row row = new Row(2);
+        row.setField(0, protos.getKey());
+        row.setField(1, protos.getValue());
+        return row;
+    }
+
+    private String[] getRowForStringList(List<String> listField) {
+        String[] list = new String[listField.size()];
+        for (int listIndex = 0; listIndex < listField.size(); listIndex++) {
+            list[listIndex] = listField.get(listIndex);
+        }
+        return list;
+    }
+
+
+    @Override
+    public Row deserialize(byte[] messageKey, byte[] message, String topic, int partition, long offset) throws IOException {
+        try {
+            GeneratedMessageV3 proto = (GeneratedMessageV3) getProtoParser().invoke(null, message);
+            return addTimestampFieldToRow(getRow(proto), proto);
+        } catch (ReflectiveOperationException e) {
+            throw new ProtoDeserializationExcpetion(e);
+        }
+    }
+
+    private Row addTimestampFieldToRow(Row row, GeneratedMessageV3 proto) {
+        Descriptors.FieldDescriptor fieldDescriptor = proto.getDescriptorForType().getFields().get(timestampFieldIndex);
+
+        Row finalRecord = new Row(row.getArity() + 1);
+        for (int fieldIndex = 0; fieldIndex < row.getArity(); fieldIndex++) {
+            finalRecord.setField(fieldIndex, row.getField(fieldIndex));
+        }
+        Row timestampRow = getRow((GeneratedMessageV3) proto.getField(fieldDescriptor));
+        long timestampSeconds = (long) timestampRow.getField(0);
+        long timestampNanos = (int) timestampRow.getField(1);
+
+        finalRecord.setField(finalRecord.getArity() - 1, Timestamp.from(Instant.ofEpochSecond(timestampSeconds, timestampNanos)));
+        return finalRecord;
+    }
+
+    @Override
+    public boolean isEndOfStream(Row nextElement) {
+        return false;
+    }
+
+    @Override
+    public TypeInformation<Row> getProducedType() {
+        return protoType.getRowType();
+    }
 }
--- src/test/java/com/gojek/daggers/KafkaProtoStreamingTableSourceTest.java
+++ src/test/java/com/gojek/daggers/KafkaProtoStreamingTableSourceTest.java
@@ -1,14 +1,10 @@
 package com.gojek.daggers;
 
 import org.apache.flink.api.java.typeutils.RowTypeInfo;
-import org.apache.flink.configuration.Configuration;
-import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.AssignerWithPeriodicWatermarks;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumerBase;
-import org.apache.flink.types.Row;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,8 +12,6 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
@@ -45,29 +39,16 @@ public class KafkaProtoStreamingTableSourceTest {
   }
 
   @Test
-  public void shouldAssignInjectedTimestampExtractor() {
-    when(streamEnv.addSource(any())).thenReturn(dataStreamSource);
-    when(dataStreamSource.assignTimestampsAndWatermarks(any(AssignerWithPeriodicWatermarks.class))).thenReturn(dataStream);
-    RowTimestampExtractor expectedRowTimestampExtractor = new RowTimestampExtractor(1, new Configuration());
-
-    DataStream<Row> actualDataStream = new KafkaProtoStreamingTableSource(flinkConsumer, expectedRowTimestampExtractor, "window_timestamp").getDataStream(streamEnv);
-
-    assertEquals(dataStream, actualDataStream);
-    verify(streamEnv).addSource(flinkConsumer);
-    verify(dataStreamSource).assignTimestampsAndWatermarks(expectedRowTimestampExtractor);
-  }
-
-  @Test
   public void shouldUseInjectedNameForRowtimeAttribute() {
     String expectedRowTimeAttribute = "window_timestamp";
-    assertEquals(expectedRowTimeAttribute, new KafkaProtoStreamingTableSource(null, null, "window_timestamp").getRowtimeAttribute());
+    assertEquals(expectedRowTimeAttribute, new KafkaProtoStreamingTableSource(null, "window_timestamp", 0L).getRowtimeAttributeDescriptors().get(0).getAttributeName());
   }
 
   @Test
   public void shouldGiveBackRowTypesComingFromProtoType() {
     RowTypeInfo expectedRowType = new RowTypeInfo();
     when(flinkConsumer.getProducedType()).thenReturn(expectedRowType);
-    KafkaProtoStreamingTableSource kafkaProtoStreamingTableSource = new KafkaProtoStreamingTableSource(flinkConsumer, null, "");
+    KafkaProtoStreamingTableSource kafkaProtoStreamingTableSource = new KafkaProtoStreamingTableSource(flinkConsumer, "", 0L);
 
     assertEquals(expectedRowType, kafkaProtoStreamingTableSource.getReturnType());
   }
