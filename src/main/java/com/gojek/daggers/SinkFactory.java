abc402f71ae3b8eb8523592948ac595ab6bac585
Sumanth <sumanth.nakshatrithaya@go-jek.com>
2018-04-17 10:16:46 +0530

    Add log sink

--- src/main/java/com/gojek/daggers/SinkFactory.java
+++ src/main/java/com/gojek/daggers/SinkFactory.java
@@ -3,6 +3,7 @@ package com.gojek.daggers;
 import com.gojek.daggers.sink.InfluxDBFactoryWrapper;
 import com.gojek.daggers.sink.InfluxErrorHandler;
 import com.gojek.daggers.sink.InfluxRowSink;
+import com.gojek.daggers.sink.LogSink;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer010;
@@ -13,26 +14,30 @@ import org.apache.flink.types.Row;
 public class SinkFactory {
   public static SinkFunction<Row> getSinkFunction(Configuration configuration, String[] columnNames) {
     String sink = configuration.getString("SINK_TYPE", "influx");
-    if (sink.equals("kafka")) {
-      String outputProtoPrefix = configuration.getString("OUTPUT_PROTO_CLASS_PREFIX", "");
-      String outputBrokerList = configuration.getString("OUTPUT_KAFKA_BROKER", "");
-      String outputTopic = configuration.getString("OUTPUT_KAFKA_TOPIC", "");
+    switch (sink) {
+      case "kafka":
+        String outputProtoPrefix = configuration.getString("OUTPUT_PROTO_CLASS_PREFIX", "");
+        String outputBrokerList = configuration.getString("OUTPUT_KAFKA_BROKER", "");
+        String outputTopic = configuration.getString("OUTPUT_KAFKA_TOPIC", "");
 
-      ProtoSerializer protoSerializer = new ProtoSerializer(outputProtoPrefix, columnNames);
+        ProtoSerializer protoSerializer = new ProtoSerializer(outputProtoPrefix, columnNames);
 
-      // Use kafka partitioner
-      FlinkKafkaPartitioner partitioner = null;
+        // Use kafka partitioner
+        FlinkKafkaPartitioner partitioner = null;
 
-      FlinkKafkaProducer010<Row> flinkKafkaProducer = new FlinkKafkaProducer010<Row>(outputTopic,
-              protoSerializer,
-              FlinkKafkaProducerBase.getPropertiesFromBrokerList(outputBrokerList),
-              partitioner);
+        FlinkKafkaProducer010<Row> flinkKafkaProducer = new FlinkKafkaProducer010<Row>(outputTopic,
+                protoSerializer,
+                FlinkKafkaProducerBase.getPropertiesFromBrokerList(outputBrokerList),
+                partitioner);
 
-      // don't commit to source kafka if we can publish to output kafka
-      flinkKafkaProducer.setFlushOnCheckpoint(true);
+        // don't commit to source kafka if we can publish to output kafka
+        flinkKafkaProducer.setFlushOnCheckpoint(true);
 
-      return flinkKafkaProducer;
+        return flinkKafkaProducer;
+      case "log":
+        return new LogSink(columnNames);
+      default :
+        return new InfluxRowSink(new InfluxDBFactoryWrapper(), columnNames, configuration, new InfluxErrorHandler());
     }
-    return new InfluxRowSink(new InfluxDBFactoryWrapper(), columnNames, configuration, new InfluxErrorHandler());
   }
 }
---
+++ src/main/java/com/gojek/daggers/sink/LogSink.java
@@ -0,0 +1,28 @@
+package com.gojek.daggers.sink;
+
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+import org.apache.flink.types.Row;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LogSink extends RichSinkFunction<Row> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogSink.class.getName());
+
+    private String[] columnNames;
+
+    public LogSink(String[] columnNames) {
+        this.columnNames = columnNames;
+    }
+
+    @Override
+    public void invoke(Row row, Context context) throws Exception {
+        Map map = new HashMap<String, String>();
+        for (int i = 0; i < columnNames.length; i++) {
+            map.put(columnNames[i], row.getField(i).toString());
+        }
+        LOGGER.info(map.toString());
+    }
+}
