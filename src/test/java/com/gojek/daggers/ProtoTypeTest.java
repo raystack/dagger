c8ad00692fe5099ff1662a20efbc434970a0a812
Sumanth <sumanth.nakshatrithaya@go-jek.com>
2018-05-03 15:28:39 +0530

    [Sobia|Sumanth] Use Stencil

--- src/test/java/com/gojek/daggers/ProtoTypeTest.java
+++ src/test/java/com/gojek/daggers/ProtoTypeTest.java
@@ -5,128 +5,132 @@ import com.gojek.esb.login.LoginRequestMessage;
 import com.gojek.esb.participant.ParticipantLogMessage;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.java.typeutils.RowTypeInfo;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.table.api.Types;
 import org.apache.flink.types.Row;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.*;
 
 public class ProtoTypeTest {
 
-  @Test
-  public void shouldGiveAllColumnNamesOfProto() throws ClassNotFoundException {
-    ProtoType participantKeyProtoType = new ProtoType("com.gojek.esb.participant.ParticipantLogKey", "rowtime");
-    ProtoType bookingKeyProtoType = new ProtoType("com.gojek.esb.booking.BookingLogKey", "rowtime");
-
-    assertArrayEquals(
-        new String[]{"order_id", "status", "event_timestamp", "bid_id", "service_type", "participant_id", "audit"},
-        participantKeyProtoType.getFieldNames());
-
-    assertArrayEquals(
-        new String[]{"service_type", "order_number", "order_url", "status", "event_timestamp", "audit"},
-        bookingKeyProtoType.getFieldNames());
-  }
-
-  @Test
-  public void shouldThrowConfigurationExceptionWhenClassNotFound() {
-    try {
-      new ProtoType("com.gojek.esb.participant.ParticipantLogKey211", "rowtime");
-      fail();
-    } catch (DaggerConfigurationException exception) {
-      assertEquals(DescriptorStore.PROTO_CLASS_MISCONFIGURED_ERROR, exception.getMessage());
-      assertTrue(exception.getCause() instanceof ReflectiveOperationException);
+    @Before
+    public void before() {
+        DescriptorStore.load(new Configuration());
     }
-  }
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
 
-  @Test
-  public void shouldThrowConfigurationExceptionWhenClassIsNotProto() {
-    try {
-      new ProtoType(String.class.getName(), "rowtime");
-      fail();
-    } catch (DaggerConfigurationException exception) {
-      assertEquals(DescriptorStore.PROTO_CLASS_MISCONFIGURED_ERROR, exception.getMessage());
-      assertTrue(exception.getCause() instanceof ReflectiveOperationException);
+    @Test
+    public void shouldGiveAllColumnNamesOfProto() throws ClassNotFoundException {
+        ProtoType participantKeyProtoType = new ProtoType("com.gojek.esb.participant.ParticipantLogKey", "rowtime");
+        ProtoType bookingKeyProtoType = new ProtoType("com.gojek.esb.booking.BookingLogKey", "rowtime");
+
+        assertArrayEquals(
+                new String[]{"order_id", "status", "event_timestamp", "bid_id", "service_type", "participant_id", "audit"},
+                participantKeyProtoType.getFieldNames());
+
+        assertArrayEquals(
+                new String[]{"service_type", "order_number", "order_url", "status", "event_timestamp", "audit"},
+                bookingKeyProtoType.getFieldNames());
     }
-  }
 
-  @Test
-  public void shouldGiveSimpleMappedFlinkTypes() {
-    ProtoType participantMessageProtoType = new ProtoType(ParticipantLogMessage.class.getName(), "rowtime");
+    @Test
+    public void shouldThrowConfigurationExceptionWhenClassNotFound() {
+        expectedEx.expect(DaggerConfigurationException.class);
+        expectedEx.expectMessage(DescriptorStore.PROTO_CLASS_MISCONFIGURED_ERROR);
+        new ProtoType("com.gojek.esb.participant.ParticipantLogKey211", "rowtime");
+    }
 
-    TypeInformation[] fieldTypes = participantMessageProtoType.getFieldTypes();
+    @Test
+    public void shouldThrowConfigurationExceptionWhenClassIsNotProto() {
+        expectedEx.expect(DaggerConfigurationException.class);
+        expectedEx.expectMessage(DescriptorStore.PROTO_CLASS_MISCONFIGURED_ERROR);
+        new ProtoType(String.class.getName(), "rowtime");
 
-    assertEquals(Types.STRING(), fieldTypes[participantLogFieldIndex("order_id")]);
-    assertEquals(Types.INT(), fieldTypes[participantLogFieldIndex("customer_straight_line_distance")]);
-    assertEquals(Types.DOUBLE(), fieldTypes[participantLogFieldIndex("receive_delay")]);
-  }
+    }
 
-  @Test
-  public void shouldGiveSubRowMappedField() {
-    ProtoType participantMessageProtoType = new ProtoType(ParticipantLogMessage.class.getName(), "rowtime");
+    @Test
+    public void shouldGiveSimpleMappedFlinkTypes() {
+        ProtoType participantMessageProtoType = new ProtoType(ParticipantLogMessage.class.getName(), "rowtime");
 
-    TypeInformation[] fieldTypes = participantMessageProtoType.getFieldTypes();
+        TypeInformation[] fieldTypes = participantMessageProtoType.getFieldTypes();
 
-    TypeInformation<Row> expectedTimestampRow = Types.ROW(new String[]{"seconds", "nanos"},
-        new TypeInformation<?>[]{Types.LONG(), Types.INT()});
-    TypeInformation<Row> driverLocationRow = Types.ROW(new String[]{"latitude", "longitude", "altitude", "accuracy", "speed"},
-        new TypeInformation<?>[]{Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE()});
+        assertEquals(Types.STRING(), fieldTypes[participantLogFieldIndex("order_id")]);
+        assertEquals(Types.INT(), fieldTypes[participantLogFieldIndex("customer_straight_line_distance")]);
+        assertEquals(Types.DOUBLE(), fieldTypes[participantLogFieldIndex("receive_delay")]);
+    }
 
-    assertEquals(expectedTimestampRow, fieldTypes[participantLogFieldIndex("event_timestamp")]);
-    assertEquals(driverLocationRow, fieldTypes[participantLogFieldIndex("location")]);
-  }
+    @Test
+    public void shouldGiveSubRowMappedField() {
+        ProtoType participantMessageProtoType = new ProtoType(ParticipantLogMessage.class.getName(), "rowtime");
 
-  @Test
-  public void shouldProcessArrayForObjectData() {
-    ProtoType bookingLogMessageProtoType = new ProtoType(BookingLogMessage.class.getName(), "rowtime");
+        TypeInformation[] fieldTypes = participantMessageProtoType.getFieldTypes();
 
-    TypeInformation[] fieldTypes = bookingLogMessageProtoType.getFieldTypes();
+        TypeInformation<Row> expectedTimestampRow = Types.ROW(new String[]{"seconds", "nanos"},
+                new TypeInformation<?>[]{Types.LONG(), Types.INT()});
+        TypeInformation<Row> driverLocationRow = Types.ROW(new String[]{"latitude", "longitude", "altitude", "accuracy", "speed"},
+                new TypeInformation<?>[]{Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE(), Types.DOUBLE()});
 
-    TypeInformation<Row> locationType = Types.ROW(new String[]{"name", "address", "latitude", "longitude", "type",
-        "note", "place_id"}, new TypeInformation<?>[]{Types.STRING(), Types.STRING(), Types.DOUBLE(), Types.DOUBLE(),
-        Types.STRING(), Types.STRING(), Types.STRING()});
-    TypeInformation<?> expectedRoutesRow = Types.OBJECT_ARRAY(Types.ROW(new String[]{"start", "end",
-        "distance_in_kms", "estimated_duration", "route_order"}, new TypeInformation<?>[]{locationType, locationType,
-        Types.FLOAT(), Types.ROW(new String[]{"seconds", "nanos"}, new TypeInformation<?>[]{Types.LONG(),
-        Types.INT()}), Types.INT()}));
+        assertEquals(expectedTimestampRow, fieldTypes[participantLogFieldIndex("event_timestamp")]);
+        assertEquals(driverLocationRow, fieldTypes[participantLogFieldIndex("location")]);
+    }
 
-    assertEquals(expectedRoutesRow, fieldTypes[bookingLogFieldIndex("routes")]);
-  }
+    @Test
+    public void shouldProcessArrayForObjectData() {
+        ProtoType bookingLogMessageProtoType = new ProtoType(BookingLogMessage.class.getName(), "rowtime");
 
-  @Test
-  public void shouldProcessArrayForStringData() {
-    ProtoType loginRequestMessageProtoType = new ProtoType(LoginRequestMessage.class.getName(), "rowtime");
+        TypeInformation[] fieldTypes = bookingLogMessageProtoType.getFieldTypes();
 
-    TypeInformation[] fieldTypes = loginRequestMessageProtoType.getFieldTypes();
+        TypeInformation<Row> locationType = Types.ROW(new String[]{"name", "address", "latitude", "longitude", "type",
+                "note", "place_id"}, new TypeInformation<?>[]{Types.STRING(), Types.STRING(), Types.DOUBLE(), Types.DOUBLE(),
+                Types.STRING(), Types.STRING(), Types.STRING()});
+        TypeInformation<?> expectedRoutesRow = Types.OBJECT_ARRAY(Types.ROW(new String[]{"start", "end",
+                "distance_in_kms", "estimated_duration", "route_order"}, new TypeInformation<?>[]{locationType, locationType,
+                Types.FLOAT(), Types.ROW(new String[]{"seconds", "nanos"}, new TypeInformation<?>[]{Types.LONG(),
+                Types.INT()}), Types.INT()}));
 
-    TypeInformation<?> registeredDeviceType = Types.OBJECT_ARRAY(Types.STRING());
+        assertEquals(expectedRoutesRow, fieldTypes[bookingLogFieldIndex("routes")]);
+    }
 
-    assertEquals(registeredDeviceType, fieldTypes[loginRequestFieldIndex("registered_device")]);
-  }
+    @Test
+    public void shouldProcessArrayForStringData() {
+        ProtoType loginRequestMessageProtoType = new ProtoType(LoginRequestMessage.class.getName(), "rowtime");
 
-  //Add test for array of primitive data type. Couldn't find a proto which uses array of primitive data type.
+        TypeInformation[] fieldTypes = loginRequestMessageProtoType.getFieldTypes();
 
-  @Test
-  public void shouldGiveNamesAndTypes() {
-    ProtoType participantMessageProtoType = new ProtoType(ParticipantLogMessage.class.getName(), "rowtime");
+        TypeInformation<?> registeredDeviceType = Types.OBJECT_ARRAY(Types.STRING());
 
-    RowTypeInfo rowType = (RowTypeInfo) participantMessageProtoType.getRowType();
+        assertEquals(registeredDeviceType, fieldTypes[loginRequestFieldIndex("registered_device")]);
+    }
 
-    assertEquals("order_id", rowType.getFieldNames()[0]);
-    assertEquals(Types.DOUBLE(), rowType.getFieldTypes()[participantLogFieldIndex("receive_delay")]);
+    //Add test for array of primitive data type. Couldn't find a proto which uses array of primitive data type.
 
-  }
+    @Test
+    public void shouldGiveNamesAndTypes() {
+        ProtoType participantMessageProtoType = new ProtoType(ParticipantLogMessage.class.getName(), "rowtime");
+
+        RowTypeInfo rowType = (RowTypeInfo) participantMessageProtoType.getRowType();
+
+        assertEquals("order_id", rowType.getFieldNames()[0]);
+        assertEquals(Types.DOUBLE(), rowType.getFieldTypes()[participantLogFieldIndex("receive_delay")]);
+
+    }
 
 
-  private int participantLogFieldIndex(String propertyName) {
-    return ParticipantLogMessage.getDescriptor().findFieldByName(propertyName).getIndex();
-  }
+    private int participantLogFieldIndex(String propertyName) {
+        return ParticipantLogMessage.getDescriptor().findFieldByName(propertyName).getIndex();
+    }
 
-  private int bookingLogFieldIndex(String propertyName) {
-    return BookingLogMessage.getDescriptor().findFieldByName(propertyName).getIndex();
-  }
+    private int bookingLogFieldIndex(String propertyName) {
+        return BookingLogMessage.getDescriptor().findFieldByName(propertyName).getIndex();
+    }
 
-  private int loginRequestFieldIndex(String propertyName) {
-    return LoginRequestMessage.getDescriptor().findFieldByName(propertyName).getIndex();
-  }
+    private int loginRequestFieldIndex(String propertyName) {
+        return LoginRequestMessage.getDescriptor().findFieldByName(propertyName).getIndex();
+    }
 
 }
