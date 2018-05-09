9bf72e016c1d04d6345e1fc9cae7b76e6f0d5a48
Sumanth <kn.sumanth@gmail.com>
2018-05-09 13:40:09 +0530

    Revert "[Sobia|Sumanth] Use Stencil"
    This reverts commit cb345982a9d282fc8deb620e141da8d1476bdff8.

--- src/main/java/com/gojek/daggers/DescriptorStore.java
+++ src/main/java/com/gojek/daggers/DescriptorStore.java
@@ -1,9 +1,6 @@
 package com.gojek.daggers;
 
-import com.gojek.de.stencil.StencilClient;
-import com.gojek.de.stencil.StencilClientFactory;
 import com.google.protobuf.Descriptors;
-import org.apache.flink.configuration.Configuration;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -11,25 +8,19 @@ import java.util.Map;
 public class DescriptorStore {
     public static final String PROTO_CLASS_MISCONFIGURED_ERROR = "proto class is misconfigured";
     private static Map<String, Descriptors.Descriptor> descriptorMap = new HashMap<>();
-    private static StencilClient client;
-
-    public static void load(Configuration configuration) {
-        if ("true".equals(configuration.getString("ENABLE_STENCIL_URL", "false"))) {
-            client = StencilClientFactory.getClient(
-                    configuration.getString("STENCIL_URL", ""),
-                    configuration.toMap()
-            );
-        } else {
-            client = StencilClientFactory.getClient();
-        }
-        client.load();
-    }
 
     public static Descriptors.Descriptor get(String className) {
-        Descriptors.Descriptor dsc = client.get(className);
-        if (dsc == null) {
-            throw new DaggerConfigurationException(PROTO_CLASS_MISCONFIGURED_ERROR);
+        Descriptors.Descriptor desc = descriptorMap.get(className);
+        if (desc == null) {
+            try {
+                Class<?> protoClass = Class.forName(className);
+                desc = (Descriptors.Descriptor) protoClass.getMethod("getDescriptor")
+                        .invoke(null);
+                descriptorMap.put(className, desc);
+            } catch (ReflectiveOperationException e) {
+                throw new DaggerConfigurationException(PROTO_CLASS_MISCONFIGURED_ERROR, e);
+            }
         }
-        return dsc;
+        return desc;
     }
 }
