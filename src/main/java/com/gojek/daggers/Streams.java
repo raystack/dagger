82ef6e25164a3d66d188e82c0f4559483cc9c2c9
Sumanth <kn.sumanth@gmail.com>
2018-11-16 13:50:42 +0530

    Add feature ENABLE_PER_PARTITION_WATERMARK

--- src/main/java/com/gojek/daggers/Streams.java
+++ src/main/java/com/gojek/daggers/Streams.java
@@ -4,26 +4,33 @@ package com.gojek.daggers;
 import com.gojek.de.stencil.StencilClient;
 import com.google.gson.Gson;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor;
+import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011;
 import org.apache.flink.types.Row;
 
+import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
 public class Streams {
-
     private Map<String, FlinkKafkaConsumer011<Row>> streams = new HashMap<>();
     private LinkedHashMap<String, String> protoClassForTable = new LinkedHashMap<>();
     private static final String KAFKA_PREFIX = "kafka_consumer_config_";
     private Configuration configuration;
     private StencilClient stencilClient;
+    private boolean enablePerPartitionWatermark;
+    private long watermarkDelay;
 
-    public Streams(Configuration configuration, String rowTimeAttributeName, StencilClient stencilClient) {
+    public Streams(Configuration configuration, String rowTimeAttributeName, StencilClient stencilClient, boolean enablePerPartitionWatermark, long watermarkDelay) {
         this.configuration = configuration;
         this.stencilClient = stencilClient;
+        this.watermarkDelay = watermarkDelay;
+        this.enablePerPartitionWatermark = enablePerPartitionWatermark;
         String jsonArrayString = configuration.getString("STREAMS", "");
         Gson gson = new Gson();
         Map[] streamsConfig = gson.fromJson(jsonArrayString, Map[].class);
@@ -45,7 +52,20 @@ public class Streams {
                 .filter(e -> e.getKey().toLowerCase().startsWith(KAFKA_PREFIX))
                 .forEach(e -> kafkaProps.setProperty(parseVarName(e.getKey(), KAFKA_PREFIX), e.getValue()));
 
-        return new FlinkKafkaConsumer011<>(Pattern.compile(topics), new ProtoDeserializer(protoClassName, timestampFieldIndex, rowTimeAttributeName, stencilClient), kafkaProps);
+        FlinkKafkaConsumer011 fc = new FlinkKafkaConsumer011<>(Pattern.compile(topics), new ProtoDeserializer(protoClassName, timestampFieldIndex, rowTimeAttributeName, stencilClient), kafkaProps);
+
+        // https://ci.apache.org/projects/flink/flink-docs-stable/dev/event_timestamps_watermarks.html#timestamps-per-kafka-partition
+        if (enablePerPartitionWatermark) {
+            fc.assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor<Row>(Time.of(watermarkDelay, TimeUnit.MILLISECONDS)) {
+                @Override
+                public long extractTimestamp(Row element) {
+                    int index = element.getArity() - 1;
+                    return ((Timestamp) element.getField(index)).getTime();
+                }
+            });
+        }
+
+        return fc;
     }
 
     public Map<String, FlinkKafkaConsumer011<Row>> getStreams() {
