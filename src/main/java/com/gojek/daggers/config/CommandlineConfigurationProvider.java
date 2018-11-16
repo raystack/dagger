fed28e79ab88820f2356b02b1191bcf87171019b
prakharmathur82 <prakharmathur82@gmail.com>
2018-11-16 10:52:29 +0530

    [Prakhar | Rasyid] Make 1.4 also decode request from base64

--- src/main/java/com/gojek/daggers/config/CommandlineConfigurationProvider.java
+++ src/main/java/com/gojek/daggers/config/CommandlineConfigurationProvider.java
@@ -1,21 +1,32 @@
 package com.gojek.daggers.config;
 
+import com.google.gson.Gson;
 import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.configuration.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Base64;
 
 public class CommandlineConfigurationProvider implements ConfigurationProvider {
 
-  private String[] args;
+    private String[] args;
 
-  public CommandlineConfigurationProvider(String[] args) {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommandlineConfigurationProvider.class.getName());
 
-    this.args = args;
-  }
+    public CommandlineConfigurationProvider(String[] args) {
+        this.args = args;
+    }
 
-  @Override
-  public Configuration get() {
-    System.out.println("params from " + CommandlineConfigurationProvider.class.getName());
-    ParameterTool.fromArgs(args).toMap().entrySet().stream().forEach(e -> System.out.println(e));
-    return ParameterTool.fromArgs(args).getConfiguration();
-  }
+    @Override
+    public Configuration get() {
+        LOGGER.info("params from " + CommandlineConfigurationProvider.class.getName());
+        if (args.length == 0) {
+            return ParameterTool.fromArgs(args).getConfiguration();
+        }
+        byte[] decodedArgs = Base64.getMimeDecoder().decode(args[0]);
+        String[] arrOfArgs = new Gson().fromJson(new String(decodedArgs), String[].class);
+        ParameterTool.fromArgs(arrOfArgs).toMap().entrySet().stream().forEach(e -> LOGGER.info(e.toString()));
+        return ParameterTool.fromArgs(arrOfArgs).getConfiguration();
+    }
 }
