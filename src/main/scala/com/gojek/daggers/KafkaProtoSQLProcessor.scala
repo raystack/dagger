3a93cf3d65b8f9f6fb6036157689a867deeb1efb
gauravsinghania <grvsinghania@gmail.com>
2019-02-22 15:48:42 +0530

    [Nikhil | Singhania] | Changes the checkpoint configuration

--- src/main/scala/com/gojek/daggers/KafkaProtoSQLProcessor.scala
+++ src/main/scala/com/gojek/daggers/KafkaProtoSQLProcessor.scala
@@ -21,66 +21,67 @@ import scala.collection.JavaConversions._
 object KafkaProtoSQLProcessor {
 
   def main(args: Array[String]) {
-
-    val configuration: Configuration = new ConfigurationProviderFactory(args).provider().get()
-
-    TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
-    val env = StreamExecutionEnvironment.getExecutionEnvironment
-    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
-    val parallelism = configuration.getInteger("PARALLELISM", 1)
-    env.setParallelism(parallelism)
-    val autoWatermarkInterval = configuration.getInteger("WATERMARK_INTERVAL_MS", 10000)
-    env.getConfig.setAutoWatermarkInterval(autoWatermarkInterval)
-
-    env.enableCheckpointing(configuration.getLong("CHECKPOINT_INTERVAL", 30000))
-    env.getCheckpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE)
-    env.getCheckpointConfig.setCheckpointTimeout(configuration.getLong("CHECKPOINT_TIMEOUT", 180000))
-    env.getCheckpointConfig.setMaxConcurrentCheckpoints(configuration.getInteger("MAX_CONCURRECT_CHECKPOINTS", 1))
-    env.getConfig.setGlobalJobParameters(configuration)
-
-    val enableRemoteStencil = configuration.getBoolean("ENABLE_STENCIL_URL", false)
-
-    val stencilUrls: util.List[String] = configuration.getString("STENCIL_URL", "").split(",").map(_.trim).toList
-
-    val stencilClient = if (!enableRemoteStencil) StencilClientFactory.getClient() else StencilClientFactory.getClient(
-      stencilUrls, configuration.toMap)
-
-    val rowTimeAttributeName = configuration.getString("ROWTIME_ATTRIBUTE_NAME", "")
-
-    val enablePerPartitionWatermark = configuration.getBoolean("ENABLE_PER_PARTITION_WATERMARK", false)
-
-    val watermarkDelay = configuration.getLong("WATERMARK_DELAY_MS", 10000)
-
-    val streams = new Streams(configuration, rowTimeAttributeName, stencilClient, enablePerPartitionWatermark, watermarkDelay)
-
-    val tableEnv = TableEnvironment.getTableEnvironment(env)
-
-    for ((tableName, kafkaConsumer) <- streams.getStreams) {
-      val tableSource: KafkaProtoStreamingTableSource = new KafkaProtoStreamingTableSource(
-        kafkaConsumer,
-        rowTimeAttributeName,
-        watermarkDelay,
-        enablePerPartitionWatermark
-      )
-      tableEnv.registerTableSource(tableName, tableSource)
-    }
-
-    tableEnv.registerFunction("S2Id", new S2Id())
-    tableEnv.registerFunction("GEOHASH", new GeoHash())
-    tableEnv.registerFunction("DistinctByCurrentStatus", new DistinctByCurrentStatus)
-    tableEnv.registerFunction("ElementAt", new ElementAt(streams.getProtos.entrySet().iterator().next().getValue, stencilClient))
-    tableEnv.registerFunction("ServiceArea", new ServiceArea())
-    tableEnv.registerFunction("ServiceAreaId", new ServiceAreaId())
-    tableEnv.registerFunction("DistinctCount", new DistinctCount())
-    tableEnv.registerFunction("Distance", new Distance())
-    tableEnv.registerFunction("AppBetaUsers", new AppBetaUsers())
-    tableEnv.registerFunction("KeyValue", new KeyValue())
-    val redisServer = configuration.getString("REDIS_SERVER", "localhost")
-    tableEnv.registerFunction("DartContains", DartContains.withRedisDataStore(new RedisConfig(redisServer)))
-    tableEnv.registerFunction("DartGet", DartGet.withRedisDataStore(new RedisConfig(redisServer)))
-    tableEnv.registerFunction("Features", new Features())
-
     try {
+
+      val configuration: Configuration = new ConfigurationProviderFactory(args).provider().get()
+
+      TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+      val env = StreamExecutionEnvironment.getExecutionEnvironment
+      env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+      val parallelism = configuration.getInteger("PARALLELISM", 1)
+      env.setParallelism(parallelism)
+      val autoWatermarkInterval = configuration.getInteger("WATERMARK_INTERVAL_MS", 10000)
+      env.getConfig.setAutoWatermarkInterval(autoWatermarkInterval)
+
+      env.enableCheckpointing(configuration.getLong("CHECKPOINT_INTERVAL", 30000))
+      env.getCheckpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE)
+      env.getCheckpointConfig.setCheckpointTimeout(configuration.getLong("CHECKPOINT_TIMEOUT", 900000))
+      env.getCheckpointConfig.setMinPauseBetweenCheckpoints(config.getLong("CHECKPOINT_MIN_PAUSE", 5000))
+      env.getCheckpointConfig.setMaxConcurrentCheckpoints(configuration.getInteger("MAX_CONCURRECT_CHECKPOINTS", 1))
+      env.getConfig.setGlobalJobParameters(configuration)
+
+      val enableRemoteStencil = configuration.getBoolean("ENABLE_STENCIL_URL", false)
+
+      val stencilUrls: util.List[String] = configuration.getString("STENCIL_URL", "").split(",").map(_.trim).toList
+
+      val stencilClient = if (!enableRemoteStencil) StencilClientFactory.getClient() else StencilClientFactory.getClient(
+        stencilUrls, configuration.toMap)
+
+      val rowTimeAttributeName = configuration.getString("ROWTIME_ATTRIBUTE_NAME", "")
+
+      val enablePerPartitionWatermark = configuration.getBoolean("ENABLE_PER_PARTITION_WATERMARK", false)
+
+      val watermarkDelay = configuration.getLong("WATERMARK_DELAY_MS", 10000)
+
+      val streams = new Streams(configuration, rowTimeAttributeName, stencilClient, enablePerPartitionWatermark, watermarkDelay)
+
+      val tableEnv = TableEnvironment.getTableEnvironment(env)
+
+      for ((tableName, kafkaConsumer) <- streams.getStreams) {
+        val tableSource: KafkaProtoStreamingTableSource = new KafkaProtoStreamingTableSource(
+          kafkaConsumer,
+          rowTimeAttributeName,
+          watermarkDelay,
+          enablePerPartitionWatermark
+        )
+        tableEnv.registerTableSource(tableName, tableSource)
+      }
+
+      tableEnv.registerFunction("S2Id", new S2Id())
+      tableEnv.registerFunction("GEOHASH", new GeoHash())
+      tableEnv.registerFunction("DistinctByCurrentStatus", new DistinctByCurrentStatus)
+      tableEnv.registerFunction("ElementAt", new ElementAt(streams.getProtos.entrySet().iterator().next().getValue, stencilClient))
+      tableEnv.registerFunction("ServiceArea", new ServiceArea())
+      tableEnv.registerFunction("ServiceAreaId", new ServiceAreaId())
+      tableEnv.registerFunction("DistinctCount", new DistinctCount())
+      tableEnv.registerFunction("Distance", new Distance())
+      tableEnv.registerFunction("AppBetaUsers", new AppBetaUsers())
+      tableEnv.registerFunction("KeyValue", new KeyValue())
+      val redisServer = configuration.getString("REDIS_SERVER", "localhost")
+      tableEnv.registerFunction("DartContains", DartContains.withRedisDataStore(new RedisConfig(redisServer)))
+      tableEnv.registerFunction("DartGet", DartGet.withRedisDataStore(new RedisConfig(redisServer)))
+      tableEnv.registerFunction("Features", new Features())
+
       val resultTable2 = tableEnv.sqlQuery(configuration.getString("SQL_QUERY", ""))
 
       resultTable2.toAppendStream[Row]
