2b41aeec8781a392206fde15cca7b6137d8f7f52
sravankorumilli <sravankorumilli@gmail.com>
2019-04-15 21:59:37 +0530

    [Sravan] use interfaces instead of abstract classes

--- src/main/java/com/gojek/daggers/decorator/MapDecorator.java
+++ src/main/java/com/gojek/daggers/decorator/MapDecorator.java
@@ -4,10 +4,10 @@ import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.types.Row;
 
-public abstract class MapDecorator implements MapFunction<Row, Row>, StreamDecorator {
+public interface MapDecorator extends MapFunction<Row, Row>, StreamDecorator {
 
     @Override
-    public DataStream<Row> decorate(DataStream<Row> inputStream) {
+    default DataStream<Row> decorate(DataStream<Row> inputStream) {
         return inputStream.map(this::map);
     }
 
