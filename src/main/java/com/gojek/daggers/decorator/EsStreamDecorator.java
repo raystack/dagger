d3b2aa91ccf246eb8947005da95c42d2aff8dccb
sravankorumilli <sravankorumilli@gmail.com>
2019-04-15 21:47:09 +0530

    [Sravan] use AsyncDecorator type to define the contract explicitly

--- src/main/java/com/gojek/daggers/decorator/EsStreamDecorator.java
+++ src/main/java/com/gojek/daggers/decorator/EsStreamDecorator.java
@@ -2,24 +2,18 @@ package com.gojek.daggers.decorator;
 
 import com.gojek.daggers.async.connector.ESAsyncConnector;
 import com.gojek.de.stencil.StencilClient;
-import com.timgroup.statsd.StatsDClient;
-import org.apache.flink.streaming.api.datastream.AsyncDataStream;
-import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.types.Row;
+import org.apache.flink.streaming.api.functions.async.AsyncFunction;
 
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
-public class EsStreamDecorator implements StreamDecorator {
+public class EsStreamDecorator extends AsyncDecorator {
     private Map<String, String> configuration;
-//    private StatsDClient statsDClient;
     private StencilClient stencilClient;
     private Integer asyncIOCapacity;
     private Integer fieldIndex;
 
-    public EsStreamDecorator(Map<String, String> configuration, StencilClient stencilClient, Integer asyncIOCapacity, Integer fieldIndex) {
+    EsStreamDecorator(Map<String, String> configuration, StencilClient stencilClient, Integer asyncIOCapacity, Integer fieldIndex) {
         this.configuration = configuration;
-//        this.statsDClient = statsDClient;
         this.stencilClient = stencilClient;
         this.asyncIOCapacity = asyncIOCapacity;
         this.fieldIndex = fieldIndex;
@@ -32,15 +26,18 @@ public class EsStreamDecorator implements StreamDecorator {
     }
 
     @Override
-    public DataStream<Row> decorate(DataStream<Row> inputStream) {
-        if (!canDecorate())
-            return inputStream;
-        Integer streamTimeout = getIntegerConfig(configuration, "stream_timeout");
-        ESAsyncConnector esConnector = new ESAsyncConnector(fieldIndex, configuration, stencilClient);
-        return AsyncDataStream.orderedWait(inputStream, esConnector, streamTimeout, TimeUnit.MILLISECONDS, asyncIOCapacity);
+    public Integer getAsyncIOCapacity() {
+        return asyncIOCapacity;
     }
 
-    private Integer getIntegerConfig(Map<String, String> fieldConfiguration, String key) {
-        return Integer.valueOf(fieldConfiguration.get(key));
+    @Override
+    public AsyncFunction getAsyncFunction() {
+        return new ESAsyncConnector(fieldIndex, configuration, stencilClient);
     }
+
+    @Override
+    public Integer getStreamTimeout() {
+        return Integer.valueOf(configuration.get("stream_timeout"));
+    }
+
 }
