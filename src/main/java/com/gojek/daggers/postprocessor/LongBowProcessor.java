dcde704a03b31ddec66086a45133db87c8dd0c68
prakharmathur82 <prakharmathur82@gmail.com>
2019-05-23 11:04:11 +0530

    [Prakhar | Singhania][GOD-197] Refactor Stream manager and entry class

--- src/main/java/com/gojek/daggers/postprocessor/LongBowProcessor.java
+++ src/main/java/com/gojek/daggers/postprocessor/LongBowProcessor.java
@@ -2,20 +2,33 @@ package com.gojek.daggers.postprocessor;
 
 import com.gojek.daggers.StreamInfo;
 import com.gojek.daggers.longbow.LongBowReader;
-import com.gojek.daggers.longbow.LongBowWriter;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.AsyncDataStream;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.functions.async.AsyncFunction;
 import org.apache.flink.types.Row;
 
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 public class LongBowProcessor implements PostProcessor {
 
+    private HashMap<String, Integer> columnIndexMap;
+    private Configuration configuration;
+
+    public LongBowProcessor(Configuration configuration, String[] columnNames) {
+        this.configuration = configuration;
+        this.columnIndexMap = new HashMap<>();
+        for (int i = 0; i < columnNames.length; i++) {
+            this.columnIndexMap.put(columnNames[i], i);
+        }
+    }
+
+
     @Override
     public StreamInfo process(StreamInfo streamInfo) {
-        DataStream<Row> writeStream = AsyncDataStream.orderedWait(streamInfo.getDataStream(), (AsyncFunction) new LongBowWriter(), 5000, TimeUnit.MILLISECONDS, 40);
-        DataStream<Row> readStream = AsyncDataStream.orderedWait(writeStream, (AsyncFunction) new LongBowReader(), 5000, TimeUnit.MILLISECONDS, 40);
+//        DataStream<Row> writeStream = AsyncDataStream.orderedWait(streamInfo.getDataStream(), (AsyncFunction) new LongBowWriter(), 5000, TimeUnit.MILLISECONDS, 40);
+        DataStream<Row> readStream = AsyncDataStream.orderedWait(streamInfo.getDataStream(), (AsyncFunction) new LongBowReader(configuration, columnIndexMap), 5000, TimeUnit.MILLISECONDS, 40);
         return new StreamInfo(readStream, new String[1]);
     }
 }
--- src/main/java/com/gojek/daggers/postprocessor/PostProcessorFactory.java
+++ src/main/java/com/gojek/daggers/postprocessor/PostProcessorFactory.java
@@ -3,6 +3,7 @@ package com.gojek.daggers.postprocessor;
 import com.gojek.de.stencil.StencilClient;
 import org.apache.flink.configuration.Configuration;
 
+import java.util.Arrays;
 import java.util.Optional;
 
 import static com.gojek.daggers.Constants.ASYNC_IO_ENABLED_DEFAULT;
@@ -12,6 +13,8 @@ public class PostProcessorFactory {
     public static Optional<PostProcessor> getPostProcessor(Configuration configuration, StencilClient stencilClient, String[] columnNames) {
         if (configuration.getBoolean(ASYNC_IO_ENABLED_KEY, ASYNC_IO_ENABLED_DEFAULT))
             return Optional.of(new AshikoProcessor(configuration, stencilClient));
+        if(Arrays.asList(columnNames).contains("longbow_key"))
+            return Optional.of(new LongBowProcessor(configuration, columnNames));
         return Optional.empty();
     }
 }
