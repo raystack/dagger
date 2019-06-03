7c69f2b0ba21cefc577cae9551304a6698194040
gauravsinghania <grvsinghania@gmail.com>
2019-06-03 11:59:50 +0530

    [Singhania] | Changes statsmanager to register all aspects by default

---
+++ src/main/java/com/gojek/daggers/async/metric/AspectType.java
@@ -0,0 +1,6 @@
+package com.gojek.daggers.async.metric;
+
+public enum AspectType {
+    Histogram,
+    Metric
+}
--- src/main/java/com/gojek/daggers/async/metric/Aspects.java
+++ src/main/java/com/gojek/daggers/async/metric/Aspects.java
@@ -1,32 +1,40 @@
 package com.gojek.daggers.async.metric;
 
+import static com.gojek.daggers.async.metric.AspectType.*;
+
 public enum Aspects {
-    DOCUMENT_FOUND("documentFound"),
-    ERROR_PARSING_RESPONSE("parseErrors"),
-    SUCCESS_RESPONSE_TIME("successResponseTime"),
-    FAILURES_ON_ES_RESPONSE_TIME("failedOnESResponseTime"),
-    FAILURES_ON_ES("failuresOnES5XX"),
-    DOCUMENT_NOT_FOUND_ON_ES("documentNotFoundOnES404"),
-    REQUEST_ERROR("requestError"),
-    REQUEST_ERRORS_RESPONSE_TIME("requestErrorsResponseTime"),
-    OTHER_ERRORS("otherErrors"),
-    OTHER_ERRORS_RESPONSE_TIME("otherErrorsResponseTime"),
-    TOTAL_ES_CALLS("totalESCalls"),
-    TOTAL_FAILED_REQUESTS("totalFailures"),
-    EMPTY_INPUT("emptyInput"),
-    ERROR_READING_RESPONSE("errorReadingResponse"),
-    OTHER_ERRORS_PROCESSING_RESPONSE("otherErrorsProcessingResponse"),
-    FAILURES_ON_BIGTABLE_WRITE_DOCUMENT("failedOnBigtableWriteDocument"),
-    FAILURES_ON_BIGTABLE_CREATE_TABLE("failedOnBigtableCreateTable"),
-    TIMEOUTS("timeouts");
+    DOCUMENT_FOUND("documentFound",Metric),
+    ERROR_PARSING_RESPONSE("parseErrors", Metric),
+    SUCCESS_RESPONSE_TIME("successResponseTime", Histogram),
+    FAILURES_ON_ES_RESPONSE_TIME("failedOnESResponseTime", Histogram),
+    FAILURES_ON_ES("failuresOnES5XX",Metric),
+    DOCUMENT_NOT_FOUND_ON_ES("documentNotFoundOnES404",Metric),
+    REQUEST_ERROR("requestError",Metric),
+    REQUEST_ERRORS_RESPONSE_TIME("requestErrorsResponseTime", Histogram),
+    OTHER_ERRORS("otherErrors",Metric),
+    OTHER_ERRORS_RESPONSE_TIME("otherErrorsResponseTime", Histogram),
+    TOTAL_ES_CALLS("totalESCalls",Metric),
+    TOTAL_FAILED_REQUESTS("totalFailures",Metric),
+    EMPTY_INPUT("emptyInput",Metric),
+    ERROR_READING_RESPONSE("errorReadingResponse",Metric),
+    OTHER_ERRORS_PROCESSING_RESPONSE("otherErrorsProcessingResponse",Metric),
+    FAILURES_ON_BIGTABLE_WRITE_DOCUMENT("failedOnBigtableWriteDocument",Metric),
+    FAILURES_ON_BIGTABLE_CREATE_TABLE("failedOnBigtableCreateTable",Metric),
+    TIMEOUTS("timeouts",Metric);
 
     private String value;
+    private AspectType aspectType;
 
-    Aspects(String value) {
+    Aspects(String value, AspectType aspectType) {
         this.value = value;
+        this.aspectType = aspectType;
     }
 
     public String getValue() {
         return value;
+    }
+
+    public AspectType getAspectType() {
+        return aspectType;
     }
 }
