11e7b39a37cc00fcd62e31bcb1062c96c0e1f151
gauravsinghania <grvsinghania@gmail.com>
2019-06-03 11:59:50 +0530

    [Singhania] | Changes statsmanager to register all aspects by default

--- src/main/java/com/gojek/daggers/async/metric/StatsManager.java
+++ src/main/java/com/gojek/daggers/async/metric/StatsManager.java
@@ -31,37 +31,17 @@ public class StatsManager {
     public void register() {
         if (enabled) {
             MetricGroup metricGroup = runtimeContext.getMetricGroup().addGroup(groupName);
-            registerHistograms(metricGroup);
-            registerMeters(metricGroup);
+            register(metricGroup);
         }
     }
 
-    private void registerHistograms(MetricGroup metricGroup) {
-        histogramMap.put(SUCCESS_RESPONSE_TIME, metricGroup
-                .histogram(SUCCESS_RESPONSE_TIME.getValue(), new DropwizardHistogramWrapper(getHistogram())));
-        histogramMap.put(OTHER_ERRORS_RESPONSE_TIME, metricGroup
-                .histogram(OTHER_ERRORS_RESPONSE_TIME.getValue(), new DropwizardHistogramWrapper(getHistogram())));
-        histogramMap.put(REQUEST_ERRORS_RESPONSE_TIME, metricGroup
-                .histogram(REQUEST_ERRORS_RESPONSE_TIME.getValue(), new DropwizardHistogramWrapper(getHistogram())));
-        histogramMap.put(FAILURES_ON_ES_RESPONSE_TIME, metricGroup
-                .histogram(FAILURES_ON_ES_RESPONSE_TIME.getValue(), new DropwizardHistogramWrapper(getHistogram())));
-    }
-
-    private void registerMeters(MetricGroup metricGroup) {
-        meterMap.put(TOTAL_ES_CALLS, metricGroup.meter(TOTAL_ES_CALLS.getValue(), new DropwizardMeterWrapper(new com.codahale.metrics.Meter())));
-        meterMap.put(DOCUMENT_FOUND, metricGroup.meter(DOCUMENT_FOUND.getValue(), new DropwizardMeterWrapper(new com.codahale.metrics.Meter())));
-        meterMap.put(ERROR_PARSING_RESPONSE, metricGroup.meter(ERROR_PARSING_RESPONSE.getValue(), new DropwizardMeterWrapper(new com.codahale.metrics.Meter())));
-        meterMap.put(FAILURES_ON_ES, metricGroup.meter(FAILURES_ON_ES.getValue(), new DropwizardMeterWrapper(new com.codahale.metrics.Meter())));
-        meterMap.put(REQUEST_ERROR, metricGroup.meter(REQUEST_ERROR.getValue(), new DropwizardMeterWrapper(new com.codahale.metrics.Meter())));
-        meterMap.put(TOTAL_FAILED_REQUESTS, metricGroup.meter(TOTAL_FAILED_REQUESTS.getValue(), new DropwizardMeterWrapper(new com.codahale.metrics.Meter())));
-        meterMap.put(OTHER_ERRORS, metricGroup.meter(OTHER_ERRORS.getValue(), new DropwizardMeterWrapper(new com.codahale.metrics.Meter())));
-        meterMap.put(DOCUMENT_NOT_FOUND_ON_ES, metricGroup.meter(DOCUMENT_NOT_FOUND_ON_ES.getValue(), new DropwizardMeterWrapper(new com.codahale.metrics.Meter())));
-        meterMap.put(EMPTY_INPUT, metricGroup.meter(EMPTY_INPUT.getValue(), new DropwizardMeterWrapper(new com.codahale.metrics.Meter())));
-        meterMap.put(OTHER_ERRORS_PROCESSING_RESPONSE, metricGroup.meter(OTHER_ERRORS_PROCESSING_RESPONSE.getValue(), new DropwizardMeterWrapper(new com.codahale.metrics.Meter())));
-        meterMap.put(ERROR_READING_RESPONSE, metricGroup.meter(ERROR_READING_RESPONSE.getValue(), new DropwizardMeterWrapper(new com.codahale.metrics.Meter())));
-        meterMap.put(FAILURES_ON_BIGTABLE_WRITE_DOCUMENT, metricGroup.meter(FAILURES_ON_BIGTABLE_WRITE_DOCUMENT.getValue(), new DropwizardMeterWrapper(new com.codahale.metrics.Meter())));
-        meterMap.put(FAILURES_ON_BIGTABLE_CREATE_TABLE, metricGroup.meter(FAILURES_ON_BIGTABLE_CREATE_TABLE.getValue(), new DropwizardMeterWrapper(new com.codahale.metrics.Meter())));
-        meterMap.put(TIMEOUTS, metricGroup.meter(TIMEOUTS.getValue(), new DropwizardMeterWrapper(new com.codahale.metrics.Meter())));
+    private void register(MetricGroup metricGroup) {
+        for (Aspects aspect : values()) {
+            if (AspectType.Histogram.equals(aspect.getAspectType()))
+                histogramMap.put(aspect, metricGroup.histogram(aspect.getValue(), new DropwizardHistogramWrapper(getHistogram())));
+            if (AspectType.Metric.equals(aspect.getAspectType()))
+                meterMap.put(aspect, metricGroup.meter(aspect.getValue(), new DropwizardMeterWrapper(new com.codahale.metrics.Meter())));
+        }
     }
 
     private com.codahale.metrics.Histogram getHistogram() {
