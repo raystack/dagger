6b8fac8fdab5a4360500b45a7cc8952f638fda21
prakharmathur82 <prakharmathur82@gmail.com>
2019-08-29 15:31:32 +0530

    [Prakhar | Arujit] Make ashiko processor to iterate over async config instead of output proto

--- src/main/java/com/gojek/daggers/postprocessor/AshikoProcessor.java
+++ src/main/java/com/gojek/daggers/postprocessor/AshikoProcessor.java
@@ -10,6 +10,7 @@ import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.types.Row;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static com.gojek.daggers.Constants.*;
@@ -27,24 +28,23 @@ public class AshikoProcessor implements PostProcessor {
     @Override
     public StreamInfo process(StreamInfo streamInfo) {
         String asyncConfigurationString = configuration.getString(ASYNC_IO_KEY, "");
-        Map<String, Object> asyncConfig = new Gson().fromJson(asyncConfigurationString, Map.class);
+        Map<String, Object> asyncConfig = new Gson().fromJson(asyncConfigurationString, LinkedHashMap.class);
+
         Descriptors.Descriptor outputDescriptor = this.outputDescriptor();
         int size = outputDescriptor.getFields().size();
         String[] columnNames = new String[size];
         DataStream<Row> resultStream = streamInfo.getDataStream();
-        for (Descriptors.FieldDescriptor fieldDescriptor : outputDescriptor.getFields()) {
-            String fieldName = fieldDescriptor.getName();
-            if (!asyncConfig.containsKey(fieldName)) {
-                continue;
-            }
-            Map<String, String> fieldConfiguration = ((Map<String, String>) asyncConfig.get(fieldName));
+
+        for (String entry : asyncConfig.keySet()) {
+            Map<String, String> fieldConfiguration = ((Map<String, String>) asyncConfig.get(entry));
             int asyncIOCapacity = Integer.valueOf(fieldConfiguration.getOrDefault(ASYNC_IO_CAPACITY_KEY, ASYNC_IO_CAPACITY_DEFAULT));
-            int fieldIndex = fieldDescriptor.getIndex();
-            fieldConfiguration.put(FIELD_NAME_KEY, fieldName);
+            int fieldIndex = outputDescriptor.findFieldByName(entry).getIndex();
+            fieldConfiguration.put(FIELD_NAME_KEY, entry);
             StreamDecorator streamDecorator = StreamDecoratorFactory.getStreamDecorator(fieldConfiguration, fieldIndex, stencilClient, asyncIOCapacity, size);
-            columnNames[fieldIndex] = fieldName;
+            columnNames[fieldIndex] = entry;
             resultStream = streamDecorator.decorate(resultStream);
         }
+
         return new StreamInfo(resultStream, columnNames);
     }
 
