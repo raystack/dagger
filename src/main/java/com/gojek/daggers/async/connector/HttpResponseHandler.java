0d6ffe6209aa81fd428d8d695ed2a180c9852a1f
prakharmathur82 <prakharmathur82@gmail.com>
2019-09-10 16:37:27 +0530

    [Prakhar | Arujit] Handle failure scenarios in Http Response Handler

--- src/main/java/com/gojek/daggers/async/connector/HttpResponseHandler.java
+++ src/main/java/com/gojek/daggers/async/connector/HttpResponseHandler.java
@@ -1,57 +1,97 @@
 package com.gojek.daggers.async.connector;
 
+import com.gojek.daggers.async.metric.ExternalSourceAspects;
 import com.gojek.daggers.utils.RowMaker;
+import com.gojek.daggers.utils.stats.StatsManager;
 import com.google.protobuf.Descriptors;
 import com.jayway.jsonpath.JsonPath;
 import org.apache.flink.streaming.api.functions.async.ResultFuture;
 import org.apache.flink.types.Row;
 import org.asynchttpclient.AsyncCompletionHandler;
 import org.asynchttpclient.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
 
-import static com.gojek.daggers.Constants.EXTERNAL_SOURCE_OUTPUT_MAPPING_KEY;
-import static com.gojek.daggers.Constants.EXTERNAL_SOURCE_OUTPUT_MAPPING_PATH_KEY;
+import static com.gojek.daggers.Constants.*;
+import static com.gojek.daggers.async.metric.ExternalSourceAspects.*;
 
 public class HttpResponseHandler extends AsyncCompletionHandler<Object> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HttpResponseHandler.class.getName());
     private Row outputRow;
     private ResultFuture<Row> resultFuture;
     private Map<String, Object> configuration;
     private String[] columnNames;
     private Descriptors.Descriptor descriptor;
+    private StatsManager statsManager;
 
     public HttpResponseHandler(Row outputRow, ResultFuture<Row> resultFuture, Map<String, Object> configuration,
-                               String[] columnNames, Descriptors.Descriptor descriptor) {
+                               String[] columnNames, Descriptors.Descriptor descriptor, StatsManager statsManager) {
         this.outputRow = outputRow;
         this.resultFuture = resultFuture;
         this.configuration = configuration;
         this.columnNames = columnNames;
         this.descriptor = descriptor;
+        this.statsManager = statsManager;
     }
 
     @Override
     public Object onCompleted(Response response) throws Exception {
-        if (response.getStatusCode() == 200 && response.hasResponseBody()) {
-            Map<String, Object> outputMappings = (Map) configuration.get(EXTERNAL_SOURCE_OUTPUT_MAPPING_KEY);
-            ArrayList<String> outputMappingKeys = new ArrayList<>(outputMappings.keySet());
-
-            outputMappingKeys.forEach(key -> {
-                Map<String, String> outputMappingKeyConfig = (Map) outputMappings.get(key);
-                Object value = JsonPath.parse(response.getResponseBody()).read(outputMappingKeyConfig.get(EXTERNAL_SOURCE_OUTPUT_MAPPING_PATH_KEY), Object.class);
-                Descriptors.FieldDescriptor fieldDescriptor = descriptor.findFieldByName(key);
-                Integer fieldIndex = Arrays.asList(columnNames).indexOf(key);
-                outputRow.setField(fieldIndex, RowMaker.fetchTypeAppropriateValue(value, fieldDescriptor));
-            });
-        }
-        resultFuture.complete(Collections.singleton(outputRow));
+        int statusCode = response.getStatusCode();
+        if (statusCode == 200)
+            successHandler(response);
+        else if (statusCode >= 400 && statusCode < 499) {
+            failureHandler(FAILURES_ON_HTTP_CALL_4XX, "Received status code : " + statusCode);
+        } else if (statusCode >= 500 && statusCode < 599) {
+            failureHandler(FAILURES_ON_HTTP_CALL_5XX, "Received status code : " + statusCode);
+        } else
+            failureHandler(FAILURES_ON_HTTP_CALL_OTHER_STATUS, "Received status code : " + statusCode);
         return response;
     }
 
     @Override
     public void onThrowable(Throwable t) {
-        super.onThrowable(t);
+        failureHandler(FAILURES_ON_HTTP_CALL_OTHER_ERRORS, t.getMessage());
     }
+
+    private void successHandler(Response response) {
+        Map<String, Object> outputMappings = getValidatedMapField(configuration, EXTERNAL_SOURCE_OUTPUT_MAPPING_KEY);
+        ArrayList<String> outputMappingKeys = new ArrayList<>(outputMappings.keySet());
+
+        outputMappingKeys.forEach(key -> {
+            Map<String, String> outputMappingKeyConfig = getValidatedMapField(outputMappings, key);
+            Object value = JsonPath.parse(response.getResponseBody()).read(outputMappingKeyConfig.get(EXTERNAL_SOURCE_OUTPUT_MAPPING_PATH_KEY), Object.class);
+            Descriptors.FieldDescriptor fieldDescriptor = descriptor.findFieldByName(key);
+            Integer fieldIndex = Arrays.asList(columnNames).indexOf(key);
+            outputRow.setField(fieldIndex, RowMaker.fetchTypeAppropriateValue(value, fieldDescriptor));
+        });
+        statsManager.markEvent(SUCCESS_RESPONSE);
+        resultFuture.complete(Collections.singleton(outputRow));
+    }
+
+    private void failureHandler(ExternalSourceAspects aspect, String logMessage) {
+        statsManager.markEvent(aspect);
+        LOGGER.error(logMessage);
+        if ((boolean) configuration.getOrDefault(EXTERNAL_SOURCE_FAIL_ON_ERRORS_KEY, EXTERNAL_SOURCE_FAIL_ON_ERRORS_DEFAULT))
+            throw new RuntimeException(logMessage);
+        resultFuture.complete(Collections.singleton(outputRow));
+    }
+
+    private Map getValidatedMapField(Map<String, Object> map, String key) {
+        Map resultMap;
+        Object value = map.get(key);
+        if (value == null)
+            throw new IllegalArgumentException(key + " config should not be empty");
+        try {
+            resultMap = (Map) value;
+        } catch (ClassCastException e) {
+            throw new IllegalArgumentException(key + " config should be passed as a map");
+        }
+        return resultMap;
+    }
+
 }
--- src/main/java/com/gojek/daggers/async/metric/ExternalSourceAspects.java
+++ src/main/java/com/gojek/daggers/async/metric/ExternalSourceAspects.java
@@ -7,8 +7,13 @@ import static com.gojek.daggers.utils.stats.AspectType.Metric;
 
 public enum ExternalSourceAspects implements Aspects {
     CLOSE_CONNECTION_ON_HTTP_CLIENT("closeConnectionOnHttpClient", Metric),
+    FAILURES_ON_HTTP_CALL_5XX("failuresOnHttp5XX", Metric),
+    FAILURES_ON_HTTP_CALL_4XX("failuresOnHttp4XX", Metric),
+    FAILURES_ON_HTTP_CALL_OTHER_STATUS("failuresOnHttpOtherStatus", Metric),
+    FAILURES_ON_HTTP_CALL_OTHER_ERRORS("failuresOnHttpOtherErrors", Metric),
     TOTAL_HTTP_CALLS("totalHttpCalls", Metric),
-    TIMEOUTS("timeouts", Metric);
+    TIMEOUTS("timeouts", Metric),
+    SUCCESS_RESPONSE("successResponse", Metric);
 
     private String value;
     private AspectType aspectType;
