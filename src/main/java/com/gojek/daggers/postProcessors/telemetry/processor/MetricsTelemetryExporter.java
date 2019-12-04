2f276d97ef17ee16a3be46a900b83b6f80107f1e
arujit <jamesbondukela@gmail.com>
2019-12-04 15:56:11 +0530

    [Prakhar | Arujit] Add Telemetry Metrics Exporter and telemetry post processor

---
+++ src/main/java/com/gojek/daggers/postProcessors/telemetry/processor/MetricsTelemetryExporter.java
@@ -0,0 +1,63 @@
+package com.gojek.daggers.postProcessors.telemetry.processor;
+
+import com.gojek.daggers.metrics.GaugeStatsManager;
+import com.gojek.daggers.metrics.TelemetryPublisher;
+import com.gojek.daggers.metrics.TelemetrySubscriber;
+import org.apache.flink.api.common.functions.RichMapFunction;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.types.Row;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.gojek.daggers.metrics.TelemetryAspects.values;
+
+public class MetricsTelemetryExporter extends RichMapFunction<Row, Row> implements TelemetrySubscriber {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MetricsTelemetryExporter.class.getName());
+    private GaugeStatsManager gaugeStatsManager;
+    private Integer gaugeValue = 1;
+    private Map<String, List<String>> metrics = new HashMap<>();
+
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        if (gaugeStatsManager == null) {
+            gaugeStatsManager = new GaugeStatsManager(getRuntimeContext(), true);
+        }
+        if (metrics != null) {
+            registerGroups(gaugeStatsManager);
+        }
+    }
+
+    @Override
+    public Row map(Row inputRow) throws Exception {
+        return inputRow;
+    }
+
+    private void registerGroups(GaugeStatsManager gaugeStatsManager) {
+        metrics.forEach((groupKey, groupValues) -> groupValues
+                .forEach(groupValue -> gaugeStatsManager.register(groupKey, groupValue, values(), gaugeValue)));
+        LOGGER.info("Sending Metrics: " + metrics);
+        metrics.clear();
+    }
+
+    @Override
+    public void updated(TelemetryPublisher publisher) {
+        mergeMetrics(publisher.getTelemetry());
+        if (gaugeStatsManager != null) {
+            registerGroups(gaugeStatsManager);
+        }
+    }
+
+    private void mergeMetrics(Map<String, List<String>> metricsFromPublisher) {
+        metricsFromPublisher.forEach((key, value) -> {
+                    if ((metrics.containsKey(key)))
+                        metrics.get(key).addAll(value);
+                    else
+                        metrics.putAll(metricsFromPublisher);
+                }
+        );
+    }
+}
