e3169ec422092188e883049db3758a3cdccde83e
MayurGubrele <mayur.gubrele@gmail.com>
2020-04-22 20:37:08 +0530

    | [Mayur] Adds pgAsyncConnector and response handler

--- src/test/java/com/gojek/daggers/postProcessors/PostProcessorConfigTest.java
+++ src/test/java/com/gojek/daggers/postProcessors/PostProcessorConfigTest.java
@@ -156,7 +156,7 @@ public class PostProcessorConfigTest {
         ArrayList<HttpSourceConfig> http = new ArrayList<>();
         ArrayList<EsSourceConfig> es = new ArrayList<>();
         ArrayList<PgSourceConfig> pg = new ArrayList<>();
-        pg.add(new PgSourceConfig("","","","","","","","", new HashMap<>()));
+        pg.add(new PgSourceConfig("","","","","","","","", "", "", "", "", "", ""));
         ExternalSourceConfig externalSourceConfig = new ExternalSourceConfig(http, es, pg);
         postProcessorConfig = new PostProcessorConfig(externalSourceConfig, null, null);
 
