178c4c57b30de5f197ee0f38cbc6a454a7084478
MayurGubrele <mayur.gubrele@gmail.com>
2020-04-22 20:37:08 +0530

    [DSTRM-221] | [Mayur] Adds pgAsyncConnector and response handler

--- src/main/java/com/gojek/daggers/postProcessors/external/pg/PgStreamDecorator.java
+++ src/main/java/com/gojek/daggers/postProcessors/external/pg/PgStreamDecorator.java
@@ -4,8 +4,11 @@ import com.gojek.daggers.core.StencilClientOrchestrator;
 import com.gojek.daggers.metrics.telemetry.TelemetrySubscriber;
 import com.gojek.daggers.postProcessors.common.ColumnNameManager;
 import com.gojek.daggers.postProcessors.external.common.StreamDecorator;
+import org.apache.flink.streaming.api.datastream.AsyncDataStream;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.types.Row;
+
+import java.util.concurrent.TimeUnit;
 
 public class PgStreamDecorator implements StreamDecorator {
     private final PgSourceConfig pgSourceConfig;
@@ -32,6 +35,8 @@ public class PgStreamDecorator implements StreamDecorator {
 
     @Override
     public DataStream<Row> decorate(DataStream<Row> inputStream) {
-        return null;
+        PgAsyncConnector pgAsyncConnector = new PgAsyncConnector(pgSourceConfig, stencilClientOrchestrator, columnNameManager, telemetryEnabled, shutDownPeriod);
+        pgAsyncConnector.notifySubscriber(telemetrySubscriber);
+        return AsyncDataStream.orderedWait(inputStream, pgAsyncConnector, pgSourceConfig.getStreamTimeout(), TimeUnit.MILLISECONDS, pgSourceConfig.getCapacity());
     }
 }
