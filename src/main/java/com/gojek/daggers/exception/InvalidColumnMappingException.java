d16b4183e403045c8fb78b2600794c213b978e28
Rasyid Hakim <muhammad.hakim@go-jek.com>
2020-04-30 23:13:20 +0530

    [Rasyid] Add cause for type mismatch and show REPEATED on mismatch

--- src/main/java/com/gojek/daggers/exception/InvalidColumnMappingException.java
+++ src/main/java/com/gojek/daggers/exception/InvalidColumnMappingException.java
@@ -4,4 +4,8 @@ public class InvalidColumnMappingException extends RuntimeException {
     public InvalidColumnMappingException(String message) {
         super(message);
     }
+
+    public InvalidColumnMappingException(String message, Throwable err) {
+        super(message, err);
+    }
 }
