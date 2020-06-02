5948361e03cdc94a36155a8febe62a0c19bb614b
Ankit Tiwari <ankit@ankit.tiwari@go-jek.com>
2020-06-02 12:23:42 +0530

    updating http integration test

--- src/intTest/java/com/gojek/daggers/postProcessors/external/http/HttpAsyncConnectorIntegrationTest.java
+++ src/intTest/java/com/gojek/daggers/postProcessors/external/http/HttpAsyncConnectorIntegrationTest.java
@@ -2,10 +2,15 @@ package com.gojek.daggers.postProcessors.external.http;
 
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import com.gojek.daggers.core.StencilClientOrchestrator;
+import com.gojek.daggers.core.StreamInfo;
 import com.gojek.daggers.postProcessors.PostProcessorConfig;
 import com.gojek.daggers.postProcessors.common.ColumnNameManager;
 import com.gojek.daggers.postProcessors.external.common.FetchOutputDecorator;
 import com.gojek.daggers.postProcessors.external.common.InitializationDecorator;
+import com.gojek.daggers.postProcessors.internal.InternalPostProcessor;
+import com.gojek.daggers.postProcessors.internal.InternalSourceConfig;
+import com.gojek.daggers.postProcessors.transfromers.TransformConfig;
+import com.gojek.daggers.postProcessors.transfromers.TransformProcessor;
 import org.apache.flink.configuration.Configuration;
 import org.apache.flink.runtime.testutils.MiniClusterResourceConfiguration;
 import org.apache.flink.streaming.api.datastream.AsyncDataStream;
@@ -16,12 +21,13 @@ import org.apache.flink.test.util.MiniClusterWithClientResource;
 import org.apache.flink.types.Row;
 import org.junit.*;
 
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class HttpAsyncConnectorIntegrationTest {
 
@@ -32,7 +38,8 @@ public class HttpAsyncConnectorIntegrationTest {
     private boolean telemetryEnabled;
     private long shutDownPeriod;
     private HttpSourceConfig httpSourceConfig;
-
+    private InternalSourceConfig internalSourceConfig;
+    private PostProcessorConfig postProcessorParsedConfig;
     @ClassRule
     public static MiniClusterWithClientResource flinkCluster =
             new MiniClusterWithClientResource(
@@ -46,6 +53,32 @@ public class HttpAsyncConnectorIntegrationTest {
         String[] inputColumnNames = new String[]{"order_id", "customer_id", "driver_id"};
         telemetryEnabled = false;
         shutDownPeriod = 0L;
+//        String postProcessorConfigString = "{\n" +
+//                "  \"external_source\": {\n" +
+//                "    \"http\": [\n" +
+//                "      {\n" +
+//                "        \"endpoint\": \"http://localhost:8089\",\n" +
+//                "        \"verb\": \"get\",\n" +
+//                "        \"request_pattern\": \"/customer/%s\",\n" +
+//                "        \"request_variables\": \"customer_id\",\n" +
+//                "        \"stream_timeout\": \"5000\",\n" +
+//                "        \"connect_timeout\": \"5000\",\n" +
+//                "        \"fail_on_errors\": \"false\", \n" +
+//                "        \"capacity\": \"30\",\n" +
+//                "        \"headers\": {\n" +
+//                "          \"content-type\": \"application/json\"\n" +
+//                "        },\n" +
+//                "        \"type\": \"com.gojek.esb.aggregate.surge.SurgeFactorLogMessage\", \n" +
+//                "        \"output_mapping\": {\n" +
+//                "          \"surge_factor\": {\n" +
+//                "            \"path\": \"$.data\"\n" +
+//                "          }\n" +
+//                "        }\n" +
+//                "      }\n" +
+//                "    ]\n" +
+//                "  }\n" +
+//                "}";
+
         String postProcessorConfigString = "{\n" +
                 "  \"external_source\": {\n" +
                 "    \"http\": [\n" +
@@ -69,9 +102,26 @@ public class HttpAsyncConnectorIntegrationTest {
                 "        }\n" +
                 "      }\n" +
                 "    ]\n" +
-                "  }\n" +
+                "  },\n" +
+                "    \"internal_source\": [\n" +
+                "       {"  +
+                "       \"output_field\": \"event_timestamp\", \n" +
+                "       \"type\": \"function\",\n"  +
+                "       \"value\": \"CURRENT_TIMESTAMP\"\n" +
+                "       }"  +
+                "   ]"  +
+                ",\n" +
+                " \"transformers\": [" +
+                "    {" +
+                "   \"transformation_arguments\": { \n" +
+                "   \"keyColumnName\": \"surge_factor\", \n" +
+                "    \"valueColumnName\": \"features\" \n"+
+                "   }," +
+                "   \"transformation_class\": \"com.gojek.dagger.transformer.FeatureTransformer\" \n" +
+                "   } \n"   +
+                "   ]   \n" +
                 "}";
-        PostProcessorConfig postProcessorParsedConfig = PostProcessorConfig.parse(postProcessorConfigString);
+        postProcessorParsedConfig = PostProcessorConfig.parse(postProcessorConfigString);
         httpSourceConfig = postProcessorParsedConfig.getExternalSource().getHttpConfig().get(0);
         columnNameManager = new ColumnNameManager(inputColumnNames, postProcessorParsedConfig.getOutputColumnNames());
     }
@@ -105,11 +155,89 @@ public class HttpAsyncConnectorIntegrationTest {
                 columnNameManager, null, telemetryEnabled, shutDownPeriod, null, null);
 
         DataStream<Row> outputStream = AsyncDataStream.orderedWait(dataStream, httpAsyncConnector, httpSourceConfig.getStreamTimeout(), TimeUnit.MILLISECONDS, httpSourceConfig.getCapacity());
-
         outputStream.map(new FetchOutputDecorator()).addSink(new CollectSink());
         env.execute();
         assertEquals(23.33F, (Float) CollectSink.values.get(0).getField(0), 0.0F);
     }
+
+
+    @Test
+    public void shouldPopulateFieldFromHTTPGetApiOnSuccessResponseWithInternalSource() throws Exception {
+
+//        withInternal();
+
+        stubFor(get(urlEqualTo("/customer/123"))
+                .withHeader("content-type", equalTo("application/json"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{data: 23.33}")));
+
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        CollectSink.values.clear();
+
+        Row inputData = new Row(3);
+        inputData.setField(0, "RB-123");
+        inputData.setField(1, "123");
+        inputData.setField(2, "456");
+
+        DataStream<Row> dataStream = env.fromElements(Row.class, inputData).map(new InitializationDecorator(columnNameManager));
+
+        HttpAsyncConnector httpAsyncConnector = new HttpAsyncConnector(httpSourceConfig, "", new StencilClientOrchestrator(new Configuration()),
+                columnNameManager, null, telemetryEnabled, shutDownPeriod, null, null);
+
+        DataStream<Row> outputStream = AsyncDataStream.orderedWait(dataStream, httpAsyncConnector, httpSourceConfig.getStreamTimeout(), TimeUnit.MILLISECONDS, httpSourceConfig.getCapacity());
+
+        InternalPostProcessor internalPostProcessor = new InternalPostProcessor(postProcessorParsedConfig);
+
+        StreamInfo streamInfo = new StreamInfo(outputStream, columnNameManager.getOutputColumnNames());
+
+        internalPostProcessor.process(streamInfo).getDataStream().map(new FetchOutputDecorator()).addSink(new CollectSink());
+        env.execute();
+
+        assertEquals(23.33F, (Float) CollectSink.values.get(0).getField(0), 0.0F);
+        assertTrue(CollectSink.values.get(0).getField(1) instanceof Timestamp);
+    }
+
+
+//    @Test
+//    public void shouldPopulateFieldFromHTTPGetApiOnSuccessResponseWithTransformer() throws Exception {
+//
+//        stubFor(get(urlEqualTo("/customer/123"))
+//                .withHeader("content-type", equalTo("application/json"))
+//                .willReturn(aResponse()
+//                        .withStatus(200)
+//                        .withHeader("Content-Type", "application/json")
+//                        .withBody("{data: 23.33}")));
+//
+//        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+//        CollectSink.values.clear();
+//
+//        Row inputData = new Row(3);
+//        inputData.setField(0, "RB-123");
+//        inputData.setField(1, "123");
+//        inputData.setField(2, "456");
+//
+//        DataStream<Row> dataStream = env.fromElements(Row.class, inputData).map(new InitializationDecorator(columnNameManager));
+//
+//        HttpAsyncConnector httpAsyncConnector = new HttpAsyncConnector(httpSourceConfig, "", new StencilClientOrchestrator(new Configuration()),
+//                columnNameManager, null, telemetryEnabled, shutDownPeriod, null, null);
+//
+//        DataStream<Row> outputStream = AsyncDataStream.orderedWait(dataStream, httpAsyncConnector, httpSourceConfig.getStreamTimeout(), TimeUnit.MILLISECONDS, httpSourceConfig.getCapacity());
+//
+//        outputStream.map(new FetchOutputDecorator());
+//
+//        TransformProcessor transformProcessor = new TransformProcessor(postProcessorParsedConfig.getTransformers());
+//
+//        StreamInfo streamInfo = new StreamInfo(outputStream, columnNameManager.getOutputColumnNames());
+//
+//        transformProcessor.process(streamInfo).getDataStream().addSink(new CollectSink());
+//        env.execute();
+//
+//        System.out.println(CollectSink.values.get(0));
+//        assertEquals(23.33F, (Float) CollectSink.values.get(0).getField(0), 0.0F);
+//    }
+
 
     private static class CollectSink implements SinkFunction<Row> {
 
