b891ddfafe9ec9229c5b60c26de1e195fe34f73f
prakharmathur82 <prakharmathur82@gmail.com>
2020-10-13 21:44:00 +0530

    [Prakhar] Use StreamInfo from flud and return types from FetchOutputDecorator

--- src/main/java/com/gojek/daggers/postProcessors/external/ExternalPostProcessor.java
+++ src/main/java/com/gojek/daggers/postProcessors/external/ExternalPostProcessor.java
@@ -1,6 +1,6 @@
 package com.gojek.daggers.postProcessors.external;
 
-import com.gojek.daggers.core.StreamInfo;
+import com.gojek.dagger.common.StreamInfo;
 import com.gojek.daggers.postProcessors.PostProcessorConfig;
 import com.gojek.daggers.postProcessors.common.PostProcessor;
 import com.gojek.daggers.postProcessors.common.Validator;
