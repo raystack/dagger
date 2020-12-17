1272306ee271d3debd7c7c53e0c6ff44dbba4ee0
jesrypandawa <jesry.p.pandawa@gmail.com>
2020-12-18 03:22:11 +0700

    [DSTRM-508][Jesry]Enhance internal post-processor for mapping all input to output fields

--- src/main/java/com/gojek/daggers/postProcessors/internal/processor/sql/SqlInternalConfigProcessor.java
+++ src/main/java/com/gojek/daggers/postProcessors/internal/processor/sql/SqlInternalConfigProcessor.java
@@ -1,5 +1,6 @@
 package com.gojek.daggers.postProcessors.internal.processor.sql;
 
+import com.gojek.daggers.exception.InvalidConfigurationException;
 import com.gojek.daggers.postProcessors.common.ColumnNameManager;
 import com.gojek.daggers.postProcessors.external.common.RowManager;
 import com.gojek.daggers.postProcessors.internal.InternalSourceConfig;
@@ -7,9 +8,12 @@ import com.gojek.daggers.postProcessors.internal.processor.InternalConfigProcess
 
 import java.io.Serializable;
 
+import static com.gojek.daggers.utils.Constants.SQL_PATH_SELECT_ALL_CONFIG_VALUE;
+
 public class SqlInternalConfigProcessor implements InternalConfigProcessor, Serializable {
 
     public static final String SQL_CONFIG_HANDLER_TYPE = "sql";
+
     private ColumnNameManager columnNameManager;
     private SqlConfigTypePathParser sqlPathParser;
     private InternalSourceConfig internalSourceConfig;
@@ -27,10 +31,17 @@ public class SqlInternalConfigProcessor implements InternalConfigProcessor, Seri
 
     @Override
     public void process(RowManager rowManager) {
-        int outputFieldIndex = columnNameManager.getOutputIndex(internalSourceConfig.getOutputField());
-        if (outputFieldIndex != -1) {
-            Object inputData = sqlPathParser.getData(rowManager);
-            rowManager.setInOutput(outputFieldIndex, inputData);
+        if (SQL_PATH_SELECT_ALL_CONFIG_VALUE.equals(internalSourceConfig.getOutputField())) {
+            for (String columnName : columnNameManager.getOutputColumnNames()){
+                int inputFieldIndex = columnNameManager.getInputIndex(columnName);
+                rowManager.setInOutput(columnNameManager.getOutputIndex(columnName), rowManager.getFromInput(inputFieldIndex));
+            }
+        } else {
+            int outputFieldIndex = columnNameManager.getOutputIndex(internalSourceConfig.getOutputField());
+            if (outputFieldIndex != -1) {
+                Object inputData = sqlPathParser.getData(rowManager);
+                rowManager.setInOutput(outputFieldIndex, inputData);
+            }
         }
     }
 }
