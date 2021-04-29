85396762bccf0114188221bf3e0790dcb456d6d4
lavkesh <lavkesh51@gmail.com>
2021-04-29 11:54:26 +0800

    Metric change

--- dagger-core/src/main/java/io/odpf/dagger/core/metrics/aspects/LongbowReaderAspects.java
+++ dagger-core/src/main/java/io/odpf/dagger/core/metrics/aspects/LongbowReaderAspects.java
@@ -1,14 +1,14 @@
 package io.odpf.dagger.core.metrics.aspects;
 
 public enum LongbowReaderAspects implements Aspects {
-    TIMEOUTS_ON_READER("timeoutsOnReader", AspectType.Metric),
-    CLOSE_CONNECTION_ON_READER("closeConnectionOnReader", AspectType.Metric),
-    SUCCESS_ON_READ_DOCUMENT("successOnReadDocument", AspectType.Metric),
-    SUCCESS_ON_READ_DOCUMENT_RESPONSE_TIME("successOnReadDocumentResponseTime", AspectType.Histogram),
-    DOCUMENTS_READ_PER_SCAN("documentsReadPerScan", AspectType.Histogram),
-    FAILED_ON_READ_DOCUMENT("failedOnReadDocument", AspectType.Metric),
-    FAILED_ON_READ_DOCUMENT_RESPONSE_TIME("failedOnReadDocumentResponseTime", AspectType.Histogram),
-    FAILED_TO_READ_LAST_RECORD("failedToReadLastRecord", AspectType.Metric);
+    TIMEOUTS_ON_READER("timeouts_on_reader", AspectType.Metric),
+    CLOSE_CONNECTION_ON_READER("close_connection_on_reader", AspectType.Metric),
+    SUCCESS_ON_READ_DOCUMENT("success_on_read_document", AspectType.Metric),
+    SUCCESS_ON_READ_DOCUMENT_RESPONSE_TIME("success_on_read_document_response_time", AspectType.Histogram),
+    DOCUMENTS_READ_PER_SCAN("documents_read_per_scan", AspectType.Histogram),
+    FAILED_ON_READ_DOCUMENT("failed_on_read_document", AspectType.Metric),
+    FAILED_ON_READ_DOCUMENT_RESPONSE_TIME("failed_on_read_document_response_time", AspectType.Histogram),
+    FAILED_TO_READ_LAST_RECORD("failed_to_read_last_record", AspectType.Metric);
 
     private String value;
     private AspectType aspectType;
--- dagger-core/src/main/java/io/odpf/dagger/core/metrics/aspects/LongbowWriterAspects.java
+++ dagger-core/src/main/java/io/odpf/dagger/core/metrics/aspects/LongbowWriterAspects.java
@@ -1,16 +1,16 @@
 package io.odpf.dagger.core.metrics.aspects;
 
 public enum LongbowWriterAspects implements Aspects {
-    SUCCESS_ON_CREATE_BIGTABLE("successOnCreateBigTable", AspectType.Metric),
-    SUCCESS_ON_CREATE_BIGTABLE_RESPONSE_TIME("successOnCreateBigTableResponseTime", AspectType.Histogram),
-    FAILURES_ON_CREATE_BIGTABLE("failedOnCreateBigTable", AspectType.Metric),
-    FAILURES_ON_CREATE_BIGTABLE_RESPONSE_TIME("failedOnCreateBigTableResponseTime", AspectType.Histogram),
-    TIMEOUTS_ON_WRITER("timeoutsOnWriter", AspectType.Metric),
-    CLOSE_CONNECTION_ON_WRITER("closeConnectionOnWriter", AspectType.Metric),
-    SUCCESS_ON_WRITE_DOCUMENT("successOnWriteDocument", AspectType.Metric),
-    SUCCESS_ON_WRITE_DOCUMENT_RESPONSE_TIME("successOnWriteDocumentResponseTime", AspectType.Histogram),
-    FAILED_ON_WRITE_DOCUMENT("failedOnWriteDocument", AspectType.Metric),
-    FAILED_ON_WRITE_DOCUMENT_RESPONSE_TIME("failedOnWriteDocumentResponseTime", AspectType.Histogram);
+    SUCCESS_ON_CREATE_BIGTABLE("success_on_create_bigtable", AspectType.Metric),
+    SUCCESS_ON_CREATE_BIGTABLE_RESPONSE_TIME("success_on_create_bigtable_response_time", AspectType.Histogram),
+    FAILURES_ON_CREATE_BIGTABLE("failed_on_create_bigtable", AspectType.Metric),
+    FAILURES_ON_CREATE_BIGTABLE_RESPONSE_TIME("failed_on_create_bigtable_response_time", AspectType.Histogram),
+    TIMEOUTS_ON_WRITER("timeouts_on_writer", AspectType.Metric),
+    CLOSE_CONNECTION_ON_WRITER("close_connection_on_writer", AspectType.Metric),
+    SUCCESS_ON_WRITE_DOCUMENT("success_on_write_document", AspectType.Metric),
+    SUCCESS_ON_WRITE_DOCUMENT_RESPONSE_TIME("success_on_write_document_response_time", AspectType.Histogram),
+    FAILED_ON_WRITE_DOCUMENT("failed_on_write_document", AspectType.Metric),
+    FAILED_ON_WRITE_DOCUMENT_RESPONSE_TIME("failed_on_write_document_response_time", AspectType.Histogram);
 
     private String value;
     private AspectType aspectType;
