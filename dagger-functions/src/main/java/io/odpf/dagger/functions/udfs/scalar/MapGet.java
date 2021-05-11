58fe65298846275942abd6d6643b325339060d0c
prakharmathur82 <prakharmathur82@gmail.com>
2021-05-11 15:35:27 +0530

    feat: add MapGet udf

---
+++ dagger-functions/src/main/java/io/odpf/dagger/functions/udfs/scalar/MapGet.java
@@ -0,0 +1,25 @@
+package io.odpf.dagger.functions.udfs.scalar;
+
+import io.odpf.dagger.common.udfs.ScalarUdf;
+import org.apache.flink.types.Row;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class MapGet extends ScalarUdf {
+    /**
+     * returns value for a corresponding key inside a map data type.
+     *
+     * @param inputMap the input map element
+     * @param key      the key
+     * @return the value
+     * @author Ujjawal
+     * @team Fraud
+     */
+    public Object eval(Row[] inputMap, Object key) {
+        List<Row> rows = Arrays.asList(inputMap);
+        Optional<Row> requiredRow = rows.stream().filter(row -> row.getField(0).equals(key)).findFirst();
+        return requiredRow.map(row -> row.getField(1)).orElse(null);
+    }
+}
