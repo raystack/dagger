2fb69f0654383ea69d8dd2c0f06c7c4b24c0d76d
prakharmathur82 <prakharmathur82@gmail.com>
2021-05-11 17:07:44 +0530

    feat: add TimeInDate udf

---
+++ dagger-functions/src/main/java/io/odpf/dagger/functions/udfs/scalar/TimeInDate.java
@@ -0,0 +1,39 @@
+package io.odpf.dagger.functions.udfs.scalar;
+
+import io.odpf.dagger.common.udfs.ScalarUdf;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class TimeInDate extends ScalarUdf {
+    private static final Integer SECOND_IN_MILLIS = 1000;
+
+    public long eval(long eventTimestamp, int hour, int minute) {
+        return eval(eventTimestamp, hour, minute, "UTC");
+    }
+
+    /**
+     * returns calender's time value in seconds for a given hour, minute, second and timezone.
+     *
+     * @param seconds  the seconds
+     * @param hour     the hour
+     * @param minute   the minute
+     * @param timeZone the time zone
+     * @return the second value
+     * @author Rasyid
+     * @team DE
+     */
+    public long eval(long seconds, int hour, int minute, String timeZone) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTimeZone(TimeZone.getTimeZone(timeZone));
+        cal.setTime(new Date(seconds * SECOND_IN_MILLIS));
+
+        cal.set(Calendar.HOUR_OF_DAY, hour);
+        cal.set(Calendar.MINUTE, minute);
+        cal.clear(Calendar.SECOND);
+        cal.clear(Calendar.MILLISECOND);
+
+        return cal.getTimeInMillis() / SECOND_IN_MILLIS;
+    }
+}
