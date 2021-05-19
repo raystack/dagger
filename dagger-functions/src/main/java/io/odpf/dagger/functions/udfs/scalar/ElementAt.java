6979ecfdcab695fcc7ce628b268ec3bd25134ea0
prakharmathur82 <prakharmathur82@gmail.com>
2021-05-19 19:41:13 +0530

    feat: add ElementAt udf

---
+++ dagger-functions/src/main/java/io/odpf/dagger/functions/udfs/scalar/ElementAt.java
@@ -0,0 +1,135 @@
+package io.odpf.dagger.functions.udfs.scalar;
+
+import com.gojek.de.stencil.client.StencilClient;
+import io.odpf.dagger.common.core.StencilClientOrchestrator;
+import io.odpf.dagger.common.udfs.ScalarUdf;
+import io.odpf.dagger.functions.udfs.scalar.elementAt.MessageReader;
+import org.apache.flink.table.functions.FunctionContext;
+import org.apache.flink.types.Row;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
+public class ElementAt extends ScalarUdf {
+    private LinkedHashMap<String, String> protos;
+    private final StencilClientOrchestrator stencilClientOrchestrator;
+    private StencilClient stencilClient;
+
+
+    public ElementAt(LinkedHashMap<String, String> protos, StencilClientOrchestrator stencilClientOrchestrator) {
+        this.protos = protos;
+        this.stencilClientOrchestrator = stencilClientOrchestrator;
+    }
+
+    @Override
+    public void open(FunctionContext context) throws Exception {
+        super.open(context);
+        if (stencilClient == null) {
+            stencilClient = getStencilClient();
+        }
+    }
+
+    /**
+     * finds out the element at a given index and a given path in an array of complex Datatypes.
+     *
+     * @param array       the array
+     * @param pathOfArray the path of array
+     * @param index       the index
+     * @param path        the path on element in the array
+     * @return the value
+     * @author gaurav.s
+     * @team DE
+     */
+    public String eval(Row[] array, String pathOfArray, int index, String path) {
+        try {
+            if (array == null || array.length <= index) {
+                return "";
+            }
+            String protoClassName = protos.entrySet().iterator().next().getValue();
+            MessageReader messageReader = new MessageReader(array[index], protoClassName, pathOfArray, stencilClient);
+            Object value = messageReader.read(path);
+            return String.valueOf(value);
+
+        } catch (Exception ignored) {
+
+        }
+        return "";
+    }
+
+    /**
+     * for the given table, finds out the element at a given index and a given path in an array of complex Datatypes.
+     *
+     * @param array       the array
+     * @param pathOfArray the path of array
+     * @param index       the index
+     * @param path        the path on element in the array
+     * @param tableName   name of the table to apply udf on (in case of multiple streams / JOINS)
+     * @return the value
+     * @author mayur.gubrele
+     * @team DE
+     */
+    public String eval(Row[] array, String pathOfArray, int index, String path, String tableName) {
+        try {
+            if (array == null || array.length <= index) {
+                return "";
+            }
+            String protoClassName = protos.get(tableName);
+            MessageReader messageReader = new MessageReader(array[index], protoClassName, pathOfArray, stencilClient);
+            Object value = messageReader.read(path);
+            return String.valueOf(value);
+
+        } catch (Exception ignored) {
+
+        }
+        return "";
+    }
+
+    /**
+     * finds the element at a given position for an object ArrayList.
+     *
+     * @param arrayList the array list
+     * @param index     the index
+     * @return the value
+     * @author mayur.gubrele
+     * @team DE
+     */
+    public Object eval(ArrayList<Object> arrayList, int index) {
+        return eval(arrayList.toArray(), index);
+    }
+
+    /**
+     * finds the element at a given position for an object Array. supports -ve index
+     *
+     * @param array the array
+     * @param index the index
+     * @return the value
+     * @author arujit
+     * @team DE
+     */
+    public Object eval(Object[] array, int index) {
+        try {
+            if (array == null || array.length <= index) {
+                return null;
+            }
+            int arrayLength = array.length;
+            if (index < 0) {
+                int absoluteIndex = arrayLength + index;
+                if (absoluteIndex < 0) {
+                    return null;
+                }
+                return array[arrayLength + index];
+            }
+            return array[index];
+        } catch (Exception ignored) {
+
+        }
+        return null;
+    }
+
+    public StencilClient getStencilClient() {
+        if (stencilClient != null) {
+            return stencilClient;
+        }
+        return stencilClientOrchestrator.getStencilClient();
+    }
+}
---
+++ dagger-functions/src/test/java/io/odpf/dagger/functions/udfs/scalar/elementAt/MessageReaderTest.java
@@ -0,0 +1,103 @@
+package io.odpf.dagger.functions.udfs.scalar.elementAt;
+
+import com.gojek.de.stencil.StencilClientFactory;
+import com.gojek.de.stencil.client.StencilClient;
+import org.apache.flink.types.Row;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MessageReaderTest {
+    private static final double CENTRAL_MONUMENT_JAKARTA_LATITUDE = -6.170024;
+    private static final double CENTRAL_MONUMENT_JAKARTA_LONGITUDE = 106.8243203;
+    private static final StencilClient STENCIL_CLIENT = StencilClientFactory.getClient();
+
+    @Test
+    public void shouldBeAbleToReadPathOfOneDepth() throws ClassNotFoundException {
+        Row routeRow = new Row(3);
+        routeRow.setField(2, 21.5F);
+
+        MessageReader messageReader = new MessageReader(routeRow, "io.odpf.dagger.consumer.TestBookingLogMessage", "routes", STENCIL_CLIENT);
+
+        assertEquals(21.5F, messageReader.read("distance_in_kms"));
+    }
+
+    @Test
+    public void shouldBeAbleToReadPathOfTwoDepth() throws ClassNotFoundException {
+        Row routeRow = new Row(3);
+        Row locationRow = new Row(4);
+        locationRow.setField(2, CENTRAL_MONUMENT_JAKARTA_LATITUDE);
+        locationRow.setField(3, CENTRAL_MONUMENT_JAKARTA_LONGITUDE);
+        routeRow.setField(0, locationRow);
+
+        MessageReader messageReader = new MessageReader(routeRow, "io.odpf.dagger.consumer.TestBookingLogMessage", "routes", STENCIL_CLIENT);
+
+        assertEquals(CENTRAL_MONUMENT_JAKARTA_LATITUDE, messageReader.read("start.latitude"));
+        assertEquals(CENTRAL_MONUMENT_JAKARTA_LONGITUDE, messageReader.read("start.longitude"));
+    }
+
+    @Test
+    public void shouldHandleWhenNoValidProtobufForMessageRoute() throws ClassNotFoundException {
+        Row routeRow = new Row(3);
+        routeRow.setField(2, 21.5F);
+
+        MessageReader messageReader = new MessageReader(routeRow, "io.odpf.dagger.consumer.TestBookingLogMessage", "invalid", STENCIL_CLIENT);
+
+        assertEquals("", messageReader.read("distance_in_kms"));
+    }
+
+    @Test(expected = ClassNotFoundException.class)
+    public void shouldHandleWhenNoValidRootProtobuf() throws ClassNotFoundException {
+        Row routeRow = new Row(3);
+        routeRow.setField(2, 21.5F);
+
+        MessageReader messageReader = new MessageReader(routeRow, "invalid", "routes", STENCIL_CLIENT);
+        messageReader.read("anything");
+    }
+
+    @Test
+    public void shouldHandleInvalidParentPathOfOneDepth() throws ClassNotFoundException {
+        Row routeRow = new Row(3);
+        routeRow.setField(2, 21.5F);
+
+        MessageReader messageReader = new MessageReader(routeRow, "io.odpf.dagger.consumer.TestBookingLogMessage", "routes", STENCIL_CLIENT);
+
+        assertEquals("", messageReader.read("invalid"));
+    }
+
+    @Test
+    public void shouldHandleInvalidPathAtParentWhenReadingNestedField() throws ClassNotFoundException {
+        Row routeRow = new Row(3);
+        Row locationRow = new Row(4);
+        locationRow.setField(2, CENTRAL_MONUMENT_JAKARTA_LATITUDE);
+        routeRow.setField(0, locationRow);
+
+        MessageReader messageReader = new MessageReader(routeRow, "io.odpf.dagger.consumer.TestBookingLogMessage", "routes", STENCIL_CLIENT);
+
+        assertEquals("", messageReader.read("invalid.start"));
+    }
+
+    @Test
+    public void shouldHandleInvalidNestedPath() throws ClassNotFoundException {
+        Row routeRow = new Row(3);
+        Row locationRow = new Row(4);
+        locationRow.setField(2, CENTRAL_MONUMENT_JAKARTA_LATITUDE);
+        routeRow.setField(0, locationRow);
+
+        MessageReader messageReader = new MessageReader(routeRow, "io.odpf.dagger.consumer.TestBookingLogMessage", "routes", STENCIL_CLIENT);
+
+        assertEquals("", messageReader.read("start.invalid"));
+    }
+
+    @Test
+    public void shouldBeAbleToReadObject() throws ClassNotFoundException {
+        Row routeRow = new Row(3);
+        Row locationRow = new Row(4);
+        locationRow.setField(2, CENTRAL_MONUMENT_JAKARTA_LATITUDE);
+        routeRow.setField(0, locationRow);
+
+        MessageReader messageReader = new MessageReader(routeRow, "io.odpf.dagger.consumer.TestBookingLogMessage", "routes", STENCIL_CLIENT);
+
+        assertEquals(locationRow, messageReader.read("start"));
+    }
+}
