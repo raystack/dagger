576c40cdc62c5c858c4592d7c7edbfb29621ce6c
prakharmathur82 <prakharmathur82@gmail.com>
2021-05-20 13:17:31 +0530

    feat: register udfs in FunctionFactory

--- dagger-functions/src/main/java/io/odpf/dagger/functions/common/Constants.java
+++ dagger-functions/src/main/java/io/odpf/dagger/functions/common/Constants.java
@@ -3,4 +3,11 @@ package io.odpf.dagger.functions.common;
 public class Constants {
     public static final Integer NUMBER_OF_DATA_TYPES_IN_FEATURE_ROW = 8;
     public static final Integer NUMBER_OF_ARGUMENTS_IN_FEATURE_ACCUMULATOR = 3;
+    public static final String INPUT_STREAMS = "STREAMS";
+    public static final String GCS_PROJECT_ID = "GCS_PROJECT_ID";
+    public static final String GCS_PROJECT_DEFAULT = "";
+    public static final String GCS_BUCKET_ID = "GCS_BUCKET_ID";
+    public static final String GCS_BUCKET_DEFAULT = "";
+    public static final String STREAM_PROTO_CLASS_NAME = "PROTO_CLASS_NAME";
+    public static final String STREAM_TABLE_NAME = "TABLE_NAME";
 }
