5169bb4eeee8f3e186c84b0b15bcdd5b69f7acaf
arujit <jamesbondukela@gmail.com>
2021-08-03 10:49:28 +0530

    fix: incorporate the changes according to the review comments

--- dagger-functions/src/test/java/io/odpf/dagger/functions/udfs/scalar/ByteToStringTest.java
+++ dagger-functions/src/test/java/io/odpf/dagger/functions/udfs/scalar/ByteToStringTest.java
@@ -2,21 +2,34 @@ package io.odpf.dagger.functions.udfs.scalar;
 
 import com.google.protobuf.ByteString;
 import org.junit.Assert;
-import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.nio.charset.StandardCharsets;
 
 public class ByteToStringTest {
-
-    @Before
-    public void setup() {
-    }
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Test
     public void shouldConvertProtoBytesToString() {
         ByteToString byteToString = new ByteToString();
         ByteString byteString = ByteString.copyFrom("testString".getBytes(StandardCharsets.UTF_8));
         Assert.assertEquals("testString", byteToString.eval(byteString));
+    }
+
+    @Test
+    public void shouldConvertProtoBytesToStringForEmptyBytes() {
+        ByteToString byteToString = new ByteToString();
+        ByteString byteString = ByteString.copyFrom("".getBytes(StandardCharsets.UTF_8));
+        Assert.assertEquals("", byteToString.eval(byteString));
+    }
+
+    @Test
+    public void shouldThrowNullPointerExceptionIfPassedForNullField() {
+        thrown.expect(NullPointerException.class);
+        ByteToString byteToString = new ByteToString();
+        Assert.assertEquals("", byteToString.eval(null));
     }
 }
