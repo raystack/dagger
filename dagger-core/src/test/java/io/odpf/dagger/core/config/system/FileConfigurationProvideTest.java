167826f90dd3b1ab208eed77ce56cda79bdde99c
kevin.bheda <kevin.bheda@go-jek.com>
2021-09-28 13:21:58 +0530

    refactor: use asserthrows instead of expectedException

--- dagger-core/src/test/java/io/odpf/dagger/core/config/system/FileConfigurationProvideTest.java
+++ dagger-core/src/test/java/io/odpf/dagger/core/config/system/FileConfigurationProvideTest.java
@@ -3,16 +3,12 @@ package io.odpf.dagger.core.config.system;
 import io.odpf.dagger.core.config.FileConfigurationProvider;
 import io.odpf.dagger.core.exception.DaggerConfigurationException;
 import org.apache.flink.configuration.Configuration;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class FileConfigurationProvideTest {
-
-    @Rule
-    public ExpectedException expectedException = ExpectedException.none();
 
     @Test
     public void readFromAConfigurationFile() {
@@ -24,9 +20,9 @@ public class FileConfigurationProvideTest {
 
     @Test
     public void shouldThrowExceptionForFileNotfound() {
-        expectedException.expect(DaggerConfigurationException.class);
-        expectedException.expectMessage("Config source not provided");
         System.setProperty("DAGGER_CONFIG_PATH", "dd");
-        Configuration stringStringMap = new FileConfigurationProvider().get();
+        DaggerConfigurationException exception = assertThrows(DaggerConfigurationException.class,
+                () -> new FileConfigurationProvider().get());
+        assertEquals("Config source not provided", exception.getMessage());
     }
 }
