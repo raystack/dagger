72714649bffbd2f5741be8286bbf6d61843786f4
kevin.bheda <kevin.bheda@go-jek.com>
2021-12-06 12:16:23 +0530

    refactor: add type inference for scalar udfs

--- dagger-functions/src/main/java/io/odpf/dagger/functions/udfs/scalar/ByteToString.java
+++ dagger-functions/src/main/java/io/odpf/dagger/functions/udfs/scalar/ByteToString.java
@@ -2,6 +2,21 @@ package io.odpf.dagger.functions.udfs.scalar;
 
 import com.google.protobuf.ByteString;
 import io.odpf.dagger.common.udfs.ScalarUdf;
+import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.table.catalog.DataTypeFactory;
+import org.apache.flink.table.functions.FunctionDefinition;
+import org.apache.flink.table.types.DataType;
+import org.apache.flink.table.types.inference.ArgumentCount;
+import org.apache.flink.table.types.inference.CallContext;
+import org.apache.flink.table.types.inference.ConstantArgumentCount;
+import org.apache.flink.table.types.inference.InputTypeStrategy;
+import org.apache.flink.table.types.inference.Signature;
+import org.apache.flink.table.types.inference.TypeInference;
+import org.apache.flink.table.types.inference.TypeStrategy;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 public class ByteToString extends ScalarUdf {
     /**
@@ -12,5 +27,34 @@ public class ByteToString extends ScalarUdf {
      */
     public String eval(ByteString byteField) {
         return byteField.toStringUtf8();
+    }
+
+    @Override
+    public TypeInference getTypeInference(DataTypeFactory typeFactory) {
+        TypeInference build = TypeInference.newBuilder()
+                .inputTypeStrategy(new InputTypeStrategy() {
+                    @Override
+                    public ArgumentCount getArgumentCount() {
+                        return  ConstantArgumentCount.of(1);
+                    }
+
+                    @Override
+                    public Optional<List<DataType>> inferInputTypes(CallContext callContext, boolean throwOnFailure) {
+                        DataType dataType = callContext.getArgumentDataTypes().get(0);
+                        return Optional.of(Arrays.asList(dataType));
+                    }
+
+                    @Override
+                    public List<Signature> getExpectedSignatures(FunctionDefinition definition) {
+                        return null;
+                    }
+                }).outputTypeStrategy(new TypeStrategy() {
+                    @Override
+                    public Optional<DataType> inferType(CallContext callContext) {
+                        return Optional.of(DataTypes.STRING());
+                    }
+                })
+                .build();
+        return build;
     }
 }
