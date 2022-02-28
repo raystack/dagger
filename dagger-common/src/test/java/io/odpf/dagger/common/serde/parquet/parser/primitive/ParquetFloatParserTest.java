cd2a143a19b13099f2e5fdf4116d4b82a5f720b9
Meghajit Mazumdar <Megh319@gmail.com>
2022-02-28 14:48:15 +0530

    feat: apply validations in ParquetFloatParser
    [https://github.com/odpf/dagger/issues/99]

--- dagger-common/src/test/java/io/odpf/dagger/common/serde/parquet/parser/primitive/ParquetFloatParserTest.java
+++ dagger-common/src/test/java/io/odpf/dagger/common/serde/parquet/parser/primitive/ParquetFloatParserTest.java
@@ -1,14 +1,54 @@
 package io.odpf.dagger.common.serde.parquet.parser.primitive;
 
+import io.odpf.dagger.common.serde.parquet.parser.validation.SimpleGroupValidation;
 import org.apache.parquet.example.data.simple.SimpleGroup;
 import org.apache.parquet.schema.GroupType;
 import org.apache.parquet.schema.Types;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.*;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 public class ParquetFloatParserTest {
+
+    @Mock
+    SimpleGroupValidation simpleGroupValidation;
+
+    @Before
+    public void setup() {
+        initMocks(this);
+    }
+
+
+    @Test
+    public void canHandleReturnsFalseIfValidationChecksFail() {
+        when(simpleGroupValidation.applyValidations(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(false);
+        ParquetFloatParser parquetFloatParser = new ParquetFloatParser(simpleGroupValidation);
+
+        boolean canHandle = parquetFloatParser.canHandle(null, "column-with-float-value");
+
+        assertFalse(canHandle);
+    }
+
+    @Test
+    public void getParserReturnsTrueIfValidationChecksPass() {
+        when(simpleGroupValidation.applyValidations(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(true);
+        GroupType parquetSchema = Types.requiredGroup()
+                .required(FLOAT).named("column-with-float-value")
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+        ParquetFloatParser parquetFloatParser = new ParquetFloatParser(simpleGroupValidation);
+
+        boolean canHandle = parquetFloatParser.canHandle(simpleGroup, "column-with-float-value");
+
+        assertTrue(canHandle);
+    }
+
 
     @Test
     public void deserializeShouldParseParquetFloatToJavaFloatType() {
@@ -20,7 +60,7 @@ public class ParquetFloatParserTest {
         simpleGroup.add("column-with-max-float-value", Float.MAX_VALUE);
         simpleGroup.add("column-with-min-float-value", Float.MIN_VALUE);
 
-        ParquetFloatParser floatParser = new ParquetFloatParser();
+        ParquetFloatParser floatParser = new ParquetFloatParser(simpleGroupValidation);
         Object actualValueForMaxFloatColumn = floatParser.deserialize(simpleGroup, "column-with-max-float-value");
         Object actualValueForMinFloatColumn = floatParser.deserialize(simpleGroup, "column-with-min-float-value");
 
@@ -28,23 +68,16 @@ public class ParquetFloatParserTest {
         assertEquals(Float.MIN_VALUE, actualValueForMinFloatColumn);
     }
 
-    @Test(expected = ClassCastException.class)
-    public void deserializeShouldThrowExceptionWhenTypeExceeded() {
+    @Test
+    public void deserializeShouldReturnDefaultValueWhenDeserializedValueIsNull() {
+        /* The field is added to the schema but not assigned a value */
         GroupType parquetSchema = Types.requiredGroup()
-                .required(FLOAT).named("column-with-min-double-value")
+                .required(FLOAT).named("some-random-field")
                 .named("TestGroupType");
         SimpleGroup simpleGroup = new SimpleGroup(parquetSchema) ;
-        simpleGroup.add("column-with-min-double-value", Double.MIN_VALUE);
+        ParquetFloatParser parquetFloatParser = new ParquetFloatParser(simpleGroupValidation);
 
-
-        ParquetFloatParser floatParser = new ParquetFloatParser();
-        floatParser.deserialize(simpleGroup, "column-with-min-double-value");
-    }
-
-    @Test
-    public void deserializeShouldReturnDefaultValueWhenSimpleGroupArgumentIsNull() {
-        ParquetFloatParser parquetFloatParser = new ParquetFloatParser();
-        Object actualValue = parquetFloatParser.deserialize(null, "some-random-field");
+        Object actualValue = parquetFloatParser.deserialize(simpleGroup, "some-random-field");
 
         assertEquals(0.0f, actualValue);
     }
