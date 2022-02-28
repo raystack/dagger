5b52214c67cd82221665f2d3cdf4028445c48274
Meghajit Mazumdar <Megh319@gmail.com>
2022-02-28 16:49:26 +0530

    feat: apply validations in ParquetDoubleParser
    [https://github.com/odpf/dagger/issues/99]

--- dagger-common/src/main/java/io/odpf/dagger/common/serde/parquet/parser/primitive/ParquetDoubleParser.java
+++ dagger-common/src/main/java/io/odpf/dagger/common/serde/parquet/parser/primitive/ParquetDoubleParser.java
@@ -1,18 +1,37 @@
 package io.odpf.dagger.common.serde.parquet.parser.primitive;
 
 import io.odpf.dagger.common.serde.parquet.parser.ParquetDataTypeParser;
+import io.odpf.dagger.common.serde.parquet.parser.validation.SimpleGroupValidation;
 import org.apache.parquet.example.data.simple.SimpleGroup;
+import org.apache.parquet.schema.PrimitiveType;
 
 import java.util.function.Supplier;
 
+import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.DOUBLE;
+
 public class ParquetDoubleParser implements ParquetDataTypeParser {
     private static final double DEFAULT_DESERIALIZED_VALUE = 0.0d;
+    private static final PrimitiveType.PrimitiveTypeName SUPPORTED_PRIMITIVE_TYPE = DOUBLE;
+    private final SimpleGroupValidation simpleGroupValidation;
+
+    public ParquetDoubleParser(SimpleGroupValidation simpleGroupValidation) {
+        this.simpleGroupValidation = simpleGroupValidation;
+    }
+
+    public boolean canHandle(SimpleGroup simpleGroup, String fieldName) {
+        return simpleGroupValidation.applyValidations(simpleGroup, fieldName, SUPPORTED_PRIMITIVE_TYPE, null);
+    }
 
     @Override
     public Object deserialize(SimpleGroup simpleGroup, String fieldName) {
         Supplier<Object> valueSupplier = () -> {
             int columnIndex = simpleGroup.getType().getFieldIndex(fieldName);
-            return simpleGroup.getDouble(columnIndex, 0);
+            /* this checks if the field value is missing */
+            if(simpleGroup.getFieldRepetitionCount(columnIndex) == 0) {
+                return null;
+            } else {
+                return simpleGroup.getDouble(columnIndex, 0);
+            }
         };
         return ParquetDataTypeParser.getValueOrDefault(simpleGroup, valueSupplier, DEFAULT_DESERIALIZED_VALUE);
     }
--- dagger-common/src/test/java/io/odpf/dagger/common/serde/parquet/parser/primitive/ParquetDoubleParserTest.java
+++ dagger-common/src/test/java/io/odpf/dagger/common/serde/parquet/parser/primitive/ParquetDoubleParserTest.java
@@ -1,14 +1,53 @@
 package io.odpf.dagger.common.serde.parquet.parser.primitive;
 
+import io.odpf.dagger.common.serde.parquet.parser.validation.SimpleGroupValidation;
 import org.apache.parquet.example.data.simple.SimpleGroup;
 import org.apache.parquet.schema.GroupType;
 import org.apache.parquet.schema.Types;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.DOUBLE;
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 public class ParquetDoubleParserTest {
+    @Mock
+    SimpleGroupValidation simpleGroupValidation;
+
+    @Before
+    public void setup() {
+        initMocks(this);
+    }
+
+    @Test
+    public void canHandleReturnsFalseIfValidationChecksFail() {
+        when(simpleGroupValidation.applyValidations(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(false);
+        ParquetDoubleParser parquetDoubleParser = new ParquetDoubleParser(simpleGroupValidation);
+
+        boolean canHandle = parquetDoubleParser.canHandle(null, "column-with-boolean-true-value");
+
+        assertFalse(canHandle);
+    }
+
+    @Test
+    public void getParserReturnsTrueIfValidationChecksPass() {
+        when(simpleGroupValidation.applyValidations(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(true);
+        GroupType parquetSchema = Types.requiredGroup()
+                .required(DOUBLE).named("column-with-double-value")
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema) ;
+        simpleGroup.add("column-with-double-value", Double.MIN_VALUE);
+        ParquetDoubleParser doubleParser = new ParquetDoubleParser(simpleGroupValidation);
+
+        boolean canHandle = doubleParser.canHandle(simpleGroup, "column-with-double-value");
+
+        assertTrue(canHandle);
+    }
+
 
     @Test
     public void deserializeShouldParseParquetDoubleToJavaDoubleType() {
@@ -20,7 +59,7 @@ public class ParquetDoubleParserTest {
         simpleGroup.add("column-with-max-double-value", Double.MAX_VALUE);
         simpleGroup.add("column-with-min-double-value", Double.MIN_VALUE);
 
-        ParquetDoubleParser doubleParser = new ParquetDoubleParser();
+        ParquetDoubleParser doubleParser = new ParquetDoubleParser(simpleGroupValidation);
         Object actualValueForMaxDoubleColumn = doubleParser.deserialize(simpleGroup, "column-with-max-double-value");
         Object actualValueForMinDoubleColumn = doubleParser.deserialize(simpleGroup, "column-with-min-double-value");
 
@@ -29,9 +68,14 @@ public class ParquetDoubleParserTest {
     }
 
     @Test
-    public void deserializeShouldReturnDefaultValueWhenSimpleGroupArgumentIsNull() {
-        ParquetDoubleParser parquetDoubleParser = new ParquetDoubleParser();
-        Object actualValue = parquetDoubleParser.deserialize(null, "some-random-field");
+    public void deserializeShouldReturnDefaultValueWhenDeserializedValueIsNull() {
+        /* The field is added to the schema but not assigned a value */
+        GroupType parquetSchema = Types.requiredGroup()
+                .required(DOUBLE).named("some-random-field")
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema) ;
+        ParquetDoubleParser parquetDoubleParser = new ParquetDoubleParser(simpleGroupValidation);
+        Object actualValue = parquetDoubleParser.deserialize(simpleGroup, "some-random-field");
 
         assertEquals(0.0d, actualValue);
     }
