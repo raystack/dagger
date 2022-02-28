c7d4ea73dbe3c8254f55d62bf6bd5ac0589befe5
Meghajit Mazumdar <Megh319@gmail.com>
2022-02-28 14:38:17 +0530

    feat: apply validations in ParquetInt32Parser
    [https://github.com/odpf/dagger/issues/99]

--- dagger-common/src/test/java/io/odpf/dagger/common/serde/parquet/parser/primitive/ParquetInt32ParserTest.java
+++ dagger-common/src/test/java/io/odpf/dagger/common/serde/parquet/parser/primitive/ParquetInt32ParserTest.java
@@ -1,14 +1,54 @@
 package io.odpf.dagger.common.serde.parquet.parser.primitive;
 
+import io.odpf.dagger.common.serde.parquet.parser.validation.SimpleGroupValidation;
 import org.apache.parquet.example.data.simple.SimpleGroup;
 import org.apache.parquet.schema.GroupType;
 import org.apache.parquet.schema.Types;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.*;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
 
 public class ParquetInt32ParserTest {
+
+    @Mock
+    SimpleGroupValidation simpleGroupValidation;
+
+    @Before
+    public void setup() {
+        initMocks(this);
+    }
+
+
+    @Test
+    public void canHandleReturnsFalseIfValidationChecksFail() {
+        when(simpleGroupValidation.applyValidations(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(false);
+        ParquetInt32Parser parquetInt32Parser = new ParquetInt32Parser(simpleGroupValidation);
+
+        boolean canHandle = parquetInt32Parser.canHandle(null, "column-with-int32-value");
+
+        assertFalse(canHandle);
+    }
+
+    @Test
+    public void getParserReturnsTrueIfValidationChecksPass() {
+        when(simpleGroupValidation.applyValidations(Mockito.any(), Mockito.any(), Mockito.any(), Mockito.any())).thenReturn(true);
+        GroupType parquetSchema = Types.requiredGroup()
+                .required(INT32)
+                .named("column-with-int32-value")
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+        ParquetInt32Parser parquetInt32Parser = new ParquetInt32Parser(simpleGroupValidation);
+
+        boolean canHandle = parquetInt32Parser.canHandle(simpleGroup, "column-with-int32-value");
+
+        assertTrue(canHandle);
+    }
 
     @Test
     public void deserializeShouldParseParquetInt32ToJavaIntType() {
@@ -20,7 +60,7 @@ public class ParquetInt32ParserTest {
         simpleGroup.add("column-with-max-int-value", Integer.MAX_VALUE);
         simpleGroup.add("column-with-min-int-value", Integer.MIN_VALUE);
 
-        ParquetInt32Parser int32Parser = new ParquetInt32Parser();
+        ParquetInt32Parser int32Parser = new ParquetInt32Parser(simpleGroupValidation);
         Object actualValueForMaxIntColumn = int32Parser.deserialize(simpleGroup, "column-with-max-int-value");
         Object actualValueForMinIntColumn = int32Parser.deserialize(simpleGroup, "column-with-min-int-value");
 
@@ -28,22 +68,17 @@ public class ParquetInt32ParserTest {
         assertEquals(Integer.MIN_VALUE, actualValueForMinIntColumn);
     }
 
-    @Test(expected = ClassCastException.class)
-    public void deserializeShouldThrowExceptionWhenTypeRangeExceeded() {
-        GroupType parquetSchema = Types.requiredGroup()
-                .required(INT64).named("column-with-max-long-value")
-                .named("TestGroupType");
-        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema) ;
-        simpleGroup.add("column-with-max-long-value", Long.MAX_VALUE);
-
-        ParquetInt32Parser int32Parser = new ParquetInt32Parser();
-        int32Parser.deserialize(simpleGroup, "column-with-max-long-value");
-    }
-
     @Test
-    public void deserializeShouldReturnDefaultValueZeroWhenSimpleGroupArgumentIsNull() {
-        ParquetInt32Parser parquetInt32Parser = new ParquetInt32Parser();
-        Object actualValue = parquetInt32Parser.deserialize(null, "some-random-field");
+    public void deserializeShouldReturnDefaultValueWhenDeserializedValueIsNull() {
+        /* The field is added to the schema but not assigned a value */
+        GroupType parquetSchema = Types.requiredGroup()
+                .required(INT32)
+                .named("some-random-field")
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+        ParquetInt32Parser parquetInt32Parser = new ParquetInt32Parser(simpleGroupValidation);
+
+        Object actualValue = parquetInt32Parser.deserialize(simpleGroup, "some-random-field");
 
         assertEquals(0, actualValue);
     }
