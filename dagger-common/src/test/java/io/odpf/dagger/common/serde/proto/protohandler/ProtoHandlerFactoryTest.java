2e073bc292b99d2ef4fb4cb5abbe5848e1017c7b
Meghajit Mazumdar <Megh319@gmail.com>
2022-03-03 15:34:21 +0530

    feat: add factory method for producing ParquetDataTypeParser
    [https://github.com/odpf/dagger/issues/99]

--- dagger-common/src/test/java/io/odpf/dagger/common/serde/proto/protohandler/ProtoHandlerFactoryTest.java
+++ dagger-common/src/test/java/io/odpf/dagger/common/serde/proto/protohandler/ProtoHandlerFactoryTest.java
@@ -1,23 +1,34 @@
 package io.odpf.dagger.common.serde.proto.protohandler;
 
 import com.google.protobuf.Descriptors;
+import io.odpf.dagger.common.exceptions.serde.DaggerDeserializationException;
+import io.odpf.dagger.common.serde.parquet.parser.ParquetDataTypeParser;
+import io.odpf.dagger.common.serde.parquet.parser.primitive.*;
 import io.odpf.dagger.consumer.TestBookingLogMessage;
 import io.odpf.dagger.consumer.TestFeedbackLogMessage;
 import io.odpf.dagger.consumer.TestNestedRepeatedMessage;
 import io.odpf.dagger.consumer.TestRepeatedEnumMessage;
+import org.apache.parquet.example.data.simple.SimpleGroup;
+import org.apache.parquet.schema.GroupType;
+import org.apache.parquet.schema.LogicalTypeAnnotation;
+import org.apache.parquet.schema.Types;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
+import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.*;
+import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.BOOLEAN;
 import static org.junit.Assert.assertEquals;
 
 public class ProtoHandlerFactoryTest {
     @Before
     public void setup() {
         ProtoHandlerFactory.clearProtoHandlerMap();
+        ProtoHandlerFactory.clearParquetDataTypeParserMap();
     }
 
     @Test
@@ -122,5 +133,204 @@ public class ProtoHandlerFactoryTest {
         ProtoHandler newProtoHandler = ProtoHandlerFactory.getProtoHandler(primitiveFieldDescriptor);
         assertEquals(PrimitiveProtoHandler.class, newProtoHandler.getClass());
         assertEquals(protoHandler, newProtoHandler);
+    }
+
+    @Test(expected = DaggerDeserializationException.class)
+    public void getParquetDataTypeParserShouldThrowExceptionWhenSimpleGroupPassedIsNull() {
+        ProtoHandlerFactory.getParquetDataTypeParser(null, "column-with-binary-string-type");
+    }
+
+    @Test(expected = DaggerDeserializationException.class)
+    public void getParquetDataTypeParserShouldThrowExceptionWhenSimpleGroupPassedDoesNotContainTheField() {
+        GroupType parquetSchema = Types.requiredGroup()
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+
+        ProtoHandlerFactory.getParquetDataTypeParser(simpleGroup, "column-with-binary-string-type");
+    }
+
+    @Test(expected = DaggerDeserializationException.class)
+    public void getParquetDataTypeParserShouldThrowExceptionWhenFieldNameIsNotAPrimitiveType() {
+        GroupType parquetSchema = Types
+                .requiredGroup()
+                .optionalGroup()
+                .required(BOOLEAN).named("boolean-nested-column")
+                .named("subgroup-column")
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+
+        ProtoHandlerFactory.getParquetDataTypeParser(simpleGroup, "subgroup-column");
+    }
+
+    @Test(expected = DaggerDeserializationException.class)
+    public void getParquetDataTypeParserShouldThrowExceptionWhenNoSuitableParserCouldBeFound() {
+        GroupType parquetSchema = Types.requiredGroup()
+                .required(INT96).named("some-random-column")
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+
+        ProtoHandlerFactory.getParquetDataTypeParser(simpleGroup, "some-random-column");
+    }
+
+    @Test
+    public void getParquetDataTypeParserShouldReturnParquetBooleanParserIfBooleanFieldIsPassed() {
+        GroupType parquetSchema = Types
+                .requiredGroup()
+                .required(BOOLEAN).named("some-column")
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+
+        ParquetDataTypeParser parquetDataTypeParser = ProtoHandlerFactory.getParquetDataTypeParser(simpleGroup, "some-column");
+
+        assertEquals(ParquetBooleanParser.class, parquetDataTypeParser.getClass());
+    }
+
+    @Test
+    public void getParquetDataTypeParserShouldReturnParquetDoubleParserIfDoubleFieldIsPassed() {
+        GroupType parquetSchema = Types
+                .requiredGroup()
+                .required(DOUBLE).named("some-column")
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+
+        ParquetDataTypeParser parquetDataTypeParser = ProtoHandlerFactory.getParquetDataTypeParser(simpleGroup, "some-column");
+
+        assertEquals(ParquetDoubleParser.class, parquetDataTypeParser.getClass());
+    }
+
+    @Test
+    public void getParquetDataTypeParserShouldReturnParquetInt32ParserIfInt32FieldIsPassed() {
+        GroupType parquetSchema = Types
+                .requiredGroup()
+                .required(INT32).named("some-column")
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+
+        ParquetDataTypeParser parquetDataTypeParser = ProtoHandlerFactory.getParquetDataTypeParser(simpleGroup, "some-column");
+
+        assertEquals(ParquetInt32Parser.class, parquetDataTypeParser.getClass());
+    }
+
+    @Test
+    public void getParquetDataTypeParserShouldReturnParquetFloatParserIfFloatFieldIsPassed() {
+        GroupType parquetSchema = Types
+                .requiredGroup()
+                .required(FLOAT).named("some-column")
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+
+        ParquetDataTypeParser parquetDataTypeParser = ProtoHandlerFactory.getParquetDataTypeParser(simpleGroup, "some-column");
+
+        assertEquals(ParquetFloatParser.class, parquetDataTypeParser.getClass());
+    }
+
+    @Test
+    public void getParquetDataTypeParserShouldReturnParquetInt64ParserIfInt64FieldIsPassed() {
+        GroupType parquetSchema = Types
+                .requiredGroup()
+                .required(INT64).named("some-column")
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+
+        ParquetDataTypeParser parquetDataTypeParser = ProtoHandlerFactory.getParquetDataTypeParser(simpleGroup, "some-column");
+
+        assertEquals(ParquetInt64Parser.class, parquetDataTypeParser.getClass());
+    }
+
+    @Test
+    public void getParquetDataTypeParserShouldReturnParquetBinaryEnumParserIfBinaryEnumFieldIsPassed() {
+        GroupType parquetSchema = Types
+                .requiredGroup()
+                .required(BINARY).as(LogicalTypeAnnotation.enumType()).named("some-column")
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+
+        ParquetDataTypeParser parquetDataTypeParser = ProtoHandlerFactory.getParquetDataTypeParser(simpleGroup, "some-column");
+
+        assertEquals(ParquetBinaryEnumParser.class, parquetDataTypeParser.getClass());
+    }
+
+    @Test
+    public void getParquetDataTypeParserShouldReturnParquetBinaryStringParserIfBinaryStringFieldIsPassed() {
+        GroupType parquetSchema = Types
+                .requiredGroup()
+                .required(BINARY).as(LogicalTypeAnnotation.stringType()).named("some-column")
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+
+        ParquetDataTypeParser parquetDataTypeParser = ProtoHandlerFactory.getParquetDataTypeParser(simpleGroup, "some-column");
+
+        assertEquals(ParquetBinaryStringParser.class, parquetDataTypeParser.getClass());
+    }
+
+    @Test
+    public void getParquetDataTypeParserShouldReturnParquetTimestampParserIfInt64TimestampFieldIsPassed() {
+        GroupType parquetSchema = Types
+                .requiredGroup()
+                .required(INT64).as(LogicalTypeAnnotation.timestampType(true, LogicalTypeAnnotation.TimeUnit.MILLIS))
+                .named("some-column")
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+
+        ParquetDataTypeParser parquetDataTypeParser = ProtoHandlerFactory.getParquetDataTypeParser(simpleGroup, "some-column");
+
+        assertEquals(ParquetTimestampParser.class, parquetDataTypeParser.getClass());
+    }
+
+    @Test
+    public void getParquetDataTypeParserShouldReturnTheSameObjectWithMultipleThreads() throws InterruptedException {
+        ExecutorService e = Executors.newFixedThreadPool(100);
+        final ArrayList<AssertionError> assertionErrors = new ArrayList<>();
+        final ParquetDataTypeParser[] cache = {null};
+
+        for (int i = 0; i < 1000; i++) {
+            e.submit(() -> {
+                try {
+                    GroupType parquetSchema = Types
+                            .requiredGroup()
+                            .required(FLOAT).named("some-column")
+                            .named("TestGroupType");
+                    SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+                    ParquetDataTypeParser parquetDataTypeParser = ProtoHandlerFactory.getParquetDataTypeParser(simpleGroup, "some-column");
+                    assertEquals(ParquetFloatParser.class, parquetDataTypeParser.getClass());
+                    synchronized (cache) {
+                        ParquetDataTypeParser oldParser = cache[0];
+                        if (oldParser != null) {
+                            assertEquals(parquetDataTypeParser, cache[0]);
+                        } else {
+                            // Only one thread will set this
+                            cache[0] = parquetDataTypeParser;
+                        }
+                    }
+                } catch (AssertionError ex) {
+                    /* Add any thrown exceptions above to the error list */
+                    assertionErrors.add(ex);
+                }
+            });
+        }
+        e.shutdown();
+        e.awaitTermination(10000, TimeUnit.MILLISECONDS);
+
+        if(!assertionErrors.isEmpty()) {
+            String[] errors = assertionErrors.stream()
+                    .map(Throwable::toString)
+                    .toArray(String[]::new);
+            throw new AssertionError(String.join("\n", errors));
+        }
+    }
+
+    @Test
+    public void getParquetDataTypeParserShouldReturnTheSameObjectWhenFactoryMethodIsCalledMultipleTimes() {
+        GroupType parquetSchema = Types
+                .requiredGroup()
+                .required(BOOLEAN).named("some-column")
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+
+        ParquetDataTypeParser parquetDataTypeParser1 = ProtoHandlerFactory.getParquetDataTypeParser(simpleGroup, "some-column");
+        ParquetDataTypeParser parquetDataTypeParser2 = ProtoHandlerFactory.getParquetDataTypeParser(simpleGroup, "some-column");
+
+        assertEquals(ParquetBooleanParser.class, parquetDataTypeParser1.getClass());
+        assertEquals(parquetDataTypeParser1, parquetDataTypeParser2);
     }
 }
