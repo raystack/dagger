0c438437caaf0df6d27fbc8b5bfe03bff5748923
Meghajit Mazumdar <Megh319@gmail.com>
2022-03-04 18:26:17 +0530

    feat: add method to deserialize from simple group in LongPrimitiveTypeHandler
    [https://github.com/odpf/dagger/issues/99]

--- dagger-common/src/main/java/io/odpf/dagger/common/serde/proto/protohandler/typehandler/LongPrimitiveTypeHandler.java
+++ dagger-common/src/main/java/io/odpf/dagger/common/serde/proto/protohandler/typehandler/LongPrimitiveTypeHandler.java
@@ -2,8 +2,10 @@ package io.odpf.dagger.common.serde.proto.protohandler.typehandler;
 
 import com.google.protobuf.Descriptors;
 import com.google.protobuf.Descriptors.FieldDescriptor.JavaType;
+import io.odpf.dagger.common.serde.parquet.parser.validation.SimpleGroupValidation;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.parquet.example.data.simple.SimpleGroup;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,7 +32,22 @@ public class LongPrimitiveTypeHandler implements PrimitiveTypeHandler {
 
     @Override
     public Object getValue(Object field) {
+        if (field instanceof SimpleGroup) {
+            return getValue((SimpleGroup) field);
+        }
         return Long.parseLong(getValueOrDefault(field, "0"));
+    }
+
+    private Object getValue(SimpleGroup simpleGroup) {
+        String fieldName = fieldDescriptor.getName();
+
+        /* this if branch checks that the field name exists in the simple group schema and is initialized */
+        if (SimpleGroupValidation.checkFieldExistsAndIsInitialized(simpleGroup, fieldName)) {
+            return simpleGroup.getLong(fieldName, 0);
+        } else {
+            /* return default value */
+            return 0L;
+        }
     }
 
     @Override
