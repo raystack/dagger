9b895934cd1b885317e5845bfa2cb4695720fd03
Meghajit Mazumdar <Megh319@gmail.com>
2022-03-04 18:52:42 +0530

    feat: add method to deserialize from simple group in StringPrimitiveTypeHandler
    [https://github.com/odpf/dagger/issues/99]

--- dagger-common/src/test/java/io/odpf/dagger/common/serde/proto/protohandler/typehandler/StringPrimitiveTypeHandlerTest.java
+++ dagger-common/src/test/java/io/odpf/dagger/common/serde/proto/protohandler/typehandler/StringPrimitiveTypeHandlerTest.java
@@ -5,11 +5,14 @@ import org.apache.flink.api.java.typeutils.ObjectArrayTypeInfo;
 
 import com.google.protobuf.Descriptors;
 import io.odpf.dagger.consumer.TestBookingLogMessage;
+import org.apache.parquet.example.data.simple.SimpleGroup;
+import org.apache.parquet.schema.GroupType;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.BINARY;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -93,4 +96,47 @@ public class StringPrimitiveTypeHandlerTest {
         assertEquals(0, ((String[]) actualValues).length);
     }
 
+    @Test
+    public void shouldFetchParsedValueForFieldOfTypeStringInSimpleGroup() {
+        Descriptors.FieldDescriptor fieldDescriptor = TestBookingLogMessage.getDescriptor().findFieldByName("order_number");
+        GroupType parquetSchema = org.apache.parquet.schema.Types.requiredGroup()
+                .required(BINARY).named("order_number")
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+        simpleGroup.add("order_number", "some-value");
+        StringPrimitiveTypeHandler stringHandler = new StringPrimitiveTypeHandler(fieldDescriptor);
+
+        Object actualValue = stringHandler.getValue(simpleGroup);
+
+        assertEquals("some-value", actualValue);
+    }
+
+    @Test
+    public void shouldFetchDefaultValueIfFieldNotPresentInSimpleGroup() {
+        Descriptors.FieldDescriptor fieldDescriptor = TestBookingLogMessage.getDescriptor().findFieldByName("order_number");
+        GroupType parquetSchema = org.apache.parquet.schema.Types.requiredGroup()
+                .required(BINARY).named("some-other-field")
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+        StringPrimitiveTypeHandler stringHandler = new StringPrimitiveTypeHandler(fieldDescriptor);
+
+        Object actualValue = stringHandler.getValue(simpleGroup);
+
+        assertEquals("", actualValue);
+    }
+
+    @Test
+    public void shouldFetchDefaultValueIfFieldNotInitializedWithAValueInSimpleGroup() {
+        Descriptors.FieldDescriptor fieldDescriptor = TestBookingLogMessage.getDescriptor().findFieldByName("order_number");
+        /* The field is added to the schema but not assigned a value */
+        GroupType parquetSchema = org.apache.parquet.schema.Types.requiredGroup()
+                .required(BINARY).named("order_number")
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+        StringPrimitiveTypeHandler stringHandler = new StringPrimitiveTypeHandler(fieldDescriptor);
+
+        Object actualValue = stringHandler.getValue(simpleGroup);
+
+        assertEquals("", actualValue);
+    }
 }
