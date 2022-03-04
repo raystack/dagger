a2a8c2caaa6b6b9ef3f3d1cea525516dcdd5b61a
Meghajit Mazumdar <Megh319@gmail.com>
2022-03-04 20:24:22 +0530

    feat: add method to deserialize from simple group in ByteStringPrimitiveTypeHandler
    [https://github.com/odpf/dagger/issues/99]

--- dagger-common/src/main/java/io/odpf/dagger/common/serde/proto/protohandler/typehandler/ByteStringPrimitiveTypeHandler.java
+++ dagger-common/src/main/java/io/odpf/dagger/common/serde/proto/protohandler/typehandler/ByteStringPrimitiveTypeHandler.java
@@ -1,11 +1,13 @@
 package io.odpf.dagger.common.serde.proto.protohandler.typehandler;
 
+import io.odpf.dagger.common.serde.parquet.parser.validation.SimpleGroupValidation;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeinfo.Types;
 
 import com.google.protobuf.ByteString;
 import com.google.protobuf.Descriptors;
 import com.google.protobuf.Descriptors.FieldDescriptor.JavaType;
+import org.apache.parquet.example.data.simple.SimpleGroup;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +34,22 @@ public class ByteStringPrimitiveTypeHandler implements PrimitiveTypeHandler {
 
     @Override
     public Object getValue(Object field) {
+        if (field instanceof SimpleGroup) {
+            return getValue((SimpleGroup) field);
+        }
         return field;
+    }
+
+    private Object getValue(SimpleGroup simpleGroup) {
+        String fieldName = fieldDescriptor.getName();
+
+        /* this if branch checks that the field name exists in the simple group schema and is initialized */
+        if (SimpleGroupValidation.checkFieldExistsAndIsInitialized(simpleGroup, fieldName)) {
+            byte[] byteArray = simpleGroup.getBinary(fieldName, 0).getBytes();
+            return ByteString.copyFrom(byteArray);
+        } else {
+            return null;
+        }
     }
 
     @Override
