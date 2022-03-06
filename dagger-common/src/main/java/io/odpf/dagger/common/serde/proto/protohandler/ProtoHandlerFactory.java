d01fb95982c040f51667a74c96a1972d1f339242
Meghajit Mazumdar <Megh319@gmail.com>
2022-03-06 22:17:16 +0530

    feat: undo adding factory method to create ParquetDataTypeParser
    - this won't be needed as the same factory method will be used
    to create handlers for both parquet and kafka
    
    [https://github.com/odpf/dagger/issues/99]

--- dagger-common/src/main/java/io/odpf/dagger/common/serde/proto/protohandler/ProtoHandlerFactory.java
+++ dagger-common/src/main/java/io/odpf/dagger/common/serde/proto/protohandler/ProtoHandlerFactory.java
@@ -1,16 +1,10 @@
 package io.odpf.dagger.common.serde.proto.protohandler;
 
 import com.google.protobuf.Descriptors;
-import io.odpf.dagger.common.exceptions.serde.DaggerDeserializationException;
-import io.odpf.dagger.common.serde.parquet.parser.ParquetDataTypeID;
-import io.odpf.dagger.common.serde.parquet.parser.ParquetDataTypeParser;
-import io.odpf.dagger.common.serde.parquet.parser.primitive.*;
-import io.odpf.dagger.common.serde.parquet.parser.validation.SimpleGroupValidation;
-import org.apache.parquet.example.data.simple.SimpleGroup;
-import org.apache.parquet.schema.LogicalTypeAnnotation;
-import org.apache.parquet.schema.PrimitiveType;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -18,9 +12,6 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class ProtoHandlerFactory {
     private static Map<String, ProtoHandler> protoHandlerMap = new ConcurrentHashMap<>();
-    private static Map<String, ParquetDataTypeParser> parquetDataTypeParserMap = new ConcurrentHashMap<>();
-    private static SimpleGroupValidation simpleGroupValidation = new SimpleGroupValidation();
-
 
     /**
      * Gets proto handler.
@@ -34,59 +25,11 @@ public class ProtoHandlerFactory {
                         .findFirst().orElseGet(() -> new PrimitiveProtoHandler(fieldDescriptor)));
     }
 
-    public static ParquetDataTypeParser getParquetDataTypeParser(SimpleGroup simpleGroup, String fieldName) {
-        validate(simpleGroup, fieldName);
-        PrimitiveType.PrimitiveTypeName fieldPrimitiveType = simpleGroup.getType().getType(fieldName).asPrimitiveType().getPrimitiveTypeName();
-        LogicalTypeAnnotation logicalType = simpleGroup.getType().getType(fieldName).getLogicalTypeAnnotation();
-        ParquetDataTypeID parquetDataTypeID = new ParquetDataTypeID(fieldPrimitiveType, logicalType);
-        return parquetDataTypeParserMap.computeIfAbsent(parquetDataTypeID.toString(),
-                fieldDataTypeID ->
-                        getPrimitiveHandlers()
-                                .stream()
-                                .filter(parquetDataTypeParser -> parquetDataTypeParser.canHandle(simpleGroup, fieldName))
-                                .findFirst()
-                                .orElseThrow(() -> {
-                                    String message = String.format("Error: Could not find a compatible parser to parse " +
-                                            "field %s in the simple group of primitive type %s and logical type %s", fieldName,
-                                            fieldPrimitiveType, logicalType);
-                                    return new DaggerDeserializationException(message);
-                                }));
-    }
-
-    private static void validate(SimpleGroup simpleGroup, String fieldName) {
-        if (!simpleGroupValidation.checkSimpleGroupNotNull(simpleGroup)) {
-            throw new DaggerDeserializationException("Error: SimpleGroup argument is null");
-        }
-        if (!simpleGroupValidation.checkFieldExistsInSimpleGroup(simpleGroup, fieldName)) {
-            throw new DaggerDeserializationException("Error: Field name " + fieldName + " does not exist in SimpleGroup argument");
-        }
-        if (!simpleGroupValidation.checkFieldIsPrimitive(simpleGroup, fieldName)) {
-            throw new DaggerDeserializationException("Error: Field name " + fieldName + " is not a primitive type");
-        }
-    }
-
     /**
      * Clear proto handler map.
      */
     protected static void clearProtoHandlerMap() {
         protoHandlerMap.clear();
-    }
-
-    protected static void clearParquetDataTypeParserMap() {
-        parquetDataTypeParserMap.clear();
-    }
-
-    private static List<ParquetDataTypeParser> getPrimitiveHandlers() {
-        return Arrays.asList(
-                new ParquetBooleanParser(simpleGroupValidation),
-                new ParquetDoubleParser(simpleGroupValidation),
-                new ParquetFloatParser(simpleGroupValidation),
-                new ParquetInt32Parser(simpleGroupValidation),
-                new ParquetInt64Parser(simpleGroupValidation),
-                new ParquetBinaryEnumParser(simpleGroupValidation),
-                new ParquetBinaryStringParser(simpleGroupValidation),
-                new ParquetTimestampParser(simpleGroupValidation)
-        );
     }
 
     private static List<ProtoHandler> getSpecificHandlers(Descriptors.FieldDescriptor fieldDescriptor) {
