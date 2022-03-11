7104a05f2b0aaecdeae0134cddaadb7ee135af8d
Meghajit Mazumdar <Megh319@gmail.com>
2022-03-11 18:25:04 +0530

    feat: throw exception for unbounded parquet source
    [https://github.com/odpf/dagger/issues/99]

--- dagger-core/src/main/java/io/odpf/dagger/core/source/parquet/ParquetFileSource.java
+++ dagger-core/src/main/java/io/odpf/dagger/core/source/parquet/ParquetFileSource.java
@@ -2,11 +2,14 @@ package io.odpf.dagger.core.source.parquet;
 
 import io.odpf.dagger.common.configuration.Configuration;
 import io.odpf.dagger.core.source.SourceType;
+import org.apache.flink.configuration.IllegalConfigurationException;
 import org.apache.flink.connector.file.src.FileSource;
 import org.apache.flink.connector.file.src.assigners.FileSplitAssigner;
 import org.apache.flink.connector.file.src.reader.FileRecordFormat;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.types.Row;
+
+import static io.odpf.dagger.core.source.SourceType.UNBOUNDED;
 
 public class ParquetFileSource {
     private final SourceType sourceType;
@@ -29,6 +32,9 @@ public class ParquetFileSource {
     }
 
     public FileSource<Row> getFileSource() {
+        if (sourceType == UNBOUNDED) {
+            throw new IllegalConfigurationException("Running KafkaSource in UNBOUNDED mode is not supported yet");
+        }
         return FileSource.forRecordFileFormat(fileRecordFormat, filePaths)
                 .setSplitAssigner(fileSplitAssigner)
                 .build();
