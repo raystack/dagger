651583fef9e32e3532687deca360d9900b2822ea
Meghajit Mazumdar <Megh319@gmail.com>
2022-03-11 18:40:39 +0530

    feat: add new field and constructor to Stream POJO
    - Stream now also maintains details about its source
    boundedness and type
    
    [https://github.com/odpf/dagger/issues/99]

--- dagger-core/src/main/java/io/odpf/dagger/core/source/Stream.java
+++ dagger-core/src/main/java/io/odpf/dagger/core/source/Stream.java
@@ -1,10 +1,10 @@
 package io.odpf.dagger.core.source;
 
+import lombok.NonNull;
 import org.apache.flink.api.connector.source.Source;
 
 import io.odpf.dagger.common.serde.DataTypes;
 import lombok.Getter;
-import lombok.NonNull;
 
 public class Stream {
     @Getter
@@ -16,11 +16,21 @@ public class Stream {
     @Getter
     @NonNull
     private DataTypes inputDataType;
+    @Getter
+    @NonNull
+    private SourceDetails[] sourceDetails;
 
     public Stream(Source source, String streamName, DataTypes inputDataType) {
         this.source = source;
         this.streamName = streamName;
         this.inputDataType = inputDataType;
+        this.sourceDetails = new SourceDetails[0];
+    }
+
+    public Stream(Source source, SourceDetails[] sourceDetails, String streamName, DataTypes inputDataType) {
+        this.source = source;
+        this.streamName = streamName;
+        this.inputDataType = inputDataType;
+        this.sourceDetails = sourceDetails;
     }
 }
-
