69c5cb9cbc4f967bcf174b370ba085bd106d36f1
Meghajit Mazumdar <Megh319@gmail.com>
2022-03-14 19:09:54 +0530

    feat: add tests for DeserializerFactory
    [https://github.com/odpf/dagger/issues/99]

--- dagger-core/src/test/java/io/odpf/dagger/core/deserializer/DeserializerFactoryTest.java
+++ dagger-core/src/test/java/io/odpf/dagger/core/deserializer/DeserializerFactoryTest.java
@@ -2,11 +2,17 @@ package io.odpf.dagger.core.deserializer;
 
 import io.odpf.dagger.common.configuration.Configuration;
 import io.odpf.dagger.common.core.StencilClientOrchestrator;
+import io.odpf.dagger.common.serde.DaggerDeserializer;
 import io.odpf.dagger.common.serde.DataTypes;
+import io.odpf.dagger.common.serde.json.deserialization.JsonDeserializer;
+import io.odpf.dagger.common.serde.parquet.deserialization.SimpleGroupDeserializer;
 import io.odpf.dagger.common.serde.proto.deserialization.ProtoDeserializer;
+import io.odpf.dagger.consumer.TestBookingLogMessage;
 import io.odpf.dagger.core.source.SourceName;
 import io.odpf.dagger.core.source.StreamConfig;
+import io.odpf.stencil.client.StencilClient;
 import org.apache.flink.configuration.IllegalConfigurationException;
+import org.apache.flink.types.Row;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,13 +25,13 @@ import static io.odpf.dagger.common.serde.DataTypes.JSON;
 import static io.odpf.dagger.common.serde.DataTypes.PROTO;
 import static io.odpf.dagger.core.source.SourceName.KAFKA;
 import static io.odpf.dagger.core.source.SourceName.PARQUET;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.*;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 @RunWith(PowerMockRunner.class)
+@PrepareForTest({DataTypes.class, SourceName.class})
 public class DeserializerFactoryTest {
     @Mock
     private Configuration configuration;
@@ -37,15 +43,35 @@ public class DeserializerFactoryTest {
     private StreamConfig streamConfig;
 
     @Mock
+    private DataTypes invalidInputSchemaDataType;
+
+    @Mock
+    private SourceName invalidSourceName;
+
+    @Mock
     private ProtoDeserializer protoDeserializer;
+
+    @Mock
+    private StencilClient stencilClient;
 
     @Before
     public void setup() {
         initMocks(this);
+
+        invalidInputSchemaDataType = mock(DataTypes.class);
+        given(invalidInputSchemaDataType.name()).willReturn("INVALID_INPUT_SCHEMA_DATA_TYPE");
+        given(invalidInputSchemaDataType.ordinal()).willReturn(2);
+        PowerMockito.mockStatic(DataTypes.class);
+        PowerMockito.when(DataTypes.values()).thenReturn(new DataTypes[]{JSON, PROTO, invalidInputSchemaDataType});
+
+        invalidSourceName = mock(SourceName.class);
+        given(invalidSourceName.name()).willReturn("INVALID_SOURCE_NAME");
+        given(invalidSourceName.ordinal()).willReturn(2);
+        PowerMockito.mockStatic(SourceName.class);
+        PowerMockito.when(SourceName.values()).thenReturn(new SourceName[]{KAFKA, PARQUET, invalidSourceName});
     }
 
     @Test
-    @PrepareForTest(SourceName.class)
     public void shouldThrowExceptionWhenSourceNameIsNotSupported() {
         SourceName sourceName = mock(SourceName.class);
         given(sourceName.name()).willReturn("INVALID_SOURCE");
@@ -70,18 +96,47 @@ public class DeserializerFactoryTest {
     }
 
     @Test
-    @PrepareForTest(DataTypes.class)
+    public void shouldReturnInstanceOfSimpleGroupDeserializerWhenParquetSourceConfiguredWithPROTOSchema() {
+        when(streamConfig.getEventTimestampFieldIndex()).thenReturn("5");
+        when(stencilClientOrchestrator.getStencilClient()).thenReturn(stencilClient);
+        when(streamConfig.getProtoClass()).thenReturn("com.tests.TestMessage");
+        when(stencilClient.get("com.tests.TestMessage")).thenReturn(TestBookingLogMessage.getDescriptor());
+
+
+        DaggerDeserializer<Row> deserializer = DeserializerFactory.create(PARQUET, PROTO, streamConfig, configuration, stencilClientOrchestrator);
+
+        assertTrue(deserializer instanceof SimpleGroupDeserializer);
+    }
+
+    @Test
+    public void shouldReturnInstanceOfProtoDeserializerWhenKafkaSourceConfiguredWithPROTOSchema() {
+        when(streamConfig.getEventTimestampFieldIndex()).thenReturn("5");
+        when(stencilClientOrchestrator.getStencilClient()).thenReturn(stencilClient);
+        when(streamConfig.getProtoClass()).thenReturn("com.tests.TestMessage");
+        when(stencilClient.get("com.tests.TestMessage")).thenReturn(TestBookingLogMessage.getDescriptor());
+
+
+        DaggerDeserializer<Row> deserializer = DeserializerFactory.create(KAFKA, PROTO, streamConfig, configuration, stencilClientOrchestrator);
+
+        assertTrue(deserializer instanceof ProtoDeserializer);
+    }
+
+    @Test
+    public void shouldReturnInstanceOfJsonDeserializerWhenKafkaSourceConfiguredWithJSONSchema() {
+        when(streamConfig.getJsonSchema()).thenReturn("{ \"$schema\": \"https://json-schema.org/draft/2020-12/schema\", \"$id\": \"https://example.com/product.schema.json\", \"title\": \"Product\", \"description\": \"A product from Acme's catalog\", \"type\": \"object\", \"properties\": { \"id\": { \"description\": \"The unique identifier for a product\", \"type\": \"string\" }, \"time\": { \"description\": \"event timestamp of the event\", \"type\": \"string\", \"format\" : \"date-time\" } }, \"required\": [ \"id\", \"time\" ] }");
+
+        DaggerDeserializer<Row> deserializer = DeserializerFactory.create(KAFKA, JSON, streamConfig, configuration, null);
+
+        assertTrue(deserializer instanceof JsonDeserializer);
+        verify(streamConfig).getJsonEventTimestampFieldName();
+    }
+
+    @Test
     public void shouldThrowExceptionWhenKafkaSourceIsConfiguredWithInvalidSchemaDataType() {
-        DataTypes inputDataType = mock(DataTypes.class);
-        given(inputDataType.name()).willReturn("DUMB_SCHEMA");
-        given(inputDataType.ordinal()).willReturn(2);
-        PowerMockito.mockStatic(DataTypes.class);
-        PowerMockito.when(DataTypes.values()).thenReturn(new DataTypes[]{JSON, PROTO, inputDataType});
-
         IllegalConfigurationException exception = assertThrows(IllegalConfigurationException.class,
-                () -> DeserializerFactory.create(KAFKA, inputDataType, streamConfig, configuration, stencilClientOrchestrator));
+                () -> DeserializerFactory.create(KAFKA, invalidInputSchemaDataType, streamConfig, configuration, stencilClientOrchestrator));
 
-        assertEquals("Invalid stream configuration: No suitable Kafka deserializer could be constructed for STREAM_INPUT_DATATYPE with value DUMB_SCHEMA",
+        assertEquals("Invalid stream configuration: No suitable Kafka deserializer could be constructed for STREAM_INPUT_DATATYPE with value INVALID_INPUT_SCHEMA_DATA_TYPE",
                 exception.getMessage());
     }
 }
