8be0e78099479f6206608588499172a02c0ab74a
Meghajit Mazumdar <Megh319@gmail.com>
2022-03-29 17:22:43 +0530

    feat: add tests for ParquetSourceProtoType
    [https://github.com/odpf/dagger/issues/99]

---
+++ dagger-core/src/test/java/io/odpf/dagger/core/streamtype/ParquetSourceProtoTypeTest.java
@@ -0,0 +1,118 @@
+package io.odpf.dagger.core.streamtype;
+
+import io.odpf.dagger.common.configuration.Configuration;
+import io.odpf.dagger.common.core.StencilClientOrchestrator;
+import io.odpf.dagger.common.serde.parquet.deserialization.SimpleGroupDeserializer;
+import io.odpf.dagger.consumer.TestBookingLogMessage;
+import io.odpf.dagger.core.source.SourceDetails;
+import io.odpf.dagger.core.source.StreamConfig;
+import io.odpf.stencil.client.StencilClient;
+import org.apache.flink.connector.file.src.FileSource;
+import org.apache.flink.types.Row;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static io.odpf.dagger.common.serde.DataTypes.PROTO;
+import static io.odpf.dagger.core.source.SourceName.KAFKA;
+import static io.odpf.dagger.core.source.SourceName.PARQUET;
+import static io.odpf.dagger.core.source.SourceType.BOUNDED;
+import static io.odpf.dagger.core.source.SourceType.UNBOUNDED;
+import static io.odpf.dagger.core.source.parquet.SourceParquetReadOrderStrategy.EARLIEST_TIME_URL_FIRST;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class ParquetSourceProtoTypeTest {
+    @Mock
+    private StreamConfig streamConfig;
+
+    @Mock
+    private Configuration configuration;
+
+    @Mock
+    private StencilClientOrchestrator stencilClientOrchestrator;
+
+    @Mock
+    private StencilClient stencilClient;
+
+    @Before
+    public void setup() {
+        initMocks(this);
+    }
+
+    @Test
+    public void shouldReturnFalseIfTheSourceNameIsNotSupported() {
+        ParquetSourceProtoType.ParquetSourceProtoTypeBuilder parquetSourceProtoTypeBuilder = new ParquetSourceProtoType.ParquetSourceProtoTypeBuilder(streamConfig, configuration, stencilClientOrchestrator);
+        when(streamConfig.getSourceDetails()).thenReturn(new SourceDetails[]{new SourceDetails(KAFKA, BOUNDED)});
+        when(streamConfig.getDataType()).thenReturn("PROTO");
+
+        boolean canBuild = parquetSourceProtoTypeBuilder.canBuild();
+
+        assertFalse(canBuild);
+    }
+
+    @Test
+    public void shouldReturnFalseIfMultipleBackToBackSourcesAreConfigured() {
+        ParquetSourceProtoType.ParquetSourceProtoTypeBuilder parquetSourceProtoTypeBuilder = new ParquetSourceProtoType.ParquetSourceProtoTypeBuilder(streamConfig, configuration, stencilClientOrchestrator);
+        when(streamConfig.getSourceDetails()).thenReturn(new SourceDetails[]{new SourceDetails(PARQUET, BOUNDED), new SourceDetails(KAFKA, UNBOUNDED)});
+        when(streamConfig.getDataType()).thenReturn("PROTO");
+
+        boolean canBuild = parquetSourceProtoTypeBuilder.canBuild();
+
+        assertFalse(canBuild);
+    }
+
+    @Test
+    public void shouldReturnFalseIfTheSourceTypeIsNotSupported() {
+        ParquetSourceProtoType.ParquetSourceProtoTypeBuilder parquetSourceProtoTypeBuilder = new ParquetSourceProtoType.ParquetSourceProtoTypeBuilder(streamConfig, configuration, stencilClientOrchestrator);
+        when(streamConfig.getSourceDetails()).thenReturn(new SourceDetails[]{new SourceDetails(PARQUET, UNBOUNDED)});
+        when(streamConfig.getDataType()).thenReturn("PROTO");
+
+        boolean canBuild = parquetSourceProtoTypeBuilder.canBuild();
+
+        assertFalse(canBuild);
+    }
+
+    @Test
+    public void shouldReturnFalseIfTheSchemaTypeIsNotSupported() {
+        ParquetSourceProtoType.ParquetSourceProtoTypeBuilder parquetSourceProtoTypeBuilder = new ParquetSourceProtoType.ParquetSourceProtoTypeBuilder(streamConfig, configuration, stencilClientOrchestrator);
+        when(streamConfig.getSourceDetails()).thenReturn(new SourceDetails[]{new SourceDetails(PARQUET, BOUNDED)});
+        when(streamConfig.getDataType()).thenReturn("JSON");
+
+        boolean canBuild = parquetSourceProtoTypeBuilder.canBuild();
+
+        assertFalse(canBuild);
+    }
+
+    @Test
+    public void shouldReturnTrueIfTheStreamTypeCanBeBuiltFromConfigs() {
+        ParquetSourceProtoType.ParquetSourceProtoTypeBuilder parquetSourceProtoTypeBuilder = new ParquetSourceProtoType.ParquetSourceProtoTypeBuilder(streamConfig, configuration, stencilClientOrchestrator);
+        when(streamConfig.getSourceDetails()).thenReturn(new SourceDetails[]{new SourceDetails(PARQUET, BOUNDED)});
+        when(streamConfig.getDataType()).thenReturn("PROTO");
+
+        boolean canBuild = parquetSourceProtoTypeBuilder.canBuild();
+
+        assertTrue(canBuild);
+    }
+
+    @Test
+    public void shouldBuildAStreamTypeWithParquetSourceAndProtoSchemaType() {
+        ParquetSourceProtoType.ParquetSourceProtoTypeBuilder parquetSourceProtoTypeBuilder = new ParquetSourceProtoType.ParquetSourceProtoTypeBuilder(streamConfig, configuration, stencilClientOrchestrator);
+
+        when(streamConfig.getSchemaTable()).thenReturn("test-table");
+        when(streamConfig.getEventTimestampFieldIndex()).thenReturn("5");
+        when(streamConfig.getProtoClass()).thenReturn("com.tests.TestMessage");
+        when(streamConfig.getParquetFilesReadOrderStrategy()).thenReturn(EARLIEST_TIME_URL_FIRST);
+        when(streamConfig.getParquetFilePaths()).thenReturn(new String[]{"gs://something", "gs://anything"});
+        when(stencilClientOrchestrator.getStencilClient()).thenReturn(stencilClient);
+        when(stencilClient.get("com.tests.TestMessage")).thenReturn(TestBookingLogMessage.getDescriptor());
+
+        StreamType<Row> streamType = parquetSourceProtoTypeBuilder.build();
+
+        assertTrue(streamType.getSource() instanceof FileSource);
+        assertEquals("test-table", streamType.getStreamName());
+        assertTrue(streamType.getDeserializer() instanceof SimpleGroupDeserializer);
+        assertEquals(PROTO, streamType.getInputDataType());
+    }
+}
