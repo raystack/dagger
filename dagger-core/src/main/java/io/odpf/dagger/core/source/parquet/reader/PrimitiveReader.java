35736bb84875a1cf670fde23de49288630d19858
Meghajit Mazumdar <Megh319@gmail.com>
2022-03-31 17:22:50 +0530

    feat: implement PrimitiveReader and add tests
    - add dependency of `parquet-protobuf` required for using
    ParquetFileReader. This reader is needed for reading row groups
    out of the parquet file
    - add test parquet files in resources
    
    [https://github.com/odpf/dagger/issues/99]

--- dagger-core/src/main/java/io/odpf/dagger/core/source/parquet/reader/PrimitiveReader.java
+++ dagger-core/src/main/java/io/odpf/dagger/core/source/parquet/reader/PrimitiveReader.java
@@ -4,7 +4,20 @@ import io.odpf.dagger.common.serde.parquet.deserialization.SimpleGroupDeserializ
 import lombok.Getter;
 import org.apache.flink.connector.file.src.reader.FileRecordFormat;
 import org.apache.flink.types.Row;
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
+import org.apache.parquet.column.page.PageReadStore;
+import org.apache.parquet.example.data.Group;
+import org.apache.parquet.example.data.simple.SimpleGroup;
+import org.apache.parquet.example.data.simple.convert.GroupRecordConverter;
+import org.apache.parquet.hadoop.ParquetFileReader;
+import org.apache.parquet.hadoop.util.HadoopInputFile;
+import org.apache.parquet.io.ColumnIOFactory;
+import org.apache.parquet.io.MessageColumnIO;
+import org.apache.parquet.io.RecordReader;
+import org.apache.parquet.schema.MessageType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
 import java.io.IOException;
@@ -15,23 +28,54 @@ public class PrimitiveReader implements FileRecordFormat.Reader<Row>, Serializab
     private final Path hadoopFilePath;
     @Getter
     private final SimpleGroupDeserializer simpleGroupDeserializer;
+    private long currentRecordIndex;
+    private final ParquetFileReader reader;
+    private long rowCount;
+    private RecordReader<Group> recordReader;
+    private final MessageType schema;
+    private static final Logger LOGGER = LoggerFactory.getLogger(PrimitiveReader.class.getName());
 
-    public PrimitiveReader(String filePath, SimpleGroupDeserializer simpleGroupDeserializer) {
+    public PrimitiveReader(String filePath, SimpleGroupDeserializer simpleGroupDeserializer) throws IOException {
         this.hadoopFilePath = new Path(filePath);
         this.simpleGroupDeserializer = simpleGroupDeserializer;
+        Path path = new Path(filePath);
+        Configuration conf = new Configuration();
+        reader = ParquetFileReader.open(HadoopInputFile.fromPath(path, conf));
+        schema = reader.getFileMetaData().getSchema();
+        changeReaderPosition(reader.readNextRowGroup());
+        String logMessage = String.format("Successfully created the ParquetFileReader and RecordReader for file %s", hadoopFilePath.getName());
+        LOGGER.info(logMessage);
+    }
+
+    private void changeReaderPosition(PageReadStore pages) {
+        rowCount = pages.getRowCount();
+        currentRecordIndex = 0;
+        MessageColumnIO columnIO = new ColumnIOFactory().getColumnIO(schema);
+        recordReader = columnIO.getRecordReader(pages, new GroupRecordConverter(schema));
     }
 
     @Nullable
     @Override
     public Row read() throws IOException {
-        return null;
+        if (currentRecordIndex >= rowCount) {
+            PageReadStore nextPage = reader.readNextRowGroup();
+            if (nextPage == null) {
+                return null;
+            }
+            changeReaderPosition(nextPage);
+        }
+        SimpleGroup simpleGroup = (SimpleGroup) recordReader.read();
+        currentRecordIndex++;
+        return simpleGroupDeserializer.deserialize(simpleGroup);
     }
 
     @Override
     public void close() throws IOException {
-
+        String logMessage = String.format("Closing the ParquetFileReader and de-referencing the RecordReader for file %s", hadoopFilePath.getName());
+        LOGGER.info(logMessage);
+        reader.close();
+        recordReader = null;
     }
-
 
     public static class PrimitiveReaderProvider implements ReaderProvider {
         private final SimpleGroupDeserializer simpleGroupDeserializer;
@@ -42,7 +86,11 @@ public class PrimitiveReader implements FileRecordFormat.Reader<Row>, Serializab
 
         @Override
         public PrimitiveReader getReader(String filePath) {
-            return new PrimitiveReader(filePath, simpleGroupDeserializer);
+            try {
+                return new PrimitiveReader(filePath, simpleGroupDeserializer);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
         }
     }
 }
--- dagger-core/src/test/java/io/odpf/dagger/core/source/parquet/reader/PrimitiveReaderTest.java
+++ dagger-core/src/test/java/io/odpf/dagger/core/source/parquet/reader/PrimitiveReaderTest.java
@@ -2,26 +2,153 @@ package io.odpf.dagger.core.source.parquet.reader;
 
 
 import io.odpf.dagger.common.serde.parquet.deserialization.SimpleGroupDeserializer;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.flink.types.Row;
 import org.apache.hadoop.fs.Path;
+import org.apache.parquet.example.data.simple.SimpleGroup;
+import org.apache.parquet.schema.GroupType;
+import org.apache.parquet.schema.LogicalTypeAnnotation;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 
-import static org.junit.Assert.assertEquals;
+import java.io.IOException;
+import java.util.List;
+
+import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.BINARY;
+import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.INT64;
+import static org.apache.parquet.schema.Types.*;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 public class PrimitiveReaderTest {
     @Mock
     private SimpleGroupDeserializer deserializer;
 
+    @Before
+    public void setup() {
+        initMocks(this);
+    }
+
     @Test
     public void shouldCreateReadersConfiguredWithTheSameDeserializerButForDifferentFilePaths() {
         PrimitiveReader.PrimitiveReaderProvider provider = new PrimitiveReader.PrimitiveReaderProvider(deserializer);
+        ClassLoader classLoader = getClass().getClassLoader();
+        String filePath1 = classLoader.getResource("test_file.parquet").getPath();
+        PrimitiveReader reader1 = provider.getReader(filePath1);
+        String filePath2 = classLoader.getResource("multiple_row_groups_test_file.parquet").getPath();
+        PrimitiveReader reader2 = provider.getReader(filePath2);
 
-        PrimitiveReader reader1 = provider.getReader("gs://first-file-path");
-        PrimitiveReader reader2 = provider.getReader("gs://second-file-path");
-
-        assertEquals(new Path("gs://first-file-path"), reader1.getHadoopFilePath());
-        assertEquals(new Path("gs://second-file-path"), reader2.getHadoopFilePath());
+        assertEquals(new Path(filePath1), reader1.getHadoopFilePath());
+        assertEquals(new Path(filePath2), reader2.getHadoopFilePath());
         assertEquals(deserializer, reader1.getSimpleGroupDeserializer());
         assertEquals(deserializer, reader2.getSimpleGroupDeserializer());
+    }
+
+    @Test
+    public void shouldReadFileAndCallDeserializerWithSimpleGroupWhenReadIsCalled() throws IOException {
+        PrimitiveReader.PrimitiveReaderProvider provider = new PrimitiveReader.PrimitiveReaderProvider(deserializer);
+        ClassLoader classLoader = getClass().getClassLoader();
+        PrimitiveReader reader = provider.getReader(classLoader.getResource("test_file.parquet").getPath());
+
+        reader.read();
+        reader.read();
+        reader.read();
+
+        SimpleGroup[] allExpectedSimpleGroups = getSimpleGroups();
+        ArgumentCaptor<SimpleGroup> simpleGroupCaptor = ArgumentCaptor.forClass(SimpleGroup.class);
+        verify(deserializer, times(3)).deserialize(simpleGroupCaptor.capture());
+        List<SimpleGroup> allActualSimpleGroups = simpleGroupCaptor.getAllValues();
+
+        for (int i = 0; i < 3; i++) {
+            SimpleGroup expectedSimpleGroup = allExpectedSimpleGroups[i];
+            SimpleGroup actualSimpleGroup = allActualSimpleGroups.get(i);
+            assertEquals(expectedSimpleGroup.getType(), actualSimpleGroup.getType());
+            assertEquals(expectedSimpleGroup.getString("name", 0), actualSimpleGroup.getString("name", 0));
+            assertEquals(expectedSimpleGroup.getLong("age", 0), actualSimpleGroup.getLong("age", 0));
+            assertEquals(expectedSimpleGroup.getString("residence", 0), actualSimpleGroup.getString("residence", 0));
+        }
+    }
+
+    @Test
+    public void shouldBeAbleToReadParquetFileContainingMultipleRowGroups() throws IOException {
+        PrimitiveReader.PrimitiveReaderProvider provider = new PrimitiveReader.PrimitiveReaderProvider(deserializer);
+        ClassLoader classLoader = getClass().getClassLoader();
+        PrimitiveReader reader = provider.getReader(classLoader.getResource("multiple_row_groups_test_file.parquet").getPath());
+
+        reader.read();
+        reader.read();
+        reader.read();
+        reader.read();
+        reader.read();
+        reader.read();
+
+        SimpleGroup[] allExpectedSimpleGroups = ArrayUtils.addAll(getSimpleGroups(),getSimpleGroups());
+        ArgumentCaptor<SimpleGroup> simpleGroupCaptor = ArgumentCaptor.forClass(SimpleGroup.class);
+        verify(deserializer, times(6)).deserialize(simpleGroupCaptor.capture());
+        List<SimpleGroup> allActualSimpleGroups = simpleGroupCaptor.getAllValues();
+
+        for (int i = 0; i < 6; i++) {
+            SimpleGroup expectedSimpleGroup = allExpectedSimpleGroups[i];
+            SimpleGroup actualSimpleGroup = allActualSimpleGroups.get(i);
+            assertEquals(expectedSimpleGroup.getType(), actualSimpleGroup.getType());
+            assertEquals(expectedSimpleGroup.getString("name", 0), actualSimpleGroup.getString("name", 0));
+            assertEquals(expectedSimpleGroup.getLong("age", 0), actualSimpleGroup.getLong("age", 0));
+            assertEquals(expectedSimpleGroup.getString("residence", 0), actualSimpleGroup.getString("residence", 0));
+        }
+    }
+
+    @Test
+    public void shouldReturnDeserializedValueWhenRecordsPresentAndNullWhenNoMoreDataLeftToRead() throws IOException {
+        PrimitiveReader.PrimitiveReaderProvider provider = new PrimitiveReader.PrimitiveReaderProvider(deserializer);
+        ClassLoader classLoader = getClass().getClassLoader();
+        PrimitiveReader reader = provider.getReader(classLoader.getResource("test_file.parquet").getPath());
+        when(deserializer.deserialize(any(SimpleGroup.class))).thenReturn(Row.of("some value"));
+
+        assertEquals(Row.of("some value"), reader.read());
+        assertEquals(Row.of("some value"), reader.read());
+        assertEquals(Row.of("some value"), reader.read());
+        assertNull(reader.read());
+    }
+
+    @Test
+    public void shouldThrowNullPointerExceptionIfReadIsCalledAfterCallingClose() throws IOException {
+        PrimitiveReader.PrimitiveReaderProvider provider = new PrimitiveReader.PrimitiveReaderProvider(deserializer);
+        ClassLoader classLoader = getClass().getClassLoader();
+        PrimitiveReader reader = provider.getReader(classLoader.getResource("test_file.parquet").getPath());
+
+        reader.close();
+
+        assertThrows(NullPointerException.class, reader::read);
+    }
+
+    private SimpleGroup[] getSimpleGroups() {
+        GroupType expectedSchema = buildMessage()
+                .optional(BINARY).as(LogicalTypeAnnotation.stringType()).named("name")
+                .optional(INT64).named("age")
+                .optional(BINARY).as(LogicalTypeAnnotation.stringType()).named("residence")
+                .named("schema");
+
+        SimpleGroup group1 = new SimpleGroup(expectedSchema);
+        group1.add("name", "Ajay");
+        group1.add("age", 24L);
+        group1.add("residence", "Mumbai");
+
+        SimpleGroup group2 = new SimpleGroup(expectedSchema);
+        group2.add("name", "Utkarsh");
+        group2.add("age", 25L);
+        group2.add("residence", "Delhi");
+
+        SimpleGroup group3 = new SimpleGroup(expectedSchema);
+        group3.add("name", "Samay");
+        group3.add("age", 29L);
+        group3.add("residence", "Pune");
+
+        return new SimpleGroup[]{group1, group2, group3};
     }
 }
