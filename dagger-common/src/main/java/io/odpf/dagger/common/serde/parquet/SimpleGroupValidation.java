66dcf95e5e3ae969c0256332414ff132f8d57a83
Meghajit Mazumdar <Megh319@gmail.com>
2022-04-05 13:50:36 +0530

    feat: delete unused methods from SimpleGroupValidation
    [https://github.com/odpf/dagger/issues/99]

--- dagger-common/src/main/java/io/odpf/dagger/common/serde/parquet/SimpleGroupValidation.java
+++ dagger-common/src/main/java/io/odpf/dagger/common/serde/parquet/SimpleGroupValidation.java
@@ -1,50 +1,8 @@
 package io.odpf.dagger.common.serde.parquet;
 
 import org.apache.parquet.example.data.simple.SimpleGroup;
-import org.apache.parquet.schema.LogicalTypeAnnotation;
-import org.apache.parquet.schema.PrimitiveType;
-
-import java.util.Objects;
 
 public class SimpleGroupValidation {
-    public boolean checkSimpleGroupNotNull(SimpleGroup simpleGroup) {
-        return !Objects.isNull(simpleGroup);
-    }
-
-    public boolean checkFieldIsPrimitive(SimpleGroup simpleGroup, String fieldName) {
-        return simpleGroup.getType().getType(fieldName).isPrimitive();
-    }
-
-    public boolean checkFieldExistsInSimpleGroup(SimpleGroup simpleGroup, String fieldName) {
-        return simpleGroup.getType().containsField(fieldName);
-    }
-
-    public boolean checkLogicalTypeIsSupported(SimpleGroup simpleGroup, String fieldName, LogicalTypeAnnotation supportedLogicalTypeAnnotation) {
-        LogicalTypeAnnotation fieldLogicalTypeAnnotation = simpleGroup.getType()
-                .getType(fieldName)
-                .getLogicalTypeAnnotation();
-        return Objects.equals(supportedLogicalTypeAnnotation, fieldLogicalTypeAnnotation);
-    }
-
-    public boolean checkParserCanParsePrimitive(SimpleGroup simpleGroup, String fieldName, PrimitiveType.PrimitiveTypeName supportedPrimitiveTypeName) {
-        PrimitiveType.PrimitiveTypeName fieldPrimitiveType = simpleGroup.getType()
-                .getType(fieldName)
-                .asPrimitiveType()
-                .getPrimitiveTypeName();
-        return Objects.equals(supportedPrimitiveTypeName, fieldPrimitiveType);
-    }
-
-    public boolean applyValidations(SimpleGroup simpleGroup,
-                                    String fieldName,
-                                    PrimitiveType.PrimitiveTypeName supportedPrimitiveTypeName,
-                                    LogicalTypeAnnotation supportedLogicalTypeAnnotation) {
-        return this.checkSimpleGroupNotNull(simpleGroup)
-                && this.checkFieldExistsInSimpleGroup(simpleGroup, fieldName)
-                && this.checkFieldIsPrimitive(simpleGroup, fieldName)
-                && this.checkParserCanParsePrimitive(simpleGroup, fieldName, supportedPrimitiveTypeName)
-                && this.checkLogicalTypeIsSupported(simpleGroup, fieldName, supportedLogicalTypeAnnotation);
-    }
-
     public static boolean checkFieldExistsAndIsInitialized(SimpleGroup simpleGroup, String fieldName) {
         return simpleGroup.getType().containsField(fieldName) && simpleGroup.getFieldRepetitionCount(fieldName) != 0;
     }
--- dagger-common/src/test/java/io/odpf/dagger/common/serde/parquet/SimpleGroupValidationTest.java
+++ dagger-common/src/test/java/io/odpf/dagger/common/serde/parquet/SimpleGroupValidationTest.java
@@ -2,224 +2,44 @@ package io.odpf.dagger.common.serde.parquet;
 
 import org.apache.parquet.example.data.simple.SimpleGroup;
 import org.apache.parquet.schema.GroupType;
-import org.apache.parquet.schema.LogicalTypeAnnotation;
-import org.apache.parquet.schema.Types;
 import org.junit.Test;
 
-import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.*;
-import static org.junit.Assert.*;
+import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.INT32;
+import static org.apache.parquet.schema.Types.buildMessage;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class SimpleGroupValidationTest {
 
     @Test
-    public void checkSimpleGroupNotNullReturnsTrueIfSimpleGroupArgumentIsNotNullElseFalse() {
-        GroupType parquetSchema = Types.requiredGroup()
-                .required(BOOLEAN)
-                .named("some-column")
-                .named("TestGroupType");
-        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
-        SimpleGroupValidation simpleGroupValidation = new SimpleGroupValidation();
-
-        boolean actualResult1 = simpleGroupValidation.checkSimpleGroupNotNull(simpleGroup);
-        boolean actualResult2 = simpleGroupValidation.checkSimpleGroupNotNull(null);
-
-        assertTrue(actualResult1);
-        assertFalse(actualResult2);
-    }
-
-    @Test
-    public void checkFieldIsPrimitiveReturnsTrueIfFieldIsOfPrimitiveTypeElseFalse() {
-        GroupType parquetSchema = Types.requiredGroup()
-                .required(INT32).named("primitive-type-column")
-                .optionalGroup()
-                .required(BOOLEAN).named("boolean-nested-column")
-                .named("complex-type-column")
-                .named("TestGroupType");
-        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
-        SimpleGroupValidation simpleGroupValidation = new SimpleGroupValidation();
-
-        boolean actualResultForPrimitive = simpleGroupValidation.checkFieldIsPrimitive(simpleGroup, "primitive-type-column");
-        boolean actualResultForComplex = simpleGroupValidation.checkFieldIsPrimitive(simpleGroup, "complex-type-column");
-
-        assertTrue(actualResultForPrimitive);
-        assertFalse(actualResultForComplex);
-    }
-
-    @Test
-    public void checkFieldExistsInSimpleGroupReturnsTrueIfSimpleGroupContainsTheFieldInItsSchemaElseFalse() {
-        GroupType parquetSchema = Types.requiredGroup()
-                .required(INT32).named("primitive-type-column")
-                .named("TestGroupType");
-        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
-        SimpleGroupValidation simpleGroupValidation = new SimpleGroupValidation();
-
-        boolean actualResultForExistingField = simpleGroupValidation.checkFieldExistsInSimpleGroup(simpleGroup, "primitive-type-column");
-        boolean actualResultForNonExistingField = simpleGroupValidation.checkFieldExistsInSimpleGroup(simpleGroup, "this-column-does-not-exist");
-
-        assertTrue(actualResultForExistingField);
-        assertFalse(actualResultForNonExistingField);
-    }
-
-    @Test
-    public void checkPrimitiveTypeIsSupportedReturnsTrueIfPrimitiveTypeNameIsSameAsProvidedArgumentElseFalse() {
-        GroupType parquetSchema = Types.requiredGroup()
-                .required(INT64).named("column-with-int64-type")
-                .named("TestGroupType");
-        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
-        SimpleGroupValidation simpleGroupValidation = new SimpleGroupValidation();
-
-        boolean actualResultForSupportedField = simpleGroupValidation.checkParserCanParsePrimitive(simpleGroup, "column-with-int64-type", INT64);
-        boolean actualResultForNonSupportedField = simpleGroupValidation.checkParserCanParsePrimitive(simpleGroup, "column-with-int64-type", BOOLEAN);
-
-        assertTrue(actualResultForSupportedField);
-        assertFalse(actualResultForNonSupportedField);
-    }
-
-    @Test
-    public void checkLogicalTypeIsSupportedReturnsTrueIfLogicalTypeOfFieldNameIsSameAsProvidedArgumentElseFalse() {
-        GroupType parquetSchema = Types.requiredGroup()
-                .required(INT64)
-                .as(LogicalTypeAnnotation.timestampType(true, LogicalTypeAnnotation.TimeUnit.MILLIS))
-                .named("column-with-timestamp-type")
-                .named("TestGroupType");
-        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
-        SimpleGroupValidation simpleGroupValidation = new SimpleGroupValidation();
-
-        boolean actualResultForSupportedField = simpleGroupValidation.checkLogicalTypeIsSupported(
-                simpleGroup, "column-with-timestamp-type",
-                LogicalTypeAnnotation.timestampType(true, LogicalTypeAnnotation.TimeUnit.MILLIS));
-        boolean actualResultForNonSupportedField = simpleGroupValidation.checkLogicalTypeIsSupported(
-                simpleGroup, "column-with-timestamp-type",
-                LogicalTypeAnnotation.timestampType(false, LogicalTypeAnnotation.TimeUnit.MILLIS));
-
-        assertTrue(actualResultForSupportedField);
-        assertFalse(actualResultForNonSupportedField);
-    }
-
-    @Test
-    public void checkLogicalTypeIsSupportedReturnsTrueIfBothLogicalTypeOfFieldNameAndSupportedLogicalTypeIsNull() {
-        GroupType parquetSchema = Types.requiredGroup()
-                .required(INT64).named("column-with-int64-type")
-                .named("TestGroupType");
-        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
-        SimpleGroupValidation simpleGroupValidation = new SimpleGroupValidation();
-
-        boolean actualResult = simpleGroupValidation.checkLogicalTypeIsSupported(simpleGroup, "column-with-int64-type", null);
-
-        assertTrue(actualResult);
-    }
-
-    @Test
-    public void applyValidationsReturnsTrueOnlyIfAllValidatorsReturnTrue() {
-        GroupType parquetSchema = Types.requiredGroup()
-                .required(INT64).named("column-with-int64-type")
-                .named("TestGroupType");
-        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
-        SimpleGroupValidation simpleGroupValidation = new SimpleGroupValidation();
-
-        boolean actualResult = simpleGroupValidation.applyValidations(simpleGroup,
-                "column-with-int64-type",
-                INT64, null);
-
-        assertTrue(actualResult);
-    }
-
-    @Test
-    public void applyValidationsReturnsFalseIfCheckSimpleGroupNotNullValidationFails() {
-        SimpleGroupValidation simpleGroupValidation = new SimpleGroupValidation();
-
-        boolean actualResult = simpleGroupValidation.applyValidations(null, "some-column", INT64, null);
-
-        assertFalse(actualResult);
-    }
-
-    @Test
-    public void applyValidationsReturnsFalseIfCheckFieldExistsInSimpleGroupValidationFails() {
-        GroupType parquetSchema = Types.requiredGroup()
-                .required(INT64).named("column-with-int64-type")
-                .named("TestGroupType");
-        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
-        SimpleGroupValidation simpleGroupValidation = new SimpleGroupValidation();
-
-        boolean actualResult = simpleGroupValidation.applyValidations(simpleGroup, "some-column", INT64, null);
-
-        assertFalse(actualResult);
-    }
-
-    @Test
-    public void applyValidationsReturnsFalseIfCheckFieldIsPrimitiveValidationFails() {
-        GroupType parquetSchema = Types.requiredGroup()
-                .required(INT32).named("primitive-type-column")
-                .optionalGroup()
-                .required(BOOLEAN).named("boolean-nested-column")
-                .named("complex-type-column")
-                .named("TestGroupType");
-        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
-        SimpleGroupValidation simpleGroupValidation = new SimpleGroupValidation();
-
-        boolean actualResult = simpleGroupValidation.applyValidations(simpleGroup, "complex-type-column", INT64, null);
-
-        assertFalse(actualResult);
-    }
-
-    @Test
-    public void applyValidationsReturnsFalseIfCheckParserCanParsePrimitiveValidationFails() {
-        GroupType parquetSchema = Types.requiredGroup()
-                .required(INT64).named("column-with-int64-type")
-                .named("TestGroupType");
-        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
-        SimpleGroupValidation simpleGroupValidation = new SimpleGroupValidation();
-
-        boolean actualResult = simpleGroupValidation.applyValidations(simpleGroup, "column-with-int64-type", BOOLEAN, null);
-
-        assertFalse(actualResult);
-    }
-
-    @Test
-    public void applyValidationsReturnsFalseIfCheckLogicalTypeIsSupportedValidationFails() {
-        GroupType parquetSchema = Types.requiredGroup()
-                .required(BINARY).as(LogicalTypeAnnotation.enumType())
-                .named("column-with-binary-enum-type")
-                .named("TestGroupType");
-        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
-        SimpleGroupValidation simpleGroupValidation = new SimpleGroupValidation();
-
-        boolean actualResult = simpleGroupValidation.applyValidations(simpleGroup,
-                "column-with-int64-type", BOOLEAN,
-                LogicalTypeAnnotation.stringType());
-
-        assertFalse(actualResult);
-    }
-
-    @Test
-    public void checkFieldExistsAndIsInitializedReturnsTrueOnlyIfSimpleGroupContainsTheFieldInItsSchemaAndIsInitialized() {
-        GroupType parquetSchema = Types.requiredGroup()
-                .required(INT32).named("primitive-type-column")
-                .named("TestGroupType");
-        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
-        simpleGroup.add("primitive-type-column", 45);
-
-        assertTrue(SimpleGroupValidation.checkFieldExistsAndIsInitialized(simpleGroup, "primitive-type-column"));
-    }
-
-    @Test
-    public void checkFieldExistsAndIsInitializedReturnsFalseIfSimpleGroupDoesNotContainTheField() {
-        GroupType parquetSchema = Types.requiredGroup()
-                .required(INT32).named("primitive-type-column")
-                .named("TestGroupType");
-        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
-        simpleGroup.add("primitive-type-column", 45);
-
-        assertFalse(SimpleGroupValidation.checkFieldExistsAndIsInitialized(simpleGroup, "random-column"));
-    }
-
-    @Test
-    public void checkFieldExistsAndIsInitializedReturnsFalseIfFieldIsNotInitializedInSimpleGroup() {
-        GroupType parquetSchema = Types.requiredGroup()
+    public void checkFieldExistsAndIsInitializedShouldReturnFalseWhenFieldIsNotInitializedInSimpleGroup() {
+        GroupType parquetSchema = buildMessage()
                 .required(INT32).named("primitive-type-column")
                 .named("TestGroupType");
         SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
 
         assertFalse(SimpleGroupValidation.checkFieldExistsAndIsInitialized(simpleGroup, "primitive-type-column"));
+    }
+
+    @Test
+    public void checkFieldExistsAndIsInitializedShouldReturnFalseWhenFieldIsNotPresentInSimpleGroup() {
+        GroupType parquetSchema = buildMessage()
+                .required(INT32).named("primitive-type-column")
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+        simpleGroup.add("primitive-type-column", 34);
+
+        assertFalse(SimpleGroupValidation.checkFieldExistsAndIsInitialized(simpleGroup, "unknown-column"));
+    }
+
+    @Test
+    public void checkFieldExistsAndIsInitializedShouldReturnTrueWhenFieldIsBothPresentAndInitializedInSimpleGroup() {
+        GroupType parquetSchema = buildMessage()
+                .required(INT32).named("primitive-type-column")
+                .named("TestGroupType");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+        simpleGroup.add("primitive-type-column", 34);
+
+        assertTrue(SimpleGroupValidation.checkFieldExistsAndIsInitialized(simpleGroup, "primitive-type-column"));
     }
 }
