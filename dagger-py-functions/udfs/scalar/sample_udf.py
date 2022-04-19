1479be9a6157ce35a283be9bd33f051337f42e0c
mayur.gubrele <2310-mayur.gubrele@users.noreply.source.golabs.io>
2022-04-19 15:21:35 +0530

    refactor: remove metric register part for sample_udf

--- dagger-py-functions/udfs/scalar/sample_udf.py
+++ dagger-py-functions/udfs/scalar/sample_udf.py
@@ -3,15 +3,7 @@ from pyflink.table.udf import udf
 
 class SampleUdf(ScalarFunction):
 
-    def __init__(self):
-        self.counter = None
-
-    def open(self, function_context):
-        mg = function_context.get_metric_group()
-        self.counter = mg.add_group("udf", "SampleUdf").counter("value")
-
     def eval(self, text: str):
-        self.counter.inc()
         return text + "_added_text"
 
 sample_udf = udf(SampleUdf(), result_type=DataTypes.STRING())
