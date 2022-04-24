85e978fed56c253e8509fe4cff6a8f8bd57a50a9
jesrypandawa <jesry.p.pandawa@gmail.com>
2022-04-25 01:50:21 +0700

    fix: fix python file test

--- dagger-functions/src/test/resources/test_udf.py
+++ dagger-functions/src/test/resources/test_udf.py
@@ -1,17 +1,7 @@
-from pyflink.table import ScalarFunction, DataTypes
+from pyflink.table import DataTypes
 from pyflink.table.udf import udf
 
-class TestUdf(ScalarFunction):
 
-    def __init__(self):
-        self.counter = None
-
-    def open(self, function_context):
-        mg = function_context.get_metric_group()
-        self.counter = mg.add_group("udf", "TestUdf").counter("value")
-
-    def eval(self, text: str):
-        self.counter.inc()
-        return text + "_added_text"
-
-test_udf = udf(TestUdf(), result_type=DataTypes.STRING())
+@udf(result_type=DataTypes.STRING())
+def test_function(text: str):
+    return text + "_added_text"
