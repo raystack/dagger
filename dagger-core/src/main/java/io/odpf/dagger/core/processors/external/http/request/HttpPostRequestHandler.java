0dc9c8768d4105cb6c48076c33f58337cd8787b4
Mayank Rai <mayank.rai@gojek.com>
2022-04-28 15:04:41 +0530

    fix: improved unit test related to dynamic header and better error reporting with invalid configuration

--- dagger-core/src/main/java/io/odpf/dagger/core/processors/external/http/request/HttpPostRequestHandler.java
+++ dagger-core/src/main/java/io/odpf/dagger/core/processors/external/http/request/HttpPostRequestHandler.java
@@ -2,12 +2,14 @@ package io.odpf.dagger.core.processors.external.http.request;
 
 import com.google.gson.Gson;
 import io.netty.util.internal.StringUtil;
+import io.odpf.dagger.core.exception.InvalidConfigurationException;
 import io.odpf.dagger.core.processors.external.http.HttpSourceConfig;
 import org.asynchttpclient.AsyncHttpClient;
 import org.asynchttpclient.BoundRequestBuilder;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UnknownFormatConversionException;
 
 /**
  * The Http post request handler.
@@ -40,8 +42,14 @@ public class HttpPostRequestHandler implements HttpRequestHandler {
                 .setBody(requestBody);
         Map<String, String> headers = httpSourceConfig.getHeaders();
         if (!StringUtil.isNullOrEmpty(httpSourceConfig.getHeaderPattern())) {
-            String dynamicHeader = String.format(httpSourceConfig.getHeaderPattern(), dynamicHeaderVariablesValues);
-            headers.putAll(new Gson().fromJson(dynamicHeader, HashMap.class));
+            try {
+                String dynamicHeader = String.format(httpSourceConfig.getHeaderPattern(), dynamicHeaderVariablesValues);
+                headers.putAll(new Gson().fromJson(dynamicHeader, HashMap.class));
+            } catch (UnknownFormatConversionException e) {
+                throw new InvalidConfigurationException(String.format("pattern config '%s' is invalid", httpSourceConfig.getHeaderPattern()));
+            } catch (IllegalArgumentException e) {
+                throw new InvalidConfigurationException(String.format("pattern config '%s' is incompatible with the variable config '%s'", httpSourceConfig.getHeaderPattern(), httpSourceConfig.getHeaderVariables()));
+            }
         }
         return addHeaders(postRequest, headers);
     }
