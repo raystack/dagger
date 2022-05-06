cdf0952c2fa63e1b87e0937d670d8dadfc4d211a
Meghajit Mazumdar <Megh319@gmail.com>
2022-05-06 19:10:57 +0530

    feat: add methods to check for SimpleGroup Map schema spec
    - add validation methods to check if SimpleGroup map
    schema follows Apache Parquet LogicalTypes spec or legacy one
    - official spec
    https://github.com/apache/parquet-format/blob/master/LogicalTypes.md#backward-compatibility-rules-1
    - add some tests
    
    [https://github.com/odpf/dagger/issues/137]

--- dagger-common/src/test/java/io/odpf/dagger/common/serde/parquet/SimpleGroupValidationTest.java
+++ dagger-common/src/test/java/io/odpf/dagger/common/serde/parquet/SimpleGroupValidationTest.java
@@ -2,10 +2,15 @@ package io.odpf.dagger.common.serde.parquet;
 
 import org.apache.parquet.example.data.simple.SimpleGroup;
 import org.apache.parquet.schema.GroupType;
+import org.apache.parquet.schema.MessageType;
 import org.junit.Test;
 
+import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.DOUBLE;
+import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.FLOAT;
 import static org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.INT32;
 import static org.apache.parquet.schema.Types.buildMessage;
+import static org.apache.parquet.schema.Types.repeatedGroup;
+import static org.apache.parquet.schema.Types.requiredGroup;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -41,5 +46,86 @@ public class SimpleGroupValidationTest {
         simpleGroup.add("primitive-type-column", 34);
 
         assertTrue(SimpleGroupValidation.checkFieldExistsAndIsInitialized(simpleGroup, "primitive-type-column"));
+    }
+
+    @Test
+    public void checkIsLegacySimpleGroupMapShouldReturnFalseWhenMapFieldIsNotOfTypeGroupType() {
+        MessageType parquetSchema = buildMessage()
+                .required(INT32).named("sample_map_field")
+                .named("TestMessage");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+
+        assertFalse(SimpleGroupValidation.checkIsLegacySimpleGroupMap(simpleGroup, "sample_map_field"));
+    }
+
+    @Test
+    public void checkIsLegacySimpleGroupMapShouldReturnFalseWhenMapFieldIsNotRepeated() {
+        GroupType mapSchema = requiredGroup()
+                .optional(INT32).named("key")
+                .optional(FLOAT).named("value")
+                .named("sample_map_field");
+        MessageType parquetSchema = buildMessage()
+                .addField(mapSchema)
+                .named("TestMessage");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+
+        assertFalse(SimpleGroupValidation.checkIsLegacySimpleGroupMap(simpleGroup, "sample_map_field"));
+    }
+
+    @Test
+    public void checkIsLegacySimpleGroupMapShouldReturnFalseWhenMapFieldSchemaDoesNotHaveCorrectNumberOfNestedFields() {
+        GroupType mapSchema = repeatedGroup()
+                .optional(INT32).named("key")
+                .optional(FLOAT).named("value")
+                .optional(DOUBLE).named("extra_field")
+                .named("sample_map_field");
+        MessageType parquetSchema = buildMessage()
+                .addField(mapSchema)
+                .named("TestMessage");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+
+        assertFalse(SimpleGroupValidation.checkIsLegacySimpleGroupMap(simpleGroup, "sample_map_field"));
+    }
+
+    @Test
+    public void checkIsLegacySimpleGroupMapShouldReturnFalseWhenMapFieldSchemaDoesNotHaveKey() {
+        GroupType mapSchema = repeatedGroup()
+                .optional(FLOAT).named("value")
+                .optional(DOUBLE).named("extra_field")
+                .named("sample_map_field");
+        MessageType parquetSchema = buildMessage()
+                .addField(mapSchema)
+                .named("TestMessage");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+
+        assertFalse(SimpleGroupValidation.checkIsLegacySimpleGroupMap(simpleGroup, "sample_map_field"));
+    }
+
+    @Test
+    public void checkIsLegacySimpleGroupMapShouldReturnFalseWhenMapFieldSchemaDoesNotHaveValue() {
+        GroupType mapSchema = repeatedGroup()
+                .optional(FLOAT).named("key")
+                .optional(DOUBLE).named("extra_field")
+                .named("sample_map_field");
+        MessageType parquetSchema = buildMessage()
+                .addField(mapSchema)
+                .named("TestMessage");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+
+        assertFalse(SimpleGroupValidation.checkIsLegacySimpleGroupMap(simpleGroup, "sample_map_field"));
+    }
+
+    @Test
+    public void checkIsLegacySimpleGroupMapShouldReturnTrueWhenMapFieldConformsToTheLegacySchema() {
+        GroupType mapSchema = repeatedGroup()
+                .optional(INT32).named("key")
+                .optional(FLOAT).named("value")
+                .named("sample_map_field");
+        MessageType parquetSchema = buildMessage()
+                .addField(mapSchema)
+                .named("TestMessage");
+        SimpleGroup simpleGroup = new SimpleGroup(parquetSchema);
+
+        assertTrue(SimpleGroupValidation.checkIsLegacySimpleGroupMap(simpleGroup, "sample_map_field"));
     }
 }
