2e1f15efa23a8d08343fe2795b91c0592ba841b7
jesrypandawa <jesry.p.pandawa@gmail.com>
2022-06-08 19:07:10 +0700

    chore: refactor python files structure and sample udf

--- dagger-py-functions/udfs/scalar/sample.py
+++ dagger-py-functions/udfs/scalar/sample.py
@@ -4,9 +4,6 @@ from pyflink.table.udf import udf
 
 @udf(result_type=DataTypes.STRING())
 def sample(text):
-    try:
-        file = open("data.zip/data/sample_data.txt", "r")
-    except FileNotFoundError:
-        file = open("data/sample_data.txt", "r")
+    file = open("data/sample_data.txt", "r")
     data = file.read()
     return text + data
