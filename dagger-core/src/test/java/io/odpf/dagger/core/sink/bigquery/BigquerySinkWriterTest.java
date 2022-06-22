9847321c59898022dc10b66102f76c46ca9bd1fe
lavkesh <lavkesh51@gmail.com>
2022-06-22 11:09:31 +0200

    test: adding more test

--- dagger-core/src/test/java/io/odpf/dagger/core/sink/bigquery/BigquerySinkWriterTest.java
+++ dagger-core/src/test/java/io/odpf/dagger/core/sink/bigquery/BigquerySinkWriterTest.java
@@ -73,10 +73,11 @@ public class BigquerySinkWriterTest {
         Mockito.when(protoSerializer.serializeValue(row)).thenReturn("testMessage".getBytes());
         OdpfSinkResponse response = Mockito.mock(OdpfSinkResponse.class);
         Mockito.when(response.hasErrors()).thenReturn(true);
-        Mockito.when(response.getErrors()).thenReturn(new HashMap<Long, ErrorInfo>() {{
+        Map<Long, ErrorInfo> errorInfoMap = new HashMap<Long, ErrorInfo>() {{
             put(1L, new ErrorInfo(new Exception("test1"), ErrorType.DESERIALIZATION_ERROR));
             put(2L, new ErrorInfo(new Exception("test2"), ErrorType.SINK_4XX_ERROR));
-        }});
+        }};
+        Mockito.when(response.getErrors()).thenReturn(errorInfoMap);
         Mockito.when(sink.pushToSink(Mockito.anyList())).thenReturn(response);
         bigquerySinkWriter.write(row, null);
         bigquerySinkWriter.write(row, null);
@@ -84,6 +85,38 @@ public class BigquerySinkWriterTest {
         Assert.assertEquals("Error occurred during writing to Bigquery", ioException.getMessage());
         Mockito.verify(sink, Mockito.times(1)).pushToSink(Mockito.anyList());
         Mockito.verify(response, Mockito.times(1)).hasErrors();
+        Mockito.verify(reporter, Mockito.times(0)).reportNonFatalException(Mockito.any());
+        Mockito.verify(reporter, Mockito.times(1)).reportFatalException(errorInfoMap.get(1L).getException());
+        Mockito.verify(reporter, Mockito.times(1)).reportFatalException(errorInfoMap.get(2L).getException());
+    }
+
+    @Test
+    public void shouldNotThrowExceptionIfErrorTypeNotConfigured() throws IOException {
+        ProtoSerializer protoSerializer = Mockito.mock(ProtoSerializer.class);
+        OdpfSink sink = Mockito.mock(OdpfSink.class);
+        ErrorReporter reporter = Mockito.mock(ErrorReporter.class);
+        Set<ErrorType> errorTypesForFailing = Collections.emptySet();
+        BigquerySinkWriter bigquerySinkWriter = new BigquerySinkWriter(protoSerializer, sink, 3, reporter, errorTypesForFailing);
+        Row row = new Row(1);
+        row.setField(0, "some field");
+        Mockito.when(protoSerializer.serializeKey(row)).thenReturn("test".getBytes());
+        Mockito.when(protoSerializer.serializeValue(row)).thenReturn("testMessage".getBytes());
+        OdpfSinkResponse response = Mockito.mock(OdpfSinkResponse.class);
+        Mockito.when(response.hasErrors()).thenReturn(true);
+        Map<Long, ErrorInfo> errorInfoMap = new HashMap<Long, ErrorInfo>() {{
+            put(1L, new ErrorInfo(new Exception("test1"), ErrorType.DESERIALIZATION_ERROR));
+            put(2L, new ErrorInfo(new Exception("test2"), ErrorType.SINK_4XX_ERROR));
+        }};
+        Mockito.when(response.getErrors()).thenReturn(errorInfoMap);
+        Mockito.when(sink.pushToSink(Mockito.anyList())).thenReturn(response);
+        bigquerySinkWriter.write(row, null);
+        bigquerySinkWriter.write(row, null);
+        bigquerySinkWriter.write(row, null);
+        Mockito.verify(sink, Mockito.times(1)).pushToSink(Mockito.anyList());
+        Mockito.verify(response, Mockito.times(1)).hasErrors();
+        Mockito.verify(reporter, Mockito.times(0)).reportFatalException(Mockito.any());
+        Mockito.verify(reporter, Mockito.times(1)).reportNonFatalException(errorInfoMap.get(1L).getException());
+        Mockito.verify(reporter, Mockito.times(1)).reportNonFatalException(errorInfoMap.get(2L).getException());
     }
 
     @Test
