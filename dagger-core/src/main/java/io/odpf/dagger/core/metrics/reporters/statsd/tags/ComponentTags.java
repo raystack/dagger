ec6bd630e62da65a814f9049be2e607383f8e8ac
Meghajit Mazumdar <Megh319@gmail.com>
2022-07-07 12:53:21 +0530

    feat: delete dagger source tag
    [https://github.com/odpf/dagger/issues/108]

--- dagger-core/src/main/java/io/odpf/dagger/core/metrics/reporters/statsd/tags/ComponentTags.java
+++ dagger-core/src/main/java/io/odpf/dagger/core/metrics/reporters/statsd/tags/ComponentTags.java
@@ -4,13 +4,11 @@ public class ComponentTags {
     private static StatsDTag[] parquetReaderTags;
     private static StatsDTag[] splitAssignerTags;
     private static final String COMPONENT_TAG_KEY = "component";
-    private static final String DAGGER_SOURCE_TAG_KEY = "dagger_source";
 
     public static StatsDTag[] getParquetReaderTags() {
         if (parquetReaderTags == null) {
             parquetReaderTags = new StatsDTag[]{
                     new StatsDTag(COMPONENT_TAG_KEY, "parquet_reader"),
-                    new StatsDTag(DAGGER_SOURCE_TAG_KEY, "parquet_source")
             };
         }
         return parquetReaderTags;
@@ -20,7 +18,6 @@ public class ComponentTags {
         if (splitAssignerTags == null) {
             splitAssignerTags = new StatsDTag[]{
                     new StatsDTag(COMPONENT_TAG_KEY, "split_assigner"),
-                    new StatsDTag(DAGGER_SOURCE_TAG_KEY, "parquet_source")
             };
         }
         return splitAssignerTags;
