b8540221bb0a36b0526a47bf802e499067953c4e
Meghajit Mazumdar <Megh319@gmail.com>
2022-07-08 21:00:06 +0530

    feat: delete unused measurements
    [https://github.com/odpf/dagger/issues/108]

--- dagger-core/src/main/java/io/odpf/dagger/core/metrics/aspects/ParquetReaderAspects.java
+++ dagger-core/src/main/java/io/odpf/dagger/core/metrics/aspects/ParquetReaderAspects.java
@@ -8,9 +8,7 @@ public enum ParquetReaderAspects implements Aspects {
     READER_CLOSED("reader_closed", AspectType.Counter),
     READER_ROWS_EMITTED("reader_rows_emitted", AspectType.Counter),
     READER_ROW_DESERIALIZATION_TIME("reader_row_deserialization_time", AspectType.Histogram),
-    READER_ROW_READ_TIME("reader_row_read_time", AspectType.Histogram),
-    READER_CREATION_EXCEPTION("reader_creation_exception", AspectType.Counter),
-    READER_DESERIALIZATION_EXCEPTION("reader_deserialization_exception", AspectType.Counter);
+    READER_ROW_READ_TIME("reader_row_read_time", AspectType.Histogram);
 
     private final String value;
     private final AspectType aspectType;
