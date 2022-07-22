3003c009682c8ded1d97af3793b9cfe3e0752ae8
Harikrishna <harikrishnakanchi@gmail.com>
2022-07-22 23:49:43 +0530

    fix: handle composite map types

--- dagger-common/src/main/java/io/odpf/dagger/common/serde/typehandler/complex/MapHandler.java
+++ dagger-common/src/main/java/io/odpf/dagger/common/serde/typehandler/complex/MapHandler.java
@@ -2,11 +2,11 @@ package io.odpf.dagger.common.serde.typehandler.complex;
 
 import com.google.protobuf.Descriptors;
 import com.google.protobuf.DynamicMessage;
-import com.google.protobuf.MapEntry;
-import com.google.protobuf.WireFormat;
-import io.odpf.dagger.common.serde.typehandler.TypeHandler;
 import io.odpf.dagger.common.serde.typehandler.RowFactory;
+import io.odpf.dagger.common.serde.typehandler.TypeHandler;
+import io.odpf.dagger.common.serde.typehandler.TypeHandlerFactory;
 import io.odpf.dagger.common.serde.typehandler.TypeInformationFactory;
+import io.odpf.dagger.common.serde.typehandler.repeated.RepeatedMessageHandler;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.types.Row;
@@ -27,6 +27,7 @@ import static io.odpf.dagger.common.serde.parquet.SimpleGroupValidation.checkIsS
 public class MapHandler implements TypeHandler {
 
     private Descriptors.FieldDescriptor fieldDescriptor;
+    private TypeHandler repeatedMessageHandler;
 
     /**
      * Instantiates a new Map proto handler.
@@ -35,6 +36,7 @@ public class MapHandler implements TypeHandler {
      */
     public MapHandler(Descriptors.FieldDescriptor fieldDescriptor) {
         this.fieldDescriptor = fieldDescriptor;
+        this.repeatedMessageHandler = new RepeatedMessageHandler(fieldDescriptor);
     }
 
     @Override
@@ -47,38 +49,44 @@ public class MapHandler implements TypeHandler {
         if (!canHandle() || field == null) {
             return builder;
         }
-
         if (field instanceof Map) {
-            convertFromMap(builder, (Map<String, String>) field);
+            Map<?, ?> mapField = (Map<?, ?>) field;
+            ArrayList<Row> rows = new ArrayList<>();
+            for (Entry<?, ?> entry : mapField.entrySet()) {
+                rows.add(Row.of(entry.getKey(), entry.getValue()));
+            }
+            return repeatedMessageHandler.transformToProtoBuilder(builder, rows.toArray());
         }
-
-        if (field instanceof Object[]) {
-            convertFromRow(builder, (Object[]) field);
-        }
-
-        return builder;
+        return repeatedMessageHandler.transformToProtoBuilder(builder, field);
     }
 
     @Override
     public Object transformFromPostProcessor(Object field) {
         ArrayList<Row> rows = new ArrayList<>();
-        if (field != null) {
-            Map<String, String> mapField = (Map<String, String>) field;
-            for (Entry<String, String> entry : mapField.entrySet()) {
-                rows.add(getRowFromMap(entry));
+        if (field == null) {
+            return rows.toArray();
+        }
+        if (field instanceof Map) {
+            Map<String, ?> mapField = (Map<String, ?>) field;
+            for (Entry<String, ?> entry : mapField.entrySet()) {
+                Descriptors.FieldDescriptor keyDescriptor = fieldDescriptor.getMessageType().findFieldByName("key");
+                Descriptors.FieldDescriptor valueDescriptor = fieldDescriptor.getMessageType().findFieldByName("value");
+                TypeHandler handler = TypeHandlerFactory.getTypeHandler(keyDescriptor);
+                Object key = handler.transformFromPostProcessor(entry.getKey());
+                Object value = TypeHandlerFactory.getTypeHandler(valueDescriptor).transformFromPostProcessor(entry.getValue());
+                rows.add(Row.of(key, value));
             }
+            return rows.toArray();
+        }
+        if (field instanceof List) {
+            return repeatedMessageHandler.transformFromPostProcessor(field);
         }
         return rows.toArray();
     }
 
     @Override
     public Object transformFromProto(Object field) {
-        ArrayList<Row> rows = new ArrayList<>();
-        if (field != null) {
-            List<DynamicMessage> protos = (List<DynamicMessage>) field;
-            protos.forEach(proto -> rows.add(getRowFromMap(proto)));
-        }
-        return rows.toArray();
+        return repeatedMessageHandler.transformFromProto(field);
     }
 
     @Override
@@ -126,54 +134,5 @@ public class MapHandler implements TypeHandler {
     @Override
     public TypeInformation getTypeInformation() {
         return Types.OBJECT_ARRAY(TypeInformationFactory.getRowType(fieldDescriptor.getMessageType()));
-    }
-
-    private Row getRowFromMap(Entry<String, String> entry) {
-        Row row = new Row(2);
-        row.setField(0, entry.getKey());
-        row.setField(1, entry.getValue());
-        return row;
-    }
-
-    private Row getRowFromMap(DynamicMessage proto) {
-        Row row = new Row(2);
-        row.setField(0, parse(proto, "key"));
-        row.setField(1, parse(proto, "value"));
-        return row;
-    }
-
-    private Object parse(DynamicMessage proto, String fieldName) {
-        Object field = proto.getField(proto.getDescriptorForType().findFieldByName(fieldName));
-        if (DynamicMessage.class.equals(field.getClass())) {
-            field = RowFactory.createRow((DynamicMessage) field);
-        }
-        return field;
-    }
-
-    private void convertFromRow(DynamicMessage.Builder builder, Object[] field) {
-        for (Object inputValue : field) {
-            Row inputRow = (Row) inputValue;
-            if (inputRow.getArity() != 2) {
-                throw new IllegalArgumentException("Row: " + inputRow.toString() + " of size: " + inputRow.getArity() + " cannot be converted to map");
-            }
-            MapEntry<String, String> mapEntry = MapEntry
-                    .newDefaultInstance(fieldDescriptor.getMessageType(), WireFormat.FieldType.STRING, "", WireFormat.FieldType.STRING, "");
-            builder.addRepeatedField(fieldDescriptor,
-                    mapEntry.toBuilder()
-                            .setKey((String) inputRow.getField(0))
-                            .setValue((String) inputRow.getField(1))
-                            .buildPartial());
-        }
-    }
-
-    private void convertFromMap(DynamicMessage.Builder builder, Map<String, String> field) {
-        for (Entry<String, String> entry : field.entrySet()) {
-            MapEntry<String, String> mapEntry = MapEntry.newDefaultInstance(fieldDescriptor.getMessageType(), WireFormat.FieldType.STRING, "", WireFormat.FieldType.STRING, "");
-            builder.addRepeatedField(fieldDescriptor,
-                    mapEntry.toBuilder()
-                            .setKey(entry.getKey())
-                            .setValue(entry.getValue())
-                            .buildPartial());
-        }
     }
 }
