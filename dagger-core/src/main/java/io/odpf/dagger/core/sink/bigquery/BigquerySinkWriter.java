39f8906a399ff0d85cb464ad529be80dc45de8c2
lavkesh <lavkesh51@gmail.com>
2022-07-28 12:30:30 +0100

    chore: fix checkstyle

--- dagger-core/src/main/java/io/odpf/dagger/core/sink/bigquery/BigquerySinkWriter.java
+++ dagger-core/src/main/java/io/odpf/dagger/core/sink/bigquery/BigquerySinkWriter.java
@@ -14,7 +14,11 @@ import org.apache.flink.api.connector.sink.SinkWriter;
 import org.apache.flink.types.Row;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Slf4j
