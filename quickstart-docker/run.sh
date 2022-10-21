2fd5d8e26f35a5fc5b214d60e433ee65138fe0cf
sumitaich1998 <aich.1998@gmail.com>
2022-10-21 17:56:47 +0530

    docs: docker-compose setup for dagger quickstart

--- quickstart-docker/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-docker compose up
-sudo apt-get update
-sudo apt-get install protoc
-protoc --proto_path=./ --encode=io.odpf.dagger.consumer.TestPrimitiveMessage ./TestLogMessage.proto < ./sample_message.txt > out.bin
