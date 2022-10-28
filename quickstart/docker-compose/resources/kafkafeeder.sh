b4e18c7419da43148c273110ab1b84c2bcd89e28
sumitaich1998 <aich.1998@gmail.com>
2022-10-28 16:03:25 +0530

    fix: fixed review comments

--- quickstart/docker-compose/resources/kafkafeeder.sh
+++ quickstart/docker-compose/resources/kafkafeeder.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 timestamp_now=$(date +%s)
-random_3char_suffix=$(openssl rand -base64 3)
+random_3char_suffix=$(pwgen 3 1)
 random_enum_index=$(($RANDOM %3))
 declare -a myArray=("FLIGHT" "BUS" "TRAIN")
 cat sample_message.txt | \
