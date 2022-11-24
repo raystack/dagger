3bd8efef560b9b96d979d4b6d7a4908f541ee596
sumitaich1998 <aich.1998@gmail.com>
2022-11-24 17:18:10 +0530

    docs: docker compose setup

--- quickstart/examples/enrichment/resources/kafkafeeder.sh
+++ quickstart/examples/enrichment/resources/kafkafeeder.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 timestamp_now=$(date +%s)
-random_3char_suffix=$(pwgen 3 1)
 random_enum_index=$(($RANDOM %3))
+random_customer_id=$(($RANDOM %12))
 declare -a myArray=("FLIGHT" "BUS" "TRAIN")
 cat sample_message.txt | \
-sed "s/replace_timestamp_here/$timestamp_now/g; s/replace_service_type_here/${myArray[$random_enum_index]}/g; s/replace_customer_suffix_here/$random_3char_suffix/g" | \
+sed "s/replace_timestamp_here/$timestamp_now/g; s/replace_service_type_here/${myArray[$random_enum_index]}/g; s/replace_customer_suffix_here/$random_customer_id/g" | \
 protoc --proto_path=io/odpf/dagger/consumer/ --encode=io.odpf.dagger.consumer.TestBookingLogMessage io/odpf/dagger/consumer/TestLogMessage.proto > message.bin
