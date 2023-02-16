e5d05895817d34b1eedcd7f850d8d70ce43d0872
sumitaich1998 <aich.1998@gmail.com>
2023-02-16 15:50:18 +0530

    tests: fix tests

--- dagger-functions/src/test/java/io/odpf/dagger/functions/transformers/HashTransformerTest.java
+++ dagger-functions/src/test/java/io/odpf/dagger/functions/transformers/HashTransformerTest.java
@@ -17,6 +17,7 @@ import org.mockito.Mock;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -39,6 +40,13 @@ public class HashTransformerTest extends DaggerContextTestBase {
                 .thenReturn(false);
         when(configuration.getString("SCHEMA_REGISTRY_STENCIL_URLS", ""))
                 .thenReturn("");
+        when(configuration.getBoolean("SCHEMA_REGISTRY_STENCIL_CACHE_AUTO_REFRESH", false))
+                .thenReturn(false);
+        when(configuration.getLong("SCHEMA_REGISTRY_STENCIL_CACHE_TTL_MS", TimeUnit.HOURS.toMillis(2)))
+                .thenReturn(TimeUnit.HOURS.toMillis(2));
+        when(configuration.getString("SCHEMA_REGISTRY_STENCIL_REFRESH_STRATEGY", "LONG_POLLING"))
+                .thenReturn("LONG_POLLING");
+
     }
 
     @Test
